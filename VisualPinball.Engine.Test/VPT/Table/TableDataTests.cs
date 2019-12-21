eb17c5bbc1d25bfec459f315312cfe10ea835b76
freezy <freezy@kodi.tv>
2019-12-22 00:26:16 +0100

    tests: Remove dummy test.

---
+++ VisualPinball.Engine.Test/VPT/Table/TableDataTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace VisualPinball.Engine.Test.VPT.Table
+{
+	public class TableDataTests
+	{
+		[Fact]
+		public void ShouldLoadFile()
+		{
+			// var table = Engine.VPT.Table.Table.Load(@"D:\Pinball\Visual Pinball\Tables\Batman Dark Knight tt&NZ 1.2.vpx");
+			var table = Engine.VPT.Table.Table.Load(@"D:\Pinball\Visual Pinball\Tables\Medieval Madness X VPX- NZ&TT 1.1.vpx");
+		}
+	}
+}
