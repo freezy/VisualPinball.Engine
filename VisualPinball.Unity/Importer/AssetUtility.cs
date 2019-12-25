4c54c1e55f5ed724af0c68afc2ce08d027b58a03
shaderbytes <ian.pretorius.sa@gmail.com>
2019-12-26 01:27:33 +0200

    plenty of changes

---
+++ VisualPinball.Unity/Importer/AssetUtility.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEditor;
+
+namespace VisualPinball.Unity.Importer
+{
+	internal class AssetUtility
+	{
+		public AssetUtility()
+		{			
+
+		}
+
+		public string CreateDirectory(string basePath, string directoryPath)
+		{
+			string fullpath = ConcatPathsWithForwardSlash(new string[] { basePath, directoryPath });
+			if (!Directory.Exists(fullpath))
+			{
+				AssetDatabase.CreateFolder(basePath, directoryPath);
+
+			}
+			return fullpath;
+		}
+
+		public Material LoadMatarial(string basePath, string materialPath)
+		{
+			string fullpath = ConcatPathsWithForwardSlash(new string[] { basePath, materialPath });
+			return AssetDatabase.LoadAssetAtPath(fullpath, typeof(Material)) as Material;
+		}
+
+		public string ConcatPathsWithForwardSlash(string[] paths)
+		{
+			string fullPath = "";
+			for (int i = 0; i < paths.Length; i++)
+			{
+				fullPath += paths[i];
+				if (i < paths.Length - 1)
+				{
+					fullPath += "/";
+				}
+
+
+			}
+			return fullPath;
+
+		}
+	}
+}
---
+++ VisualPinball.Unity/Importer/VPXData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace VisualPinball.Unity.Importer
+{
+	class VPXData : ScriptableObject
+	{
+	}
+}
