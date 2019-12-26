109307a6431035d9fc0991fcf9f2f354a4715657
freezy <freezy@kodi.tv>
2019-12-26 01:10:13 +0100

    refactor: Make AssetUtility static, plus some cleanup.

--- VisualPinball.Unity/Importer/AssetUtility.cs
+++ VisualPinball.Unity/Importer/AssetUtility.cs
@@ -1,50 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using System.IO;
+﻿using System.IO;
 using UnityEditor;
+using UnityEngine;
 
 namespace VisualPinball.Unity.Importer
 {
-	internal class AssetUtility
+	internal static class AssetUtility
 	{
-		public AssetUtility()
-		{			
-
-		}
-
-		public string CreateDirectory(string basePath, string directoryPath)
+		public static string CreateDirectory(string basePath, string directoryPath)
 		{
-			string fullpath = ConcatPathsWithForwardSlash(new string[] { basePath, directoryPath });
-			if (!Directory.Exists(fullpath))
-			{
+			var fullPath = ConcatPathsWithForwardSlash(new[] { basePath, directoryPath });
+			if (!Directory.Exists(fullPath)) {
 				AssetDatabase.CreateFolder(basePath, directoryPath);
-
-			}
-			return fullpath;
-		}
-
-		public Material LoadMatarial(string basePath, string materialPath)
-		{
-			string fullpath = ConcatPathsWithForwardSlash(new string[] { basePath, materialPath });
-			return AssetDatabase.LoadAssetAtPath(fullpath, typeof(Material)) as Material;
-		}
-
-		public string ConcatPathsWithForwardSlash(string[] paths)
-		{
-			string fullPath = "";
-			for (int i = 0; i < paths.Length; i++)
-			{
-				fullPath += paths[i];
-				if (i < paths.Length - 1)
-				{
-					fullPath += "/";
-				}
-
-
 			}
 			return fullPath;
+		}
 
+		public static Material LoadMaterial(string basePath, string materialPath)
+		{
+			var fullPath = ConcatPathsWithForwardSlash(new[] { basePath, materialPath });
+			return AssetDatabase.LoadAssetAtPath(fullPath, typeof(Material)) as Material;
+		}
+
+		public static string ConcatPathsWithForwardSlash(string[] paths)
+		{
+			var fullPath = "";
+			for (var i = 0; i < paths.Length; i++) {
+				fullPath += paths[i];
+				if (i < paths.Length - 1) {
+					fullPath += "/";
+				}
+			}
+			return fullPath;
 		}
 	}
 }
