59f353e1cc3321371c5189eaa6873d343a608a71
freezy <freezy@kodi.tv>
2020-01-11 00:16:46 +0100

    surface: Fix mesh generation.

---
+++ VisualPinball.Engine.Test/VPT/Surface/SurfaceDataTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace VisualPinball.Engine.Test.VPT.Surface
+{
+	public class SurfaceDataTests
+	{
+		[Fact]
+		public void ShouldLoadCorrectData()
+		{
+			var table = Engine.VPT.Table.Table.Load(@"..\..\Fixtures\SurfaceData.vpx");
+			var data = table.Surfaces["Wall"].Data;
+
+			Assert.Equal(0.6985f, data.DisableLightingBelow);
+			Assert.Equal(0.1165f, data.DisableLightingTop);
+		}
+	}
+}
--- VisualPinball.Engine/VPT/Surface/Surface.cs
+++ VisualPinball.Engine/VPT/Surface/Surface.cs
@@ -1,11 +1,19 @@
 using System.IO;
+using VisualPinball.Engine.Game;
 
 namespace VisualPinball.Engine.VPT.Surface
 {
-	public class Surface : Item<SurfaceData>
+	public class Surface : Item<SurfaceData>, IRenderable
 	{
+		private readonly SurfaceMeshGenerator _meshGenerator;
 		public Surface(BinaryReader reader, string itemName) : base(new SurfaceData(reader, itemName))
 		{
+			_meshGenerator = new SurfaceMeshGenerator(Data);
+		}
+
+		public RenderObject[] GetRenderObjects(Table.Table table)
+		{
+			return _meshGenerator.GetRenderObjects(table);
 		}
 	}
 }
