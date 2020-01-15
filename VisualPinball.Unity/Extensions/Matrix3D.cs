1d9fe4417dbd85fc2b8f0d7c1540bdcbb115e861
shaderbytes <ian.pretorius.sa@gmail.com>
2020-01-15 07:30:55 +0200

    matrix mahem

--- VisualPinball.Unity/Extensions/Matrix3D.cs
+++ VisualPinball.Unity/Extensions/Matrix3D.cs
@@ -12,9 +12,9 @@ namespace VisualPinball.Unity.Extensions
 			var c4 = vpMatrix.Column4;
 			return new Matrix4x4(
 				new Vector4(c1.Item1, c1.Item2, c1.Item3, c1.Item4),
-				new Vector4(c2.Item1, c2.Item2, c2.Item3, c2.Item4),
-				new Vector4(c3.Item1, c3.Item2, c3.Item3, c3.Item4),
-				new Vector4(c4.Item1, c4.Item2, c4.Item3, c4.Item4)
+				new Vector4(c2.Item1, c2.Item3, c2.Item2, c2.Item4),
+				new Vector4(c3.Item1, c3.Item3, c3.Item2, c3.Item4),
+				new Vector4(c4.Item1, c4.Item3, -c4.Item2, c4.Item4)
 			);
 		}
 	}
