752b70642e9aadcc7aac6b9fb414c09aba8ab864
freezy <freezy@kodi.tv>
2020-01-22 21:15:33 +0100

    kicker: Add mesh generation.

--- VisualPinball.Engine/VPT/Kicker/KickerMeshGenerator.cs
+++ VisualPinball.Engine/VPT/Kicker/KickerMeshGenerator.cs
@@ -1,9 +1,11 @@
-﻿using VisualPinball.Engine.Game;
+﻿using System;
+using VisualPinball.Engine.Game;
+using VisualPinball.Engine.Math;
 using VisualPinball.Engine.Resources.Meshes;
 
 namespace VisualPinball.Engine.VPT.Kicker
 {
-	public class KickerMeshGenerator
+	public class KickerMeshGenerator : MeshGenerator
 	{
 		private readonly KickerData _data;
 
@@ -14,7 +16,68 @@ namespace VisualPinball.Engine.VPT.Kicker
 
 		public RenderObjectGroup GetRenderObjects(Table.Table table, Origin origin, bool asRightHanded)
 		{
+			var (preMatrix, _) = GetPreMatrix(table, origin, asRightHanded);
+			var postMatrix = GetPostMatrix(table, origin);
+			return new RenderObjectGroup(_data.Name, "Kickers", postMatrix, new RenderObject(
+					name: _data.Name,
+					mesh: GetBaseMesh().Transform(preMatrix),
+					material: table.GetMaterial(_data.Material),
+					isVisible: _data.KickerType != KickerType.KickerInvisible
+				)
+			);
+		}
 
+		protected override Tuple<Matrix3D, Matrix3D> GetTransformationMatrix(Table.Table table)
+		{
+			var zOffset = 0.0f;
+			var zRot = _data.Orientation;
+			switch (_data.KickerType) {
+				case KickerType.KickerCup:
+					zOffset = -0.18f;
+					break;
+				case KickerType.KickerWilliams:
+					zRot = _data.Orientation + 90.0f;
+					break;
+				case KickerType.KickerHole:
+					zRot = 0.0f;
+					break;
+				default:
+					zRot = 0.0f;
+					break;
+			}
+
+			// scale matrix
+			var scaleMatrix = new Matrix3D();
+			scaleMatrix.SetScaling(_data.Radius, _data.Radius, _data.Radius);
+
+			// translation matrix
+			var transMatrix = new Matrix3D();
+			transMatrix.SetTranslation(_data.Center.X, _data.Center.Y, zOffset);
+
+			// rotation matrix
+			var rotMatrix = new Matrix3D();
+			rotMatrix.RotateZMatrix(MathF.DegToRad(zRot));
+
+			var fullMatrix = scaleMatrix.Clone();
+			fullMatrix.Multiply(rotMatrix);
+			fullMatrix.Multiply(transMatrix);
+			scaleMatrix.SetScaling(1.0f, 1.0f, table.GetScaleZ());
+			fullMatrix.Multiply(scaleMatrix);
+
+			return new Tuple<Matrix3D, Matrix3D>(fullMatrix, null);
+		}
+
+		private Mesh GetBaseMesh()
+		{
+			switch (_data.KickerType) {
+				case KickerType.KickerCup: return KickerCupMesh.Clone();
+				case KickerType.KickerWilliams: return KickerWilliamsMesh.Clone();
+				case KickerType.KickerGottlieb: return KickerGottliebMesh.Clone();
+				case KickerType.KickerCup2: return KickerT1Mesh.Clone();
+				case KickerType.KickerHole: return KickerHoleMesh.Clone();
+				case KickerType.KickerHoleSimple: return KickerSimpleHoleMesh.Clone();
+				default:  return KickerSimpleHoleMesh.Clone();
+			}
 		}
 
 		#region Mesh Imports
