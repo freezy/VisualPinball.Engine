1edc68ae640c794e2fee8c2f6c92595c92ae7eeb
freezy <freezy@kodi.tv>
2020-01-22 21:07:19 +0100

    kicker: Add missing meshes and item and generator classes.

---
+++ VisualPinball.Engine/VPT/Kicker/Kicker.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using VisualPinball.Engine.Game;
+
+namespace VisualPinball.Engine.VPT.Kicker
+{
+	public class Kicker : Item<KickerData>, IRenderable
+	{
+		private readonly KickerMeshGenerator _meshGenerator;
+
+		public Kicker(BinaryReader reader, string itemName) : base(new KickerData(reader, itemName))
+		{
+			_meshGenerator = new KickerMeshGenerator(Data);
+		}
+
+		public RenderObjectGroup GetRenderObjects(Table.Table table, Origin origin = Origin.Global, bool asRightHanded = true)
+		{
+			return _meshGenerator.GetRenderObjects(table, origin, asRightHanded);
+		}
+	}
+}
---
+++ VisualPinball.Engine/VPT/Kicker/KickerMeshGenerator.cs
@@ -0,0 +1,31 @@
+﻿using VisualPinball.Engine.Game;
+using VisualPinball.Engine.Resources.Meshes;
+
+namespace VisualPinball.Engine.VPT.Kicker
+{
+	public class KickerMeshGenerator
+	{
+		private readonly KickerData _data;
+
+		public KickerMeshGenerator(KickerData data)
+		{
+			_data = data;
+		}
+
+		public RenderObjectGroup GetRenderObjects(Table.Table table, Origin origin, bool asRightHanded)
+		{
+
+		}
+
+		#region Mesh Imports
+
+		private static readonly Mesh KickerCupMesh = new Mesh(KickerCup.Vertices, KickerCup.Indices);
+		private static readonly Mesh KickerGottliebMesh = new Mesh(KickerGottlieb.Vertices, KickerGottlieb.Indices);
+		private static readonly Mesh KickerHoleMesh = new Mesh(KickerHole.Vertices, KickerHole.Indices);
+		private static readonly Mesh KickerSimpleHoleMesh = new Mesh(KickerSimpleHole.Vertices, KickerSimpleHole.Indices);
+		private static readonly Mesh KickerT1Mesh = new Mesh(KickerT1.Vertices, KickerT1.Indices);
+		private static readonly Mesh KickerWilliamsMesh = new Mesh(KickerWilliams.Vertices, KickerWilliams.Indices);
+
+		#endregion
+	}
+}
