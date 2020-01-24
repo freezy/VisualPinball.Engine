00b79f492ea10101cbaf9a5487a2d09682498a3b
freezy <freezy@kodi.tv>
2020-01-24 23:44:33 +0100

    perf: Add profiler and profile table import.

--- VisualPinball.Engine/VPT/Table/TableLoader.cs
+++ VisualPinball.Engine/VPT/Table/TableLoader.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
 using NLog;
@@ -16,11 +17,14 @@ namespace VisualPinball.Engine.VPT.Table
 
 		public static Table Load(string filename, bool loadGameItems = true)
 		{
+			Profiler.Start("TableLoader.Load()");
 			var cf = new CompoundFile(filename);
 			try {
+				Profiler.Start("I/O");
 				var gameStorage = cf.RootStorage.GetStorage("GameStg");
 				var gameData = gameStorage.GetStream("GameData");
 				var bytes = gameData.GetData();
+				Profiler.Stop("I/O");
 
 				using (var stream = new MemoryStream(bytes))
 				using (var reader = new BinaryReader(stream)) {
@@ -38,6 +42,7 @@ namespace VisualPinball.Engine.VPT.Table
 
 			} finally {
 				cf.Close();
+				Profiler.Stop("TableLoader.Load()");
 			}
 		}
 
@@ -65,10 +70,13 @@ namespace VisualPinball.Engine.VPT.Table
 
 		private static void LoadGameItems(Table table, CFStorage storage)
 		{
+			Profiler.Start("LoadGameItems");
 			for (var i = 0; i < table.Data.NumGameItems; i++) {
+				Profiler.Start("LoadGameItems (I/O)");
 				var itemName = $"GameItem{i}";
 				var itemStream = storage.GetStream(itemName);
 				var itemData = itemStream.GetData();
+				Profiler.Stop("LoadGameItems (I/O)");
 				if (itemData.Length < 4) {
 					Logger.Warn("Skipping {itemName} because it has size of {itemDataLength}.", itemName, itemData.Length);
 					continue;
@@ -76,105 +84,100 @@ namespace VisualPinball.Engine.VPT.Table
 
 				var reader = new BinaryReader(new MemoryStream(itemData));
 				var itemType = reader.ReadInt32();
+				Profiler.Start("LoadGameItems (parse)");
 				switch (itemType) {
 					case ItemType.Bumper: {
-						Logger.Info("Loading bumper {itemName}", itemName);
 						var item = new VisualPinball.Engine.VPT.Bumper.Bumper(reader, itemName);
 						table.Bumpers[item.Name] = item;
 						break;
 					}
 					case ItemType.Flasher: {
-						Logger.Info("Loading flasher {itemName}", itemName);
 						var item = new VisualPinball.Engine.VPT.Flasher.Flasher(reader, itemName);
 						table.Flashers[item.Name] = item;
 						break;
 					}
 					case ItemType.Flipper: {
-						Logger.Info("Loading flipper {itemName}", itemName);
 						var item = new VisualPinball.Engine.VPT.Flipper.Flipper(reader, itemName);
 						table.Flippers[item.Name] = item;
 						break;
 					}
 					case ItemType.Gate: {
-						Logger.Info("Loading gate {itemName}", itemName);
 						var item = new VisualPinball.Engine.VPT.Gate.Gate(reader, itemName);
 						table.Gates[item.Name] = item;
 						break;
 					}
 					case ItemType.HitTarget: {
-						Logger.Info("Loading hit target {itemName}", itemName);
 						var item = new VisualPinball.Engine.VPT.HitTarget.HitTarget(reader, itemName);
 						table.HitTargets[item.Name] = item;
 						break;
 					}
 					case ItemType.Kicker: {
-						Logger.Info("Loading kicker {itemName}", itemName);
 						var item = new VisualPinball.Engine.VPT.Kicker.Kicker(reader, itemName);
 						table.Kickers[item.Name] = item;
 						break;
 					}
 					case ItemType.Light: {
-						Logger.Info("Loading light {itemName}", itemName);
 						var item = new VisualPinball.Engine.VPT.Light.Light(reader, itemName);
 						table.Lights[item.Name] = item;
 						break;
 					}
 					case ItemType.Primitive: {
-						Logger.Info("Loading primitive {itemName}", itemName);
 						var item = new Primitive.Primitive(reader, itemName);
 						table.Primitives[item.Name] = item;
 						break;
 					}
 					case ItemType.Ramp: {
-						Logger.Info("Loading ramp {itemName}", itemName);
 						var item = new Ramp.Ramp(reader, itemName);
 						table.Ramps[item.Name] = item;
 						break;
 					}
 					case ItemType.Rubber: {
-						Logger.Info("Loading rubber {itemName}", itemName);
 						var item = new Rubber.Rubber(reader, itemName);
 						table.Rubbers[item.Name] = item;
 						break;
 					}
 					case ItemType.Spinner: {
-						Logger.Info("Loading spinner {itemName}", itemName);
 						var item = new Spinner.Spinner(reader, itemName);
 						table.Spinners[item.Name] = item;
 						break;
 					}
 					case ItemType.Surface: {
-						Logger.Info("Loading surface {itemName}", itemName);
 						var item = new Surface.Surface(reader, itemName);
 						table.Surfaces[item.Name] = item;
 						break;
 					}
 					case ItemType.Trigger: {
-						Logger.Info("Loading trigger {itemName}", itemName);
 						var item = new Trigger.Trigger(reader, itemName);
 						table.Triggers[item.Name] = item;
 						break;
 					}
 				}
+				Profiler.Stop("LoadGameItems (parse)");
 			}
+			Profiler.Stop("LoadGameItems");
 		}
 
 		private static void LoadTextures(Table table, CFStorage storage)
 		{
+			Profiler.Start("LoadTextures");
 			for (var i = 0; i < table.Data.NumTextures; i++) {
 				var textureName = $"Image{i}";
+				Profiler.Start("LoadTextures (I/O)");
 				var textureStream = storage.GetStream(textureName);
 				var textureData = textureStream.GetData();
+				Profiler.Stop("LoadTextures (I/O)");
 				if (textureData.Length < 4) {
 					Logger.Warn("Skipping {itemName} because it has size of {itemDataLength}.", textureName, textureData.Length);
 					continue;
 				}
 
+				Profiler.Start("LoadTextures (parse)");
 				var reader = new BinaryReader(new MemoryStream(textureData));
-				Logger.Info("Loading texture {itemName}", textureName);
 				var texture = new Texture(reader, textureName);
 				table.Textures[texture.Name.ToLower()] = texture;
+				Profiler.Stop("LoadTextures (parse)");
 			}
+			Profiler.Stop("LoadTextures");
 		}
 
 		private static void LoadTableInfo(Table table, CFStorage storage)
