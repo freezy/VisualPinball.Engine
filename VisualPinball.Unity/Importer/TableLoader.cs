0c1fff0ddd4660b2eded2d727955aef2fcffa444
freezy <freezy@kodi.tv>
2020-01-25 01:05:41 +0100

    perf: Add missing game elements to job.

--- VisualPinball.Unity/Importer/TableLoader.cs
+++ VisualPinball.Unity/Importer/TableLoader.cs
@@ -7,8 +7,20 @@ using Unity.Collections;
 using Unity.Jobs;
 using VisualPinball.Engine.IO;
 using VisualPinball.Engine.VPT;
+using VisualPinball.Engine.VPT.Bumper;
+using VisualPinball.Engine.VPT.Flasher;
+using VisualPinball.Engine.VPT.Flipper;
+using VisualPinball.Engine.VPT.Gate;
+using VisualPinball.Engine.VPT.HitTarget;
+using VisualPinball.Engine.VPT.Kicker;
+using VisualPinball.Engine.VPT.Light;
 using VisualPinball.Engine.VPT.Primitive;
+using VisualPinball.Engine.VPT.Ramp;
+using VisualPinball.Engine.VPT.Rubber;
+using VisualPinball.Engine.VPT.Spinner;
+using VisualPinball.Engine.VPT.Surface;
 using VisualPinball.Engine.VPT.Table;
+using VisualPinball.Engine.VPT.Trigger;
 
 namespace VisualPinball.Unity.Importer
 {
@@ -43,9 +55,69 @@ namespace VisualPinball.Unity.Importer
 					if (job.ItemObj[i].ToInt32() > 0) {
 						var objHandle = (GCHandle) job.ItemObj[i];
 						switch (job.ItemType[i]) {
+							case ItemType.Bumper: {
+								var item = objHandle.Target as Bumper;
+								table.Bumpers[item.Name] = item;
+								break;
+							}
+							case ItemType.Flasher: {
+								var item = objHandle.Target as Flasher;
+								table.Flashers[item.Name] = item;
+								break;
+							}
+							case ItemType.Flipper: {
+								var item = objHandle.Target as Flipper;
+								table.Flippers[item.Name] = item;
+								break;
+							}
+							case ItemType.Gate: {
+								var item = objHandle.Target as Gate;
+								table.Gates[item.Name] = item;
+								break;
+							}
+							case ItemType.HitTarget: {
+								var item = objHandle.Target as HitTarget;
+								table.HitTargets[item.Name] = item;
+								break;
+							}
+							case ItemType.Kicker: {
+								var item = objHandle.Target as Kicker;
+								table.Kickers[item.Name] = item;
+								break;
+							}
+							case ItemType.Light: {
+								var item = objHandle.Target as Light;
+								table.Lights[item.Name] = item;
+								break;
+							}
 							case ItemType.Primitive: {
 								var item = objHandle.Target as Primitive;
 								table.Primitives[item.Name] = item;
+								break;
+							}
+							case ItemType.Ramp: {
+								var item = objHandle.Target as Ramp;
+								table.Ramps[item.Name] = item;
+								break;
+							}
+							case ItemType.Rubber: {
+								var item = objHandle.Target as Rubber;
+								table.Rubbers[item.Name] = item;
+								break;
+							}
+							case ItemType.Spinner: {
+								var item = objHandle.Target as Spinner;
+								table.Spinners[item.Name] = item;
+								break;
+							}
+							case ItemType.Surface: {
+								var item = objHandle.Target as Surface;
+								table.Surfaces[item.Name] = item;
+								break;
+							}
+							case ItemType.Trigger: {
+								var item = objHandle.Target as Trigger;
+								table.Triggers[item.Name] = item;
 								break;
 							}
 						}
