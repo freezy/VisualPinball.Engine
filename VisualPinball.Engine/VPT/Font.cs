b3379aea64a2d5716a66af9561face057255e678
freezy <freezy@kodi.tv>
2020-02-06 00:33:13 +0100

    textbox: Fix parsing and add font support.

---
+++ VisualPinball.Engine/VPT/Font.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Text;
+using VisualPinball.Engine.VPT.Table;
+
+namespace VisualPinball.Engine.VPT
+{
+	public class Font
+	{
+		public string Name;
+		public ushort Weight;
+		public uint Size;
+		public bool Italic;
+
+		public Font(BinaryReader reader)
+		{
+			reader.BaseStream.Seek(3, SeekOrigin.Current);
+			Italic = reader.ReadByte() > 0;
+			Weight = reader.ReadUInt16();
+			Size = reader.ReadUInt32();
+			var nameLen = (int)reader.ReadByte();
+			Name = Encoding.ASCII.GetString(reader.ReadBytes(nameLen));
+		}
+
+		public void Write(BinaryWriter writer, HashWriter hashWriter)
+		{
+			writer.Write(new byte[]{ 0x01, 0x0, 0x0 });
+			writer.Write((byte)(Italic ? 0x02 : 0x0));
+			writer.Write(Size);
+			writer.Write((byte)Name.Length);
+			writer.Write(Encoding.ASCII.GetBytes(Name));
+		}
+	}
+}
