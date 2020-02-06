1e8fc47046b2b9a091a21cfc929e35522f33cc41
freezy <freezy@kodi.tv>
2020-02-07 00:36:31 +0100

    flipper: Add data write tests.

--- VisualPinball.Engine/VPT/Flipper/FlipperData.cs
+++ VisualPinball.Engine/VPT/Flipper/FlipperData.cs
@@ -17,98 +17,106 @@ namespace VisualPinball.Engine.VPT.Flipper
 	[Serializable]
 	public class FlipperData : ItemData
 	{
-		[BiffString("NAME", IsWideString = true)]
+		[BiffString("NAME", IsWideString = true, Pos = 14)]
 		public override string Name { get; set; }
 
-		[BiffFloat("BASR")]
+		[BiffFloat("BASR", Pos = 2)]
 		public float BaseRadius = 21.5f;
 
-		[BiffFloat("ENDR")]
+		[BiffFloat("ENDR", Pos = 3)]
 		public float EndRadius = 13.0f;
 
-		[BiffFloat("FRMN")]
+		[BiffFloat("FRMN", Pos = 29)]
 		public float FlipperRadiusMin;
 
-		[BiffFloat("FLPR")]
+		[BiffFloat("FLPR", Pos = 4)]
 		public float FlipperRadiusMax = 130.0f;
 
-		[BiffFloat("FLPR")]
+		[BiffFloat("FLPR", SkipWrite = true)]
 		public float FlipperRadius = 130.0f;
 
-		[BiffFloat("ANGS")]
+		[BiffFloat("ANGS", Pos = 6)]
 		public float StartAngle = 121.0f;
 
-		[BiffFloat("ANGE")]
+		[BiffFloat("ANGE", Pos = 7)]
 		public float EndAngle = 70.0f;
 
-		[BiffFloat("FHGT")]
+		[BiffFloat("FHGT", Pos = 30)]
 		public float Height = 50.0f;
 
-		[BiffVertex("VCEN")]
+		[BiffVertex("VCEN", Pos = 1)]
 		public Vertex2D Center;
 
-		[BiffString("IMAG")]
+		[BiffString("IMAG", Pos = 31)]
 		public string Image;
 
-		[BiffString("SURF")]
+		[BiffString("SURF", Pos = 12)]
 		public string Surface;
 
-		[BiffString("MATR")]
+		[BiffString("MATR", Pos = 13)]
 		public string Material;
 
-		[BiffString("RUMA")]
+		[BiffString("RUMA", Pos = 15)]
 		public string RubberMaterial;
 
-		[BiffFloat("RTHF")]
+		[BiffFloat("RTHF", Pos = 16.1)]
 		public float RubberThickness = 7.0f;
 
-		[BiffFloat("RHGF")]
+		[BiffFloat("RHGF", Pos = 17.1)]
 		public float RubberHeight = 19.0f;
 
-		[BiffFloat("RWDF")]
+		[BiffFloat("RWDF", Pos = 18.1)]
 		public float RubberWidth = 24.0f;
 
-		[BiffFloat("FORC")]
+		[BiffFloat("FORC", Pos = 9)]
 		public float Mass;
 
-		[BiffFloat("STRG")]
+		[BiffFloat("STRG", Pos = 19)]
 		public float Strength;
 
-		[BiffFloat("ELAS")]
+		[BiffFloat("ELAS", Pos = 20)]
 		public float Elasticity;
 
-		[BiffFloat("ELFO")]
+		[BiffFloat("ELFO", Pos = 21)]
 		public float ElasticityFalloff;
 
-		[BiffFloat("FRIC")]
+		[BiffFloat("FRIC", Pos = 22)]
 		public float Friction;
 
-		[BiffFloat("FRTN")]
+		[BiffFloat("FRTN", Pos = 5)]
 		public float Return;
 
-		[BiffFloat("RPUP")]
+		[BiffFloat("RPUP", Pos = 23)]
 		public float RampUp;
 
-		[BiffFloat("TODA")]
+		[BiffFloat("TODA", Pos = 25)]
 		public float TorqueDamping;
 
-		[BiffFloat("TDAA")]
+		[BiffFloat("TDAA", Pos = 26)]
 		public float TorqueDampingAngle;
 
-		[BiffFloat("SCTR")]
+		[BiffFloat("SCTR", Pos = 24)]
 		public float Scatter;
 
-		[BiffInt("OVRP")]
+		[BiffInt("OVRP", Pos = 8)]
 		public int OverridePhysics;
 
-		[BiffBool("VSBL")]
+		[BiffBool("VSBL", Pos = 27)]
 		public bool IsVisible = true;
 
-		[BiffBool("ENBL")]
+		[BiffBool("ENBL", Pos = 28)]
 		public bool IsEnabled = true;
 
-		[BiffBool("REEN")]
+		[BiffBool("REEN", Pos = 32)]
 		public bool IsReflectionEnabled = true;
+
+		[BiffBool("TMON", Pos = 10)]
+		public bool IsTimerEnabled;
+
+		[BiffInt("TMIN", Pos = 11)]
+		public int TimerInterval;
+
+		protected override bool SkipHash() => true;
 
 		#region BIFF
 
