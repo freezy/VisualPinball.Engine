3c130350373c945430c1fc65cd330774fc8b7901
freezy <freezy@kodi.tv>
2020-02-10 23:26:56 +0100

    unity: Add serializable dictionary.

---
+++ VisualPinball.Unity/Common/SerializableDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VisualPinball.Unity.Common
+{
+	[Serializable]
+	public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+	{
+		[SerializeField]
+		private List<TKey> keys = new List<TKey>();
+
+		[SerializeField]
+		private List<TValue> values = new List<TValue>();
+
+		// save the dictionary to lists
+		public void OnBeforeSerialize()
+		{
+			keys.Clear();
+			values.Clear();
+			foreach (var pair in this) {
+				keys.Add(pair.Key);
+				values.Add(pair.Value);
+			}
+		}
+
+		// load dictionary from lists
+		public void OnAfterDeserialize()
+		{
+			Clear();
+
+			if (keys.Count != values.Count)
+				throw new Exception(string.Format("there are {0} keys and {1} values after deserialization. Make sure that both key and value types are serializable."));
+
+			for (var i = 0; i < keys.Count; i++)
+				Add(keys[i], values[i]);
+		}
+	}
+}
