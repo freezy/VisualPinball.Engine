02854095980c64e345c8536893cbce12369044f4
freezy <freezy@kodi.tv>
2020-02-20 00:01:46 +0100

    flippers: Add tests for movements.

--- VisualPinball.Engine.Test/VPT/Flipper/FlipperPhysicsTests.cs
+++ VisualPinball.Engine.Test/VPT/Flipper/FlipperPhysicsTests.cs
@@ -1,4 +1,5 @@
-﻿using VisualPinball.Engine.Game;
+﻿using FluentAssertions;
+using VisualPinball.Engine.Game;
 using VisualPinball.Engine.Math;
 using VisualPinball.Engine.Test.Test;
 using Xunit;
@@ -7,20 +8,76 @@ namespace VisualPinball.Engine.Test.VPT.Flipper
 {
 	public class FlipperPhysicsTests
 	{
-		[Fact]
-		public void ShouldMove()
-		{
-			var table = Engine.VPT.Table.Table.Load(VpxPath.Flipper);
-			var player = new Player(table).Init();
+		private readonly Engine.VPT.Table.Table _table;
 
-			var flipper = table.Flippers["FlipperR"];
-			var flipperState = flipper.State;
+		public FlipperPhysicsTests()
+		{
+			_table = Engine.VPT.Table.Table.Load(VpxPath.Flipper);
+		}
+
+		[Fact]
+		public void ShouldMoveToTheEndWhenSolenoidIsTurnedOn()
+		{
+			var player = new Player(_table).Init();
+			var flipper = _table.Flippers["FlipperR"];
+			var startAngleRad = MathF.DegToRad(flipper.Data.StartAngle);
 			var endAngleRad = MathF.DegToRad(flipper.Data.EndAngle);
+
+			// assert it's down
+			flipper.State.Angle.Should().Be(startAngleRad);
 
 			flipper.RotateToEnd();
 			player.SimulateTime(100);
 
-			Assert.Equal(endAngleRad, flipperState.Angle);
+			// assert it's up
+			flipper.State.Angle.Should().Be(endAngleRad);
+		}
+
+		[Fact]
+		public void ShouldMoveToStartWhenSolenoidIsTurnedOff()
+		{
+			var player = new Player(_table).Init();
+			var flipper = _table.Flippers["FlipperR"];
+			var startAngleRad = MathF.DegToRad(flipper.Data.StartAngle);
+
+			// move up
+			flipper.RotateToEnd();
+			player.SimulateTime(52);
+
+			// move down again
+			flipper.RotateToStart();
+			player.SimulateTime(300);
+
+			flipper.State.Angle.Should().Be(startAngleRad);
+		}
+
+		[Fact]
+		public void ShouldMoveBackToEndWhenPressedWhileMoving()
+		{
+			var player = new Player(_table).Init();
+			var flipper = _table.Flippers["FlipperR"];
+			var startAngleRad = MathF.DegToRad(flipper.Data.StartAngle);
+			var endAngleRad = MathF.DegToRad(flipper.Data.EndAngle);
+
+			// move up
+			flipper.RotateToEnd();
+			player.SimulateTime(52); // hit at 51ms
+			flipper.State.Angle.Should().BeGreaterThan(startAngleRad);
+
+			// move down
+			flipper.RotateToStart();
+			player.SimulateTime(200);
+
+			// assert it's in the middle
+			flipper.State.Angle.Should().BeGreaterThan(startAngleRad);
+			flipper.State.Angle.Should().BeLessThan(endAngleRad);
+
+			// move up again
+			flipper.RotateToEnd();
+			player.SimulateTime(500);
+
+			// assert it's back up in less than half the time
+			flipper.State.Angle.Should().Be(endAngleRad);
 		}
 	}
 }
