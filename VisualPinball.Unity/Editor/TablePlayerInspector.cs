2a933fc95b42d47f55d405a53929572fe965b4a3
freezy <freezy@kodi.tv>
2020-02-23 22:43:20 +0100

    unity: Add ball spawner to table player component.

---
+++ VisualPinball.Unity/Editor/TablePlayerInspector.cs
@@ -0,0 +1,49 @@
+﻿using UnityEditor;
+using UnityEngine;
+using VisualPinball.Engine.Game;
+using VisualPinball.Engine.Math;
+using VisualPinball.Engine.VPT.Ball;
+using VisualPinball.Engine.VPT.Table;
+using VisualPinball.Unity.Game;
+
+namespace VisualPinball.Unity.Editor
+{
+	[CustomEditor(typeof(TablePlayer))]
+	public class TablePlayerInspector : UnityEditor.Editor
+	{
+		private TablePlayer _tablePlayer;
+
+		private void OnEnable()
+		{
+			_tablePlayer = (TablePlayer) target;
+		}
+
+		public override void OnInspectorGUI()
+		{
+			if (GUILayout.Button("Spawn Ball")) {
+				_tablePlayer.Player?.CreateBall(new TestBallCreator(600f, 200f, 0));
+			}
+		}
+	}
+
+	public class TestBallCreator : IBallCreationPosition
+	{
+		private readonly Vertex3D _pos;
+		private readonly Vertex3D _vel;
+
+		public TestBallCreator(float x, float y, float z)
+		{
+			_pos = new Vertex3D(x, y, z);
+			_vel = new Vertex3D(0, 0, 0);
+		}
+
+		public Vertex3D GetBallCreationPosition(Table table) => _pos;
+
+		public Vertex3D GetBallCreationVelocity(Table table) => _vel;
+
+		public void OnBallCreated(PlayerPhysics physics, Ball ball)
+		{
+			// do nothing
+		}
+	}
+}
