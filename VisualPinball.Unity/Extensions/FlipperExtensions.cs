a7feacd2b7f3063fd566fa71021be90f2f69121e
freezy <freezy@kodi.tv>
2020-02-26 21:07:12 +0100

    unity: Fix assembly references.

--- VisualPinball.Unity/Extensions/FlipperExtensions.cs
+++ VisualPinball.Unity/Extensions/FlipperExtensions.cs
@@ -1,12 +1,8 @@
 ﻿using Unity.Entities;
-using Unity.Mathematics;
 using UnityEngine;
 using VisualPinball.Engine.Game;
 using VisualPinball.Engine.VPT.Flipper;
-using VisualPinball.Engine.VPT.Surface;
-using VisualPinball.Engine.VPT.Table;
 using VisualPinball.Unity.Components;
-using VisualPinball.Unity.Physics.Flipper;
 
 namespace VisualPinball.Unity.Extensions
 {
