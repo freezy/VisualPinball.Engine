7896e7882bcfd739cf02f73dc3b3aa0a9ee3f05b
freezy <freezy@kodi.tv>
2020-02-27 00:27:15 +0100

    unity: Fix euler calculations.

--- VisualPinball.Unity/Physics/Flipper/FlipperRotateSystem.cs
+++ VisualPinball.Unity/Physics/Flipper/FlipperRotateSystem.cs
@@ -4,6 +4,7 @@ using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
 using UnityEngine;
+using VisualPinball.Unity.Extensions;
 
 namespace VisualPinball.Unity.Physics.Flipper
 {
@@ -14,14 +15,14 @@ namespace VisualPinball.Unity.Physics.Flipper
 		{
 			public void Execute(ref Rotation rot, [ReadOnly] ref FlipperMovementData movement, [ReadOnly] ref LocalToWorld ltw)
 			{
-				var r = new Quaternion {x = rot.Value.value[0], y = rot.Value.value[1], z = rot.Value.value[2], w = rot.Value.value[3]};
-				var e = r.eulerAngles;
+				var e = rot.Value.ToEuler();
 
 				// nothing applied:
 				// left flipper parent: -0.374 / 0.655 / 0.569 / 0.325
 				//                base:  0     / 0     / 0     / 1
 				//              rubber: -0.374 / 0.655 / 0.569 / 0.325
-				rot.Value = quaternion.Euler(e.x, e.y, movement.Angle);
+
+				rot.Value = quaternion.EulerXYZ(e.x, e.y, movement.Angle);
 			}
 		}
 
