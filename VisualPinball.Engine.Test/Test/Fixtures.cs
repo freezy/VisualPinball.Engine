273bea8260021510f3cb164a42e5600b779fa382
freezy <freezy@kodi.tv>
2020-03-05 00:12:25 +0100

    test: Move fixtures to common folder.

--- VisualPinball.Engine.Test/Test/Fixtures.cs
+++ VisualPinball.Engine.Test/Test/Fixtures.cs
@@ -2,61 +2,61 @@ namespace VisualPinball.Engine.Test.Test
 {
 	public static class VpxPath
 	{
-		public const string Bumper = @"..\..\VPT\Bumper\BumperTest.vpx";
-		public const string Collection = @"..\..\VPT\Collection\CollectionTest.vpx";
-		public const string Decal = @"..\..\VPT\Decal\DecalTest.vpx";
-		public const string DispReel = @"..\..\VPT\DispReel\DispReelTest.vpx";
-		public const string Flasher = @"..\..\VPT\Flasher\FlasherTest.vpx";
-		public const string Flipper = @"..\..\VPT\Flipper\FlipperTest.vpx";
-		public const string Gate = @"..\..\VPT\Gate\GateTest.vpx";
-		public const string HitTarget = @"..\..\VPT\HitTarget\HitTargetTest.vpx";
-		public const string Kicker = @"..\..\VPT\Kicker\KickerTest.vpx";
-		public const string Light = @"..\..\VPT\Light\LightTest.vpx";
-		public const string LightSeq = @"..\..\VPT\LightSeq\LightSeqTest.vpx";
-		public const string Material = @"..\..\VPT\MaterialTest.vpx";
-		public const string MaterialTexture = @"..\..\VPT\MaterialTextureTest.vpx";
-		public const string Plunger = @"..\..\VPT\Plunger\PlungerTest.vpx";
-		public const string Primitive = @"..\..\VPT\Primitive\PrimitiveTest.vpx";
-		public const string PrimitiveCompressed = @"..\..\VPT\Primitive\PrimitiveCompressed.vpx";
-		public const string Ramp = @"..\..\VPT\Ramp\RampTest.vpx";
-		public const string Rubber = @"..\..\VPT\Rubber\RubberTest.vpx";
-		public const string Sound = @"..\..\VPT\Sound\SoundTest.vpx";
-		public const string Spinner = @"..\..\VPT\Spinner\SpinnerTest.vpx";
-		public const string Surface = @"..\..\VPT\Surface\SurfaceTest.vpx";
-		public const string Table = @"..\..\VPT\Table\TableTest.vpx";
-		public const string TableChecksum = @"..\..\VPT\Table\TableChecksumTest.vpx";
-		public const string TextBox = @"..\..\VPT\Textbox\TextboxTest.vpx";
-		public const string Texture = @"..\..\VPT\TextureTest.vpx";
-		public const string Timer = @"..\..\VPT\Timer\TimerTest.vpx";
-		public const string Trigger = @"..\..\VPT\Trigger\TriggerTest.vpx";
+		public const string Bumper = @"..\..\Fixtures~\BumperTest.vpx";
+		public const string Collection = @"..\..\Fixtures~\CollectionTest.vpx";
+		public const string Decal = @"..\..\Fixtures~\DecalTest.vpx";
+		public const string DispReel = @"..\..\Fixtures~\DispReelTest.vpx";
+		public const string Flasher = @"..\..\Fixtures~\FlasherTest.vpx";
+		public const string Flipper = @"..\..\Fixtures~\FlipperTest.vpx";
+		public const string Gate = @"..\..\Fixtures~\GateTest.vpx";
+		public const string HitTarget = @"..\..\Fixtures~\HitTargetTest.vpx";
+		public const string Kicker = @"..\..\Fixtures~\KickerTest.vpx";
+		public const string Light = @"..\..\Fixtures~\LightTest.vpx";
+		public const string LightSeq = @"..\..\Fixtures~\LightSeqTest.vpx";
+		public const string Material = @"..\..\Fixtures~\MaterialTest.vpx";
+		public const string MaterialTexture = @"..\..\Fixtures~\MaterialTextureTest.vpx";
+		public const string Plunger = @"..\..\Fixtures~\PlungerTest.vpx";
+		public const string Primitive = @"..\..\Fixtures~\PrimitiveTest.vpx";
+		public const string PrimitiveCompressed = @"..\..\Fixtures~\PrimitiveCompressed.vpx";
+		public const string Ramp = @"..\..\Fixtures~\RampTest.vpx";
+		public const string Rubber = @"..\..\Fixtures~\RubberTest.vpx";
+		public const string Sound = @"..\..\Fixtures~\SoundTest.vpx";
+		public const string Spinner = @"..\..\Fixtures~\SpinnerTest.vpx";
+		public const string Surface = @"..\..\Fixtures~\SurfaceTest.vpx";
+		public const string Table = @"..\..\Fixtures~\TableTest.vpx";
+		public const string TableChecksum = @"..\..\Fixtures~\TableChecksumTest.vpx";
+		public const string TextBox = @"..\..\Fixtures~\TextboxTest.vpx";
+		public const string Texture = @"..\..\Fixtures~\TextureTest.vpx";
+		public const string Timer = @"..\..\Fixtures~\TimerTest.vpx";
+		public const string Trigger = @"..\..\Fixtures~\TriggerTest.vpx";
 	}
 
 	public static class ObjPath
 	{
-		public const string Bumper = @"..\..\VPT\Bumper\BumperTest.obj";
-		public const string Flipper = @"..\..\VPT\Flipper\FlipperTest.obj";
-		public const string Gate = @"..\..\VPT\Gate\GateTest.obj";
-		public const string HitTarget = @"..\..\VPT\HitTarget\HitTargetTest.obj";
-		public const string Kicker = @"..\..\VPT\Kicker\KickerTest.obj";
-		public const string Primitive = @"..\..\VPT\Primitive\PrimitiveTest.obj";
-		public const string PrimitiveCompressed = @"..\..\VPT\Primitive\PrimitiveCompressed.obj";
-		public const string Ramp = @"..\..\VPT\Ramp\RampTest.obj";
-		public const string Rubber = @"..\..\VPT\Rubber\RubberTest.obj";
-		public const string Spinner = @"..\..\VPT\Spinner\SpinnerTest.obj";
-		public const string Surface = @"..\..\VPT\Surface\SurfaceTest.obj";
-		public const string Table = @"..\..\VPT\Table\TableTest.obj";
-		public const string Trigger = @"..\..\VPT\Trigger\TriggerTest.obj";
+		public const string Bumper = @"..\..\Fixtures~\BumperTest.obj";
+		public const string Flipper = @"..\..\Fixtures~\FlipperTest.obj";
+		public const string Gate = @"..\..\Fixtures~\GateTest.obj";
+		public const string HitTarget = @"..\..\Fixtures~\HitTargetTest.obj";
+		public const string Kicker = @"..\..\Fixtures~\KickerTest.obj";
+		public const string Primitive = @"..\..\Fixtures~\PrimitiveTest.obj";
+		public const string PrimitiveCompressed = @"..\..\Fixtures~\PrimitiveCompressed.obj";
+		public const string Ramp = @"..\..\Fixtures~\RampTest.obj";
+		public const string Rubber = @"..\..\Fixtures~\RubberTest.obj";
+		public const string Spinner = @"..\..\Fixtures~\SpinnerTest.obj";
+		public const string Surface = @"..\..\Fixtures~\SurfaceTest.obj";
+		public const string Table = @"..\..\Fixtures~\TableTest.obj";
+		public const string Trigger = @"..\..\Fixtures~\TriggerTest.obj";
 	}
 
 	public static class TexturePath
 	{
-		public const string Exr = @"..\..\VPT\comp_piz.exr";
-		public const string Bmp = @"..\..\VPT\test_pattern.bmp";
-		public const string BmpArgb = @"..\..\VPT\test_pattern_argb.bmp";
-		public const string BmpXrgb = @"..\..\VPT\test_pattern_xrgb.bmp";
-		public const string Jpg = @"..\..\VPT\test_pattern.jpg";
-		public const string Png = @"..\..\VPT\test_pattern.png";
-		public const string PngTransparent = @"..\..\VPT\test_pattern_transparent.png";
-		public const string Hdr = @"..\..\VPT\test_pattern_hdr.hdr";
+		public const string Exr = @"..\..\Fixtures~\comp_piz.exr";
+		public const string Bmp = @"..\..\Fixtures~\test_pattern.bmp";
+		public const string BmpArgb = @"..\..\Fixtures~\test_pattern_argb.bmp";
+		public const string BmpXrgb = @"..\..\Fixtures~\test_pattern_xrgb.bmp";
+		public const string Jpg = @"..\..\Fixtures~\test_pattern.jpg";
+		public const string Png = @"..\..\Fixtures~\test_pattern.png";
+		public const string PngTransparent = @"..\..\Fixtures~\test_pattern_transparent.png";
+		public const string Hdr = @"..\..\Fixtures~\test_pattern_hdr.hdr";
 	}
 }
