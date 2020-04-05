f2e9838cd5cb95f1227340f55d70719145279324
freezy <freezy@kodi.tv>
2020-04-05 15:24:14 +0200

    unity: Add base physics system groups.

--- VisualPinball.Unity/VisualPinball.Unity/Game/VisualPinballSimulationSystemGroup.cs
+++ VisualPinball.Unity/VisualPinball.Unity/Game/VisualPinballSimulationSystemGroup.cs
@@ -1,23 +1,47 @@
 ﻿using Unity.Entities;
 using Unity.Transforms;
+using VisualPinball.Engine.Math;
+using VisualPinball.Unity.Physics;
 
 namespace VisualPinball.Unity.Game
 {
+	/// <summary>
+	/// Main physics simulation system, executed once per frame.
+	/// </summary>
 	[UpdateBefore(typeof(TransformSystemGroup))]
 	public class VisualPinballSimulationSystemGroup : ComponentSystemGroup
 	{
-		private const float FixedDeltaTime = 0.001f;
+		public double PhysicsDiffTime;
 
-		private bool _initialized; // doing it in OnCreate() results in an endless loop
+		private long _nextPhysicsFrameTime;
+		private long _currentPhysicsTime;
 
 		protected override void OnUpdate()
 		{
-			if (!_initialized) {
-				//FixedRateUtils.EnableFixedRateWithCatchUp(World.GetOrCreateSystem<SimulationSystemGroup>(), FixedDeltaTime);
-				//FixedRateUtils.EnableFixedRateWithCatchUp(this, FixedDeltaTime);
-				_initialized = true;
+			var initialTimeUsec = (long)(Time.ElapsedTime * 1000000);
+			var curPhysicsFrameTime = _currentPhysicsTime == 0
+				? (long) (initialTimeUsec - Time.DeltaTime * 1000000)
+				: _currentPhysicsTime;
+
+			while (curPhysicsFrameTime < initialTimeUsec) {
+
+				PhysicsDiffTime = (_nextPhysicsFrameTime - curPhysicsFrameTime) * (1.0 / PhysicsConstants.DefaultStepTime);
+
+				// update velocities
+				World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<UpdateVelocitiesSystemGroup>().Update();
+
+				// simulate cycle
+				World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<PhysicsSimulateCycleSystemGroup>().Update();
+
+				// new cycle, on physics frame boundary
+				curPhysicsFrameTime = _nextPhysicsFrameTime;
+
+				// advance physics position
+				_nextPhysicsFrameTime += PhysicsConstants.PhysicsStepTime;
 			}
-			base.OnUpdate();
+
+			_currentPhysicsTime = curPhysicsFrameTime;
 		}
+
 	}
 }
--- VisualPinball.Unity/VisualPinball.Unity/VPT/Flipper/FlipperDisplacementSystem.cs
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/Flipper/FlipperDisplacementSystem.cs
@@ -1,12 +1,19 @@
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Jobs;
-using VisualPinball.Engine.Math;
-using VisualPinball.Unity.Game;
+using Unity.Mathematics;
+using VisualPinball.Unity.Physics;
 
 namespace VisualPinball.Unity.VPT.Flipper
 {
-	[UpdateInGroup(typeof(VisualPinballSimulationSystemGroup))]
+	public struct FlipperRotatedEvent
+	{
+		public bool Direction;
+		public float AngleSpeed;
+		public int EntityIndex;
+	}
+
+	[UpdateInGroup(typeof(UpdateDisplacementSystemGroup))]
 	public class FlipperDisplacementSystem : JobComponentSystem
 	{
 
@@ -17,60 +24,62 @@ namespace VisualPinball.Unity.VPT.Flipper
 
 			public void Execute(ref FlipperMovementData state, [ReadOnly] ref FlipperMaterialData data)
 			{
-				var dTime = DTime * PhysicsConstants.DefaultStepTime / PhysicsConstants.PhysicsStepTime;
-				// state.Angle += state.AngleSpeed * dTime; // move flipper angle
-				//
-				// var angleMin = math.min(data.AngleStart, data.AngleEnd);
-				// var angleMax = math.max(data.AngleStart, data.AngleEnd);
-				//
-				// if (state.Angle > angleMax) {
-				// 	state.Angle = angleMax;
-				// }
-				//
-				// if (state.Angle < angleMin) {
-				// 	state.Angle = angleMin;
-				// }
+				var dTime = DTime;
+				state.Angle += state.AngleSpeed * dTime; // move flipper angle
 
-				// if (math.abs(state.AngleSpeed) < 0.0005f) {
-				// 	// avoids "jumping balls" when two or more balls held on flipper (and more other balls are in play) //!! make dependent on physics update rate
-				// 	return;
-				// }
-				//
-				// var handleEvent = false;
-				//
-				// if (state.Angle >= angleMax) {
-				// 	// hit stop?
-				// 	if (state.AngleSpeed > 0) {
-				// 		handleEvent = true;
-				// 	}
-				//
-				// } else if (state.Angle <= angleMin) {
-				// 	if (state.AngleSpeed < 0) {
-				// 		handleEvent = true;
-				// 	}
-				// }
+				var angleMin = math.min(data.AngleStart, data.AngleEnd);
+				var angleMax = math.max(data.AngleStart, data.AngleEnd);
 
-				// if (handleEvent) {
-				// 	var angleSpeed = math.abs(math.degrees(state.AngleSpeed));
-				// 	state.AngularMomentum *= -0.3f; // make configurable?
-				// 	state.AngleSpeed = state.AngularMomentum / data.Inertia;
-				//
-				// 	if (state.EnableRotateEvent > 0) {
-				// 		//_events.FireVoidEventParam(Event.LimitEventsEOS, angleSpeed); // send EOS event
-				//
-				// 	} else if (state.EnableRotateEvent < 0) {
-				// 		//_events.FireVoidEventParam(Event.LimitEventsBOS, angleSpeed); // send Beginning of Stroke/Park event
-				// 	}
-				//
-				// 	state.EnableRotateEvent = 0;
-				// }
+				if (state.Angle > angleMax) {
+					state.Angle = angleMax;
+				}
+
+				if (state.Angle < angleMin) {
+					state.Angle = angleMin;
+				}
+
+				if (math.abs(state.AngleSpeed) < 0.0005f) {
+					// avoids "jumping balls" when two or more balls held on flipper (and more other balls are in play) //!! make dependent on physics update rate
+					return;
+				}
+
+				var handleEvent = false;
+
+				if (state.Angle >= angleMax) {
+					// hit stop?
+					if (state.AngleSpeed > 0) {
+						handleEvent = true;
+					}
+
+				} else if (state.Angle <= angleMin) {
+					if (state.AngleSpeed < 0) {
+						handleEvent = true;
+					}
+				}
+
+				if (handleEvent) {
+					var angleSpeed = math.abs(math.degrees(state.AngleSpeed));
+					state.AngularMomentum *= -0.3f; // make configurable?
+					state.AngleSpeed = state.AngularMomentum / data.Inertia;
+
+					if (state.EnableRotateEvent > 0) {
+						//_events.FireVoidEventParam(Event.LimitEventsEOS, angleSpeed); // send EOS event
+
+					} else if (state.EnableRotateEvent < 0) {
+						//_events.FireVoidEventParam(Event.LimitEventsBOS, angleSpeed); // send Beginning of Stroke/Park event
+					}
+
+					state.EnableRotateEvent = 0;
+				}
 			}
 		}
 
 		protected override JobHandle OnUpdate(JobHandle inputDeps)
 		{
+			var sim = World.DefaultGameObjectInjectionWorld.GetExistingSystem<PhysicsSimulateCycleSystemGroup>();
+
 			var flipperDisplacementJob = new FlipperDisplacement {
-				DTime = Time.DeltaTime
+				DTime = (float)sim.DTime
 			};
 			return flipperDisplacementJob.Schedule(this, inputDeps);
 		}
