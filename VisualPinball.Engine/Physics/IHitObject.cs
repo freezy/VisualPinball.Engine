45428b8678ea92dbfcca1a604cd7575a172f5f0c
freezy <freezy@kodi.tv>
2020-04-08 23:58:16 +0200

    unity: Add hit object storage POC.

--- VisualPinball.Engine/Physics/IHitObject.cs
+++ VisualPinball.Engine/Physics/IHitObject.cs
@@ -1,9 +1,12 @@
-﻿using VisualPinball.Engine.Math;
+﻿using VisualPinball.Engine.Game;
+using VisualPinball.Engine.Math;
+using VisualPinball.Engine.VPT.Ball;
 
 namespace VisualPinball.Engine.Physics
 {
 	public interface IHitObject
 	{
 		Rect3D HitBBox { get; }
+		void DoHitTest(Ball ball, CollisionEvent coll, PlayerPhysics physics);
 	}
 }
