e2a7a32eb3bcc4ebd4b2b8b8a199bd980d04bdf0
freezy <freezy@kodi.tv>
2020-04-15 00:13:52 +0200

    unity: Create correct quad tree on game start.

--- VisualPinball.Unity/VisualPinball.Unity/Physics/HitTest/BallBroadPhaseSystem.cs
+++ VisualPinball.Unity/VisualPinball.Unity/Physics/HitTest/BallBroadPhaseSystem.cs
@@ -1,18 +1,21 @@
 ﻿using Unity.Entities;
 using Unity.Jobs;
-using VisualPinball.Engine.Physics;
+using VisualPinball.Unity.Physics.Collider;
 using VisualPinball.Unity.VPT.Ball;
 
 namespace VisualPinball.Unity.Physics.HitTest
 {
 	public class BallBroadPhaseSystem : JobComponentSystem
 	{
-		public HitQuadTree KdTree;
+		public BlobAssetReference<QuadTree> QuadTree;
 
 		protected override JobHandle OnUpdate(JobHandle inputDeps)
 		{
+			var quadTree = QuadTree;
 			return Entities.ForEach((ref BallData ballData) => {
+				if (quadTree.Value.Center.x > 0) {
 
+				}
 			}).Schedule(inputDeps);
 		}
 	}
