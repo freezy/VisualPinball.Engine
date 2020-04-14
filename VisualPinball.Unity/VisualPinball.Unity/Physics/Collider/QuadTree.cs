bc775bdf6378b319c62bbdb1221c0b13b6250593
freezy <freezy@kodi.tv>
2020-04-15 00:04:32 +0200

    style: Move quad tree creation into struct.

--- VisualPinball.Unity/VisualPinball.Unity/Physics/Collider/QuadTree.cs
+++ VisualPinball.Unity/VisualPinball.Unity/Physics/Collider/QuadTree.cs
@@ -1,4 +1,5 @@
-﻿using Unity.Entities;
+﻿using Unity.Collections;
+using Unity.Entities;
 using Unity.Mathematics;
 using VisualPinball.Engine.Physics;
 using VisualPinball.Unity.Extensions;
@@ -12,26 +13,31 @@ namespace VisualPinball.Unity.Physics.Collider
 		public float3 Center;
 		public bool IsLeaf;
 
-		public static void Create(HitQuadTree src, ref QuadTree dest, BlobBuilder builder)
+		public static BlobAssetReference<QuadTree> CreateBlobAssetReference(HitQuadTree hitQuadTree)
+		{
+			using (var blobBuilder = new BlobBuilder(Allocator.Temp)) {
+				ref var rootQuadTree = ref blobBuilder.ConstructRoot<QuadTree>();
+				Create(hitQuadTree, ref rootQuadTree, blobBuilder);
+				return blobBuilder.CreateBlobAssetReference<QuadTree>(Allocator.Persistent);
+			}
+		}
+
+		private static void Create(HitQuadTree src, ref QuadTree dest, BlobBuilder builder)
 		{
 			var children = builder.Allocate(ref dest.Children, 4);
 			for (var i = 0; i < 4; i++) {
 				if (src.Children[i] != null) {
-					Create(src.Children[i], ref children[i], builder);
+					Create(src.Children[i], ref children[i].Value, builder);
 				}
 			}
-			dest.Center = src.Center.ToUnityFloat3();
-			dest.IsLeaf = src.IsLeaf;
 
 			var colliders = builder.Allocate(ref dest.HitObjects, src.HitObjects.Count);
 			for (var i = 0; i < src.HitObjects.Count; i++) {
 				Collider.Create(src.HitObjects[i], ref colliders[i], builder);
 			}
-		}
 
-		private static void Create(HitQuadTree src, ref BlobPtr<QuadTree> dest, BlobBuilder builder)
-		{
-			Create(src, ref dest.Value, builder);
+			dest.Center = src.Center.ToUnityFloat3();
+			dest.IsLeaf = src.IsLeaf;
 		}
 	}
 }
