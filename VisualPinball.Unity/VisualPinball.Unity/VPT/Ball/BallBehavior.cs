66b93d000a938b5d4ce7346440b975b4272c5cf1
freezy <freezy@kodi.tv>
2020-04-16 00:23:22 +0200

    unity: Properly pass collision data to system.

---
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/Ball/BallBehavior.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace VisualPinball.Unity.VPT.Ball
+{
+	public class BallBehavior : MonoBehaviour, IConvertGameObjectToEntity
+	{
+		public float3 Position;
+		public float Radius;
+		public float Mass;
+
+		public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+		{
+			dstManager.AddComponentData(entity, new BallData {
+				IsFrozen = false,
+				Position = Position,
+				Radius = Radius,
+				Velocity = new float3(0, 0, 0)
+			});
+		}
+	}
+}
