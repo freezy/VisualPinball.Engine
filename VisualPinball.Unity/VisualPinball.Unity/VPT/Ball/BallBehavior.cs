4e56ff4b641437c5328d041c71251da584aafa42
freezy <freezy@kodi.tv>
2020-05-23 01:46:01 +0200

    unity: Create ball entity without IConvertGameObjectToEntity.

--- VisualPinball.Unity/VisualPinball.Unity/VPT/Ball/BallBehavior.cs
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/Ball/BallBehavior.cs
@@ -1,7 +1,11 @@
 ﻿using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
 using UnityEngine;
+using VisualPinball.Unity.Game;
 using VisualPinball.Unity.Physics.Collision;
+using VisualPinball.Unity.Physics.SystemGroup;
 
 namespace VisualPinball.Unity.VPT.Ball
 {
@@ -25,24 +29,79 @@ namespace VisualPinball.Unity.VPT.Ball
 				Velocity = Velocity,
 				Orientation = float3x3.identity
 			});
-			 dstManager.AddComponentData(entity, new CollisionEventData {
+			dstManager.AddComponentData(entity, new CollisionEventData {
 				HitTime = -1,
 				HitDistance = 0,
 				HitFlag = false,
 				IsContact = false,
 				HitNormal = new float3(0, 0, 0),
 			});
-			// dstManager.AddComponentData(entity, new VisualPinball.Unity.Physics.Collider.Collider {
-			// 	Header = new ColliderHeader {
-			// 		Type = ColliderType.None,
-			// 		Aabb = new Aabb(),
-			// 		EntityIndex = -1
-			// 	}
-			// });
 			dstManager.AddBuffer<OverlappingStaticColliderBufferElement>(entity);
 			dstManager.AddBuffer<OverlappingDynamicBufferElement>(entity);
 			dstManager.AddBuffer<ContactBufferElement>(entity);
 			dstManager.AddBuffer<BallInsideOfBufferElement>(entity);
+
+			dstManager.World.GetOrCreateSystem<VisualPinballSimulationSystemGroup>().NumBalls++;
+		}
+
+		public static void CreateEntity(EntityManager entityManager, Mesh mesh, Material material,
+			float3 worldPos, float scale, float3 localPos, float3 velocity, float radius, float mass)
+		{
+			var startSimulationEntityCommandBufferSystem = World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<StartSimulationEntityCommandBufferSystem>();
+			var ecb = startSimulationEntityCommandBufferSystem.CreateCommandBuffer();
+
+			var archetype = entityManager.CreateArchetype(
+				typeof(Translation),
+				typeof(Scale),
+				typeof(Rotation),
+				typeof(RenderMesh),
+				typeof(LocalToWorld),
+				typeof(RenderBounds),
+				typeof(BallData),
+				typeof(CollisionEventData),
+				typeof(OverlappingStaticColliderBufferElement),
+				typeof(OverlappingDynamicBufferElement),
+				typeof(ContactBufferElement),
+				typeof(BallInsideOfBufferElement)
+			);
+			var entity = ecb.CreateEntity(archetype);
+
+			ecb.SetSharedComponent(entity, new RenderMesh {
+				mesh = mesh,
+				material = material
+			});
+
+			ecb.SetComponent(entity, new RenderBounds {
+				Value = mesh.bounds.ToAABB()
+			});
+
+			ecb.SetComponent(entity, new Translation {
+				Value = worldPos
+			});
+
+			ecb.SetComponent(entity, new Scale {
+				Value = scale
+			});
+
+			ecb.SetComponent(entity, new BallData {
+				Id = _id++,
+				IsFrozen = false,
+				Position = localPos,
+				Radius = radius,
+				Mass = mass,
+				Velocity = velocity,
+				Orientation = float3x3.identity
+			});
+
+			ecb.SetComponent(entity, new CollisionEventData {
+				HitTime = -1,
+				HitDistance = 0,
+				HitFlag = false,
+				IsContact = false,
+				HitNormal = new float3(0, 0, 0),
+			});
+
+			entityManager.World.GetOrCreateSystem<VisualPinballSimulationSystemGroup>().NumBalls++;
 		}
 	}
 }
