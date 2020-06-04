2362289ce2f51e94494a9ef0e54dea7e18bf8624
freezy <freezy@kodi.tv>
2020-06-05 00:21:34 +0200

    gate: Convert to entity on import.

---
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/Gate/GateExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Entities;
+using UnityEngine;
+using VisualPinball.Engine.Game;
+using VisualPinball.Engine.VPT.Flipper;
+using VisualPinball.Unity.Extensions;
+
+namespace VisualPinball.Unity.VPT.Gate
+{
+	public static class GateExtensions
+	{
+		public static GateBehavior SetupGameObject(this Engine.VPT.Gate.Gate gate, GameObject obj, RenderObjectGroup rog)
+		{
+			var ic = obj.AddComponent<GateBehavior>().SetData(gate.Data);
+			obj.AddComponent<ConvertToEntity>();
+			return ic as GateBehavior;
+		}
+	}
+}
