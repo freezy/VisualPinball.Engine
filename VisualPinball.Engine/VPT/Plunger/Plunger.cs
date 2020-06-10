129f1d5d2d1a991d63a1e7841fc2729a49344b25
freezy <freezy@kodi.tv>
2020-06-10 21:16:18 +0200

    plunger: Setup hit object.

--- VisualPinball.Engine/VPT/Plunger/Plunger.cs
+++ VisualPinball.Engine/VPT/Plunger/Plunger.cs
@@ -1,11 +1,17 @@
 using System.IO;
 using VisualPinball.Engine.Game;
+using VisualPinball.Engine.Physics;
 
 namespace VisualPinball.Engine.VPT.Plunger
 {
-	public class Plunger : Item<PlungerData>, IRenderable
+	public class Plunger : Item<PlungerData>, IRenderable, IHittable
 	{
+		public bool IsCollidable => true;
+		public EventProxy EventProxy { get; private set; }
+
+		public const float PlungerHeight = 50.0f;
 		private readonly PlungerMeshGenerator _meshGenerator;
+		private HitObject[] _hitObjects;
 
 		public Plunger(PlungerData data) : base(data)
 		{
@@ -14,9 +20,18 @@ namespace VisualPinball.Engine.VPT.Plunger
 
 		public Plunger(BinaryReader reader, string itemName) : this(new PlungerData(reader, itemName)) { }
 
+		public void SetupPlayer(Player player, Table.Table table)
+		{
+			var zHeight = table.GetSurfaceHeight(Data.Surface, Data.Center.X, Data.Center.Y);
+			EventProxy = new EventProxy(this);
+			_hitObjects = new HitObject[] {new PlungerHit(Data, zHeight)};
+		}
+
 		public RenderObjectGroup GetRenderObjects(Table.Table table, Origin origin = Origin.Global, bool asRightHanded = true)
 		{
 			return _meshGenerator.GetRenderObjects(20, table, origin, asRightHanded);
 		}
+
+		public HitObject[] GetHitShapes() => _hitObjects;
 	}
 }
