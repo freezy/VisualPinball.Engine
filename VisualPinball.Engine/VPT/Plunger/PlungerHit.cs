c0eca582769a2b74b52bbd0a504494a9926086fb
freezy <freezy@kodi.tv>
2020-06-11 00:20:12 +0200

    plunger: Wire up data.

--- VisualPinball.Engine/VPT/Plunger/PlungerHit.cs
+++ VisualPinball.Engine/VPT/Plunger/PlungerHit.cs
@@ -11,6 +11,8 @@ namespace VisualPinball.Engine.VPT.Plunger
 		public readonly LineSeg[] LineSegSide = new LineSeg[2];
 		public readonly HitLineZ[] JointBase = new HitLineZ[2];
 		public readonly HitLineZ[] JointEnd  = new HitLineZ[2];
+		public readonly float Position;
+		public readonly float FrameTop;
 
 		private readonly PlungerData _data;
 		private readonly float _zHeight;
@@ -26,9 +28,24 @@ namespace VisualPinball.Engine.VPT.Plunger
 			_y = _data.Center.Y + _data.Height;
 			_x2 = _data.Center.X + _data.Width;
 
+			FrameTop = data.Center.Y - data.Stroke;
+			var frameBottom = data.Center.Y;
+			var frameLen = frameBottom - FrameTop;
+			var restPos = data.ParkPosition;
+
+			Position = FrameTop + restPos * frameLen;
+
+			// static
 			LineSegBase = new LineSeg(new Vertex2D(_x, _y), new Vertex2D(_x2, _y), zHeight, zHeight + Plunger.PlungerHeight);
 			JointBase[0] = new HitLineZ(new Vertex2D(_x, _y), zHeight, zHeight + Plunger.PlungerHeight);
 			JointBase[1] = new HitLineZ(new Vertex2D(_x2, _y), zHeight, zHeight + Plunger.PlungerHeight);
+
+			// dynamic
+			LineSegSide[0] = new LineSeg(new Vertex2D(_x + 0.0001f, Position), new Vertex2D(_x, _y), zHeight, zHeight + Plunger.PlungerHeight);
+			LineSegSide[1] = new LineSeg(new Vertex2D(_x2, _y), new Vertex2D(_x2 + 0.0001f, Position), zHeight, zHeight + Plunger.PlungerHeight);
+			LineSegEnd = new LineSeg(new Vertex2D(_x2, Position), new Vertex2D(_x, Position), zHeight, zHeight + Plunger.PlungerHeight);
+			JointEnd[0] = new HitLineZ(new Vertex2D(_x, Position), zHeight, zHeight + Plunger.PlungerHeight);
+			JointEnd[1] = new HitLineZ(new Vertex2D(_x2, Position), zHeight, zHeight + Plunger.PlungerHeight);
 		}
 
 		public override void CalcHitBBox()
