609e7f3ad102593642644f9374a27fcf6913e66e
freezy <freezy@kodi.tv>
2020-06-17 00:24:04 +0200

    test: Setup Unity test project.

---
+++ VisualPinball.Unity/VisualPinball.Unity.Test/VPT/Bumper/BumperCollisionTests.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using VisualPinball.Engine.Test.Test;
+using VisualPinball.Engine.VPT.Table;
+
+namespace VisualPinball.Unity.Test.VPT.Bumper
+{
+	public class BumperCollisionTests
+	{
+		private Table _table;
+
+		[SetUp]
+		public void Setup()
+		{
+			_table = Table.Load(VpxPath.Bumper);
+		}
+
+		[Test]
+		public void TestSomething()
+		{
+			
+		}
+	}
+}
