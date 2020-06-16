f334b18287f9cb7a06dd74fa26c60597dd855bcc
freezy <freezy@kodi.tv>
2020-06-16 22:01:18 +0200

    plunger: Properly animate modern plunger.

--- VisualPinball.Unity/VisualPinball.Unity/VPT/Plunger/PlungerAnimationSystem.cs
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/Plunger/PlungerAnimationSystem.cs
@@ -1,6 +1,5 @@
 using Unity.Entities;
 using Unity.Profiling;
-using Unity.Rendering;
 using VisualPinball.Unity.Physics.SystemGroup;
 
 namespace VisualPinball.Unity.VPT.Plunger
@@ -21,22 +20,26 @@ namespace VisualPinball.Unity.VPT.Plunger
 			{
 				marker.Begin();
 
-				var rodAnimData = animationDatas[staticData.RodEntity];
-				var springAnimData = animationDatas[staticData.SpringEntity];
-
 				var frame0 = (int)((movementData.Position - staticData.FrameStart) / (staticData.FrameEnd - staticData.FrameStart) * (staticData.NumFrames - 1) + 0.5f);
 				var frame = frame0 < 0 ? 0 : frame0 >= staticData.NumFrames ? staticData.NumFrames - 1 : frame0;
 
-				if (rodAnimData.CurrentFrame != frame) {
-					rodAnimData.CurrentFrame = frame;
-					rodAnimData.IsDirty = true;
-					animationDatas[staticData.RodEntity] = rodAnimData;
+
+				if (animationDatas.Exists(staticData.RodEntity)) {
+					var rodAnimData = animationDatas[staticData.RodEntity];
+					if (rodAnimData.CurrentFrame != frame) {
+						rodAnimData.CurrentFrame = frame;
+						rodAnimData.IsDirty = true;
+						animationDatas[staticData.RodEntity] = rodAnimData;
+					}
 				}
 
-				if (springAnimData.CurrentFrame != frame) {
-					springAnimData.CurrentFrame = frame;
-					springAnimData.IsDirty = true;
-					animationDatas[staticData.SpringEntity] = springAnimData;
+				if (animationDatas.Exists(staticData.SpringEntity)) {
+					var springAnimData = animationDatas[staticData.SpringEntity];
+					if (springAnimData.CurrentFrame != frame) {
+						springAnimData.CurrentFrame = frame;
+						springAnimData.IsDirty = true;
+						animationDatas[staticData.SpringEntity] = springAnimData;
+					}
 				}
 
 				marker.End();
