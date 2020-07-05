77362c9dd89b3fc2be6a1d1c42f2ec54b852b98e
freezy <freezy@kodi.tv>
2020-07-05 13:35:16 +0200

    hittarget: Make it hittable.

---
+++ VisualPinball.Engine/VPT/HitTarget/HitTargetHitGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using VisualPinball.Engine.Game;
+using VisualPinball.Engine.Physics;
+
+namespace VisualPinball.Engine.VPT.HitTarget
+{
+	public class HitTargetHitGenerator
+	{
+		private readonly HitTargetData _data;
+
+		public HitTargetHitGenerator(HitTargetData data)
+		{
+			_data = data;
+		}
+
+		public HitObject[] GenerateHitObjects(Table.Table table, EventProxy eventProxy)
+		{
+			var hitObjects = new List<HitObject>();
+
+			return hitObjects.ToArray();
+		}
+	}
+}
