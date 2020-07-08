b5b98182958dff42b5102056ca2f3d0534f012d3
freezy <freezy@kodi.tv>
2020-07-09 00:01:32 +0200

    events: Document flipper API.

--- VisualPinball.Unity/VisualPinball.Unity/VPT/Flipper/FlipperApi.cs
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/Flipper/FlipperApi.cs
@@ -17,21 +17,48 @@ namespace VisualPinball.Unity.VPT.Flipper
 		/// </summary>
 		public event EventHandler Init;
 
+		/// <summary>
+		/// Event triggered when the flipper was touched by the ball, but did
+		/// not collide.
+		/// </summary>
 		public event EventHandler Hit;
+
+		/// <summary>
+		/// Event triggered when the flipper collided with the ball.
+		/// </summary>
 		public event EventHandler<CollideEventArgs> Collide;
+
+		/// <summary>
+		/// Event triggered when the flipper comes to rest, i.e. moves back to
+		/// the resting position.
+		/// </summary>
 		public event EventHandler<RotationEventArgs> LimitBos;
+
+		/// <summary>
+		/// Event triggered when the flipper reaches the end position.
+		/// </summary>
 		public event EventHandler<RotationEventArgs> LimitEos;
+
+		// todo
 		public event EventHandler Timer;
 
 		public FlipperApi(Engine.VPT.Flipper.Flipper flipper, Entity entity, Player player) : base(flipper, entity, player)
 		{
 		}
 
+		/// <summary>
+		/// Enables the flipper's solenoid, making the flipper to start moving
+		/// to its end position.
+		/// </summary>
 		public void RotateToEnd()
 		{
 			EngineProvider<IPhysicsEngine>.Get().FlipperRotateToEnd(Entity);
 		}
 
+		/// <summary>
+		/// Disables the flipper's solenoid, making the flipper rotate back to
+		/// its resting position.
+		/// </summary>
 		public void RotateToStart()
 		{
 			EngineProvider<IPhysicsEngine>.Get().FlipperRotateToStart(Entity);
@@ -66,13 +93,26 @@ namespace VisualPinball.Unity.VPT.Flipper
 		#endregion
 	}
 
+	/// <summary>
+	/// Event data when the flipper either reaches resting or end
+	/// position.
+	/// </summary>
 	public struct RotationEventArgs
 	{
+		/// <summary>
+		/// Angle speed with which the new position was reached.
+		/// </summary>
 		public float AngleSpeed;
 	}
 
+	/// <summary>
+	/// Event data when the ball collides with the flipper.
+	/// </summary>
 	public struct CollideEventArgs
 	{
+		/// <summary>
+		/// The relative normal velocity with which the flipper was hit.
+		/// </summary>
 		public float FlipperHit;
 	}
 }
