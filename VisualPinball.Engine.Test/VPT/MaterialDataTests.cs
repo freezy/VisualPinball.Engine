577d2626ed5866c52303b4c0c455667c0efdbebe
freezy <freezy@kodi.tv>
2020-07-11 00:38:39 +0200

    material: Make read test more precise.

--- VisualPinball.Engine.Test/VPT/MaterialDataTests.cs
+++ VisualPinball.Engine.Test/VPT/MaterialDataTests.cs
@@ -34,23 +34,20 @@ namespace VisualPinball.Engine.Test.VPT
 			material.ClearCoat.Green.Should().Be(51);
 			material.ClearCoat.Blue.Should().Be(52);
 			material.Edge.Should().Be(0.3874f);
-			//material.EdgeAlpha.Should().Be(0.1968504f);
-			material.EdgeAlpha.Should().BeInRange(0.1968503f, 0.1968505f); // comment out previous and watch what happens..
+			material.EdgeAlpha.Should().BeApproximately(0.1968504f, 0.00001f);
 			material.Elasticity.Should().Be(1.1f);
 			material.ElasticityFalloff.Should().Be(2.02f);
 			material.Friction.Should().Be(2.322f);
 			material.Glossiness.Red.Should().Be(255);
 			material.Glossiness.Green.Should().Be(158);
 			material.Glossiness.Blue.Should().Be(70);
-			//material.GlossyImageLerp.Should().Be(0.4078431f);
-			material.GlossyImageLerp.Should().BeInRange(0.4078430f, 0.4078432f);
+			material.GlossyImageLerp.Should().BeApproximately(0.4078431f, 0.00001f);
 			material.IsMetal.Should().Be(false);
 			material.IsOpacityActive.Should().Be(true);
 			material.Opacity.Should().Be(0.8183f);
 			material.Roughness.Should().Be(0.53628f);
 			material.ScatterAngle.Should().Be(7.4332f);
 			material.Thickness.Should().Be(0.654902f);
-			//material.Thickness.Should().BeInRange(0.04705881f, 0.04705883f);
 			material.WrapLighting.Should().Be(0.5492f);
 		}
 	}
