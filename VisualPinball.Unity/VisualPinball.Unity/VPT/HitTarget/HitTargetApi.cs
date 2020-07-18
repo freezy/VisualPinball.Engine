835dd4d5ed0bbf66726a0d126d3b8bed0b5783e3
freezy <freezy@kodi.tv>
2020-07-19 00:45:22 +0200

    hittarget: Add drop status to API.

--- VisualPinball.Unity/VisualPinball.Unity/VPT/HitTarget/HitTargetApi.cs
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/HitTarget/HitTargetApi.cs
@@ -17,8 +17,49 @@ namespace VisualPinball.Unity.VPT.HitTarget
 		/// </summary>
 		public event EventHandler Hit;
 
+		/// <summary>
+		/// Sets the status of a drop target.
+		/// </summary>
+		///
+		/// <remarks>
+		/// Setting this will animate the drop target to the desired position.
+		/// </remarks>
+		///
+		/// <exception cref="InvalidOperationException">Thrown if target is not a drop target (but a hit target, which can't be dropped)</exception>
+		public bool IsDropped {
+			get => EntityManager.GetComponentData<HitTargetAnimationData>(Entity).IsDropped;
+			set => SetIsDropped(value);
+		}
+
 		internal HitTargetApi(Engine.VPT.HitTarget.HitTarget item, Entity entity, Player player) : base(item, entity, player)
 		{
+		}
+
+		/// <summary>
+		///
+		/// </summary>
+		/// <param name="isDropped"></param>
+		/// <exception cref="InvalidOperationException"></exception>
+		private void SetIsDropped(bool isDropped)
+		{
+			if (!Item.Data.IsDropTarget) {
+				throw new InvalidOperationException($"You tried to drop hit target {Item.Name}, but only drop targets are droppable!");
+			}
+
+			var data = EntityManager.GetComponentData<HitTargetAnimationData>(Entity);
+			if (data.IsDropped != isDropped) {
+				data.MoveAnimation = true;
+				if (isDropped) {
+					data.MoveDown = true;
+
+				} else {
+					data.MoveDown = false;
+					data.TimeStamp = World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<VisualPinballSimulationSystemGroup>().TimeMsec;
+				}
+			} else {
+				data.IsDropped = isDropped;
+			}
+			EntityManager.SetComponentData(Entity, data);
 		}
 
 		#region Events
--- VisualPinball.Unity/VisualPinball.Unity/VPT/HitTarget/HitTargetMovementData.cs
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/HitTarget/HitTargetMovementData.cs
@@ -4,6 +4,8 @@ namespace VisualPinball.Unity.VPT.HitTarget
 {
 	public struct HitTargetMovementData : IComponentData
 	{
+		public const float DropTargetLimit = 52.0f;
+
 		public float ZOffset;
 		public float XRotation;
 	}
