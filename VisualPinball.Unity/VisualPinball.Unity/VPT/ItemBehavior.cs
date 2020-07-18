ff19272161b9a52bd58fc35948ac740d98b3e68d
Roger Bocksnick <roger.bocksnick@gmail.com>
2020-07-18 15:33:20 -0700

    fix build issues

--- VisualPinball.Unity/VisualPinball.Unity/VPT/ItemBehavior.cs
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/ItemBehavior.cs
@@ -116,7 +116,9 @@ namespace VisualPinball.Unity.VPT
 		protected void TryRenameField(string undoName, ref string field, string oldName, string newName)
 		{
 			if (field == oldName) {
+#if UNITY_EDITOR
 				Undo.RecordObject(this, undoName);
+#endif
 				field = newName;
 			}
 		}
