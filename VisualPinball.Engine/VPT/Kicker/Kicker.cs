d57521c5d618e248be636405518b3eaa0115ad07
freezy <freezy@kodi.tv>
2020-07-20 22:54:27 +0200

    kicker: Add hit object creation.

--- VisualPinball.Engine/VPT/Kicker/Kicker.cs
+++ VisualPinball.Engine/VPT/Kicker/Kicker.cs
@@ -2,14 +2,18 @@
 using VisualPinball.Engine.Common;
 using VisualPinball.Engine.Game;
 using VisualPinball.Engine.Math;
+using VisualPinball.Engine.Physics;
 
 namespace VisualPinball.Engine.VPT.Kicker
 {
-	public class Kicker : Item<KickerData>, IRenderable, IBallCreationPosition
+	public class Kicker : Item<KickerData>, IRenderable, IBallCreationPosition, IHittable
 	{
-		public string[] UsedMaterials => new string[] { Data.Material };
+		public bool IsCollidable => true;
+		public EventProxy EventProxy { get; private set; }
+		public string[] UsedMaterials => new[] { Data.Material };
 
 		private readonly KickerMeshGenerator _meshGenerator;
+		private KickerHit _hit;
 
 		public Kicker(KickerData data) : base(data)
 		{
@@ -18,9 +22,24 @@ namespace VisualPinball.Engine.VPT.Kicker
 
 		public Kicker(BinaryReader reader, string itemName) : this(new KickerData(reader, itemName)) { }
 
+
+		public void Init(Table.Table table)
+		{
+			var height = table.GetSurfaceHeight(Data.Surface, Data.Center.X, Data.Center.Y) * table.GetScaleZ();
+
+			// reduce the hit circle radius because only the inner circle of the kicker should start a hit event
+			var radius = Data.Radius * (Data.LegacyMode ? Data.FallThrough ? 0.75f : 0.6f : 1f);
+			_hit = new KickerHit(Data, radius, height, table); // height of kicker hit cylinder
+		}
+
 		public RenderObjectGroup GetRenderObjects(Table.Table table, Origin origin = Origin.Global, bool asRightHanded = true)
 		{
 			return _meshGenerator.GetRenderObjects(table, origin, asRightHanded);
+		}
+
+		public HitObject[] GetHitShapes()
+		{
+			return new HitObject[] {_hit};
 		}
 
 		public Vertex3D GetBallCreationPosition(Table.Table table)
