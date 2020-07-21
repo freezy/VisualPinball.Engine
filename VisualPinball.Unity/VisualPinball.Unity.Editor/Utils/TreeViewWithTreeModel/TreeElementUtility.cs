d97973d6817d0add67976f1e49cd4d5a18f3328f
VrôÕnsh <vroonsh@free.fr>
2020-07-21 17:54:06 +0200

    layers: integrate TreeViewWithModel framework from Unity
    - removed unit tests, will integrate them back after
    - update naming convention & comments to match VPE standard

---
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/Utils/TreeViewWithTreeModel/TreeElementUtility.cs
@@ -0,0 +1,198 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace VisualPinball.Unity.Editor.Utils.TreeViewWithTreeModel
+{
+
+	/// <summary>
+	/// TreeElementUtility and TreeElement are useful helper classes for backend tree data structures.
+	/// See tests at the bottom for examples of how to use.
+	/// </summary>
+	public static class TreeElementUtility
+	{
+		/// <summary>
+		/// Will flatten a hierarchical tree to a TreeElement sorted list
+		/// </summary>
+		/// <typeparam name="T">the TreeElement derived class you'll use for your TreeView</typeparam>
+		/// <param name="root">the root element of your tree structure</param>
+		/// <param name="result">the flat sorted list of TreeElement</param>
+		public static void TreeToList<T>(T root, IList<T> result) where T : TreeElement
+		{
+			if (result == null)
+				throw new NullReferenceException("The input 'IList<T> result' list is null");
+			result.Clear();
+
+			Stack<T> stack = new Stack<T>();
+			stack.Push(root);
+
+			while (stack.Count > 0)
+			{
+				T current = stack.Pop();
+				result.Add(current);
+
+				if (current.Children != null && current.Children.Count > 0)
+				{
+					for (int i = current.Children.Count - 1; i >= 0; i--)
+					{
+						stack.Push((T)current.Children[i]);
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Will contruct a hierarchical tree from a flat sorted list of TreeElements
+		/// </summary>
+		/// <typeparam name="T">the TreeElement derived class you'll use for your TreeView</typeparam>
+		/// <param name="list">a flattened list of TreeElement</param>
+		/// <returns>the root of the tree parsed from the list (always the first element).</returns>
+		/// <remarks>
+		/// Important: the first item and is required to have a depth value of -1. 
+		/// The rest of the items should have depth >= 0. 
+		/// </remarks>
+		public static T ListToTree<T>(IList<T> list) where T : TreeElement
+		{
+			// Validate input
+			ValidateDepthValues (list);
+
+			// Clear old states
+			foreach (var element in list)
+			{
+				element.Parent = null;
+				element.Children = null;
+			}
+
+			// Set child and parent references using depth info
+			for (int parentIndex = 0; parentIndex < list.Count; parentIndex++)
+			{
+				var parent = list[parentIndex];
+				bool alreadyHasValidChildren = parent.Children != null;
+				if (alreadyHasValidChildren)
+					continue;
+
+				int parentDepth = parent.Depth;
+				int childCount = 0;
+
+				// Count children based depth value, we are looking at children until it's the same depth as this object
+				for (int i = parentIndex + 1; i < list.Count; i++)
+				{
+					if (list[i].Depth == parentDepth + 1)
+						childCount++;
+					if (list[i].Depth <= parentDepth)
+						break;
+				}
+
+				// Fill child array
+				List<TreeElement> childList = null;
+				if (childCount != 0)
+				{
+					childList = new List<TreeElement>(childCount); // Allocate once
+					childCount = 0;
+					for (int i = parentIndex + 1; i < list.Count; i++)
+					{
+						if (list[i].Depth == parentDepth + 1)
+						{
+							list[i].Parent = parent;
+							childList.Add(list[i]);
+							childCount++;
+						}
+
+						if (list[i].Depth <= parentDepth)
+							break;
+					}
+				}
+
+				parent.Children = childList;
+			}
+
+			return list[0];
+		}
+
+		/// <summary>
+		/// Will check all depth values in a flatten items list
+		/// </summary>
+		/// <param name="list">a flattened list of TreeElement</param>
+		public static void ValidateDepthValues<T>(IList<T> list) where T : TreeElement
+		{
+			if (list.Count == 0)
+				throw new ArgumentException("list should have items, count is 0, check before calling ValidateDepthValues", "list");
+
+			if (list[0].Depth != -1)
+				throw new ArgumentException("list item at index 0 should have a depth of -1 (since this should be the hidden root of the tree). Depth is: " + list[0].Depth, "list");
+
+			for (int i = 0; i < list.Count - 1; i++)
+			{
+				int depth = list[i].Depth;
+				int nextDepth = list[i + 1].Depth;
+				if (nextDepth > depth && nextDepth - depth > 1)
+					throw new ArgumentException(string.Format("Invalid depth info in input list. Depth cannot increase more than 1 per row. Index {0} has depth {1} while index {2} has depth {3}", i, depth, i + 1, nextDepth));
+			}
+
+			for (int i = 1; i < list.Count; ++i)
+				if (list[i].Depth < 0)
+					throw new ArgumentException("Invalid depth value for item at index " + i + ". Only the first item (the root) should have depth below 0.");
+
+			if (list.Count > 1 && list[1].Depth != 0)
+				throw new ArgumentException("Input list item at index 1 is assumed to have a depth of 0", "list");
+		}
+
+
+		/// <summary>
+		/// Will recompute depth values in a tree structure
+		/// </summary>
+		/// <param name="root">the root element of your tree structure</param>
+		public static void UpdateDepthValues<T>(T root) where T : TreeElement
+		{
+			if (root == null)
+				throw new ArgumentNullException("root", "The root is null");
+
+			if (!root.HasChildren)
+				return;
+
+			Stack<TreeElement> stack = new Stack<TreeElement>();
+			stack.Push(root);
+			while (stack.Count > 0)
+			{
+				TreeElement current = stack.Pop();
+				if (current.Children != null)
+				{
+					foreach (var child in current.Children)
+					{
+						child.Depth = current.Depth + 1;
+						stack.Push(child);
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Will check if child item has an ancestor in a TreeElement list
+		/// </summary>
+		/// <param name="child">the child item to check</param>
+		/// <param name="elements">potential ancestors list</param>
+		/// <returns>true if one of the list's element is an ancestor of child</returns>
+		static bool IsChildOf<T>(T child, IList<T> elements) where T : TreeElement
+		{
+			while (child != null)
+			{
+				child = (T)child.Parent;
+				if (elements.Contains(child))
+					return true;
+			}
+			return false;
+		}
+
+		public static IList<T> FindCommonAncestorsWithinList<T>(IList<T> elements) where T : TreeElement
+		{
+			if (elements.Count == 1)
+				return new List<T>(elements);
+
+			List<T> result = new List<T>(elements);
+			result.RemoveAll(g => IsChildOf(g, elements));
+			return result;
+		}
+	}
+
+}
---
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/Utils/TreeViewWithTreeModel/TreeViewWithTreeModel.cs
@@ -0,0 +1,249 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+
+namespace VisualPinball.Unity.Editor.Utils.TreeViewWithTreeModel
+{
+	/// <summary>
+	/// TreeViewItem class which will be used by your TreeViewWithTreeModel using the same TreeElement generic provided by your TreeModel
+	/// </summary>
+	/// <typeparam name="T">a TreeElement generic class</typeparam>
+	internal class TreeViewItem<T> : TreeViewItem where T : TreeElement
+	{
+		public T Data { get; set; }
+
+		public TreeViewItem (int id, int depth, string displayName, T data) : base (id, depth, displayName)
+		{
+			this.Data = data;
+		}
+	}
+
+	/// <summary>
+	/// The TreeWiewWithTreeModel class is a TreeView which will display a provided TreeModel
+	/// </summary>
+	/// <typeparam name="T">a TreeElement generic class</typeparam>
+	internal class TreeViewWithTreeModel<T> : TreeView where T : TreeElement
+	{
+		TreeModel<T> _treeModel;
+		readonly List<TreeViewItem> _rows = new List<TreeViewItem>(100);
+		public event Action treeChanged;
+
+		public TreeModel<T> TreeModel { get { return _treeModel; } }
+		public event Action<IList<TreeViewItem>>  beforeDroppingDraggedItems;
+
+
+		public TreeViewWithTreeModel (TreeViewState state, TreeModel<T> model) : base (state)
+		{
+			Init (model);
+		}
+
+		public TreeViewWithTreeModel (TreeViewState state, MultiColumnHeader multiColumnHeader, TreeModel<T> model)
+			: base(state, multiColumnHeader)
+		{
+			Init (model);
+		}
+
+		void Init (TreeModel<T> model)
+		{
+			_treeModel = model;
+			_treeModel.modelChanged += ModelChanged;
+		}
+
+		void ModelChanged ()
+		{
+			if (treeChanged != null)
+				treeChanged ();
+
+			Reload ();
+		}
+
+		protected override TreeViewItem BuildRoot()
+		{
+			int depthForHiddenRoot = -1;
+			return new TreeViewItem<T>(_treeModel.Root.Id, depthForHiddenRoot, _treeModel.Root.Name, _treeModel.Root);
+		}
+
+		protected override IList<TreeViewItem> BuildRows (TreeViewItem root)
+		{
+			if (_treeModel.Root == null)
+			{
+				Debug.LogError ("tree model root is null. did you call SetData()?");
+			}
+
+			_rows.Clear ();
+			if (!string.IsNullOrEmpty(searchString))
+			{
+				Search (_treeModel.Root, searchString, _rows);
+			}
+			else
+			{
+				if (_treeModel.Root.HasChildren)
+					AddChildrenRecursive(_treeModel.Root, 0, _rows);
+			}
+
+			// We still need to setup the child parent information for the rows since this 
+			// information is used by the TreeView internal logic (navigation, dragging etc)
+			SetupParentsAndChildrenFromDepths (root, _rows);
+
+			return _rows;
+		}
+
+		void AddChildrenRecursive (T parent, int depth, IList<TreeViewItem> newRows)
+		{
+			foreach (T child in parent.Children)
+			{
+				var item = new TreeViewItem<T>(child.Id, depth, child.Name, child);
+				newRows.Add(item);
+
+				if (child.HasChildren)
+				{
+					if (IsExpanded(child.Id))
+					{
+						AddChildrenRecursive (child, depth + 1, newRows);
+					}
+					else
+					{
+						item.children = CreateChildListForCollapsedParent();
+					}
+				}
+			}
+		}
+
+		void Search(T searchFromThis, string search, List<TreeViewItem> result)
+		{
+			if (string.IsNullOrEmpty(search))
+				throw new ArgumentException("Invalid search: cannot be null or empty", "search");
+
+			const int kItemDepth = 0; // tree is flattened when searching
+
+			Stack<T> stack = new Stack<T>();
+			foreach (var element in searchFromThis.Children)
+				stack.Push((T)element);
+			while (stack.Count > 0)
+			{
+				T current = stack.Pop();
+				// Matches search?
+				if (current.Name.IndexOf(search, StringComparison.OrdinalIgnoreCase) >= 0)
+				{
+					result.Add(new TreeViewItem<T>(current.Id, kItemDepth, current.Name, current));
+				}
+
+				if (current.Children != null && current.Children.Count > 0)
+				{
+					foreach (var element in current.Children)
+					{
+						stack.Push((T)element);
+					}
+				}
+			}
+			SortSearchResult(result);
+		}
+
+		protected virtual void SortSearchResult (List<TreeViewItem> rows)
+		{
+			rows.Sort ((x,y) => EditorUtility.NaturalCompare (x.displayName, y.displayName)); // sort by displayName by default, can be overriden for multicolumn solutions
+		}
+	
+		protected override IList<int> GetAncestors (int id)
+		{
+			return _treeModel.GetAncestors(id);
+		}
+
+		protected override IList<int> GetDescendantsThatHaveChildren (int id)
+		{
+			return _treeModel.GetDescendantsThatHaveChildren(id);
+		}
+
+
+		// Dragging
+		//-----------
+	
+		const string k_GenericDragID = "GenericDragColumnDragging";
+
+		protected override bool CanStartDrag (CanStartDragArgs args)
+		{
+			return true;
+		}
+
+		protected override void SetupDragAndDrop(SetupDragAndDropArgs args)
+		{
+			if (hasSearch)
+				return;
+
+			DragAndDrop.PrepareStartDrag();
+			var draggedRows = GetRows().Where(item => args.draggedItemIDs.Contains(item.id)).ToList();
+			DragAndDrop.SetGenericData(k_GenericDragID, draggedRows);
+			DragAndDrop.objectReferences = new UnityEngine.Object[] { }; // this IS required for dragging to work
+			string title = draggedRows.Count == 1 ? draggedRows[0].displayName : "< Multiple >";
+			DragAndDrop.StartDrag (title);
+		}
+
+		protected override DragAndDropVisualMode HandleDragAndDrop (DragAndDropArgs args)
+		{
+			// Check if we can handle the current drag data (could be dragged in from other areas/windows in the editor)
+			var draggedRows = DragAndDrop.GetGenericData(k_GenericDragID) as List<TreeViewItem>;
+			if (draggedRows == null)
+				return DragAndDropVisualMode.None;
+
+			// Parent item is null when dragging outside any tree view items.
+			switch (args.dragAndDropPosition)
+			{
+				case DragAndDropPosition.UponItem:
+				case DragAndDropPosition.BetweenItems:
+					{
+						bool validDrag = ValidDrag(args.parentItem, draggedRows);
+						if (args.performDrop && validDrag)
+						{
+							T parentData = ((TreeViewItem<T>)args.parentItem).Data;
+							OnDropDraggedElementsAtIndex(draggedRows, parentData, args.insertAtIndex == -1 ? 0 : args.insertAtIndex);
+						}
+						return validDrag ? DragAndDropVisualMode.Move : DragAndDropVisualMode.None;
+					}
+
+				case DragAndDropPosition.OutsideItems:
+					{
+						if (args.performDrop)
+							OnDropDraggedElementsAtIndex(draggedRows, _treeModel.Root, _treeModel.Root.Children.Count);
+
+						return DragAndDropVisualMode.Move;
+					}
+				default:
+					Debug.LogError("Unhandled enum " + args.dragAndDropPosition);
+					return DragAndDropVisualMode.None;
+			}
+		}
+
+		public virtual void OnDropDraggedElementsAtIndex (List<TreeViewItem> draggedRows, T parent, int insertIndex)
+		{
+			if (beforeDroppingDraggedItems != null)
+				beforeDroppingDraggedItems (draggedRows);
+
+			var draggedElements = new List<TreeElement> ();
+			foreach (var x in draggedRows)
+				draggedElements.Add (((TreeViewItem<T>) x).Data);
+		
+			var selectedIDs = draggedElements.Select (x => x.Id).ToArray();
+			_treeModel.MoveElements (parent, insertIndex, draggedElements);
+			SetSelection(selectedIDs, TreeViewSelectionOptions.RevealAndFrame);
+		}
+
+
+		bool ValidDrag(TreeViewItem parent, List<TreeViewItem> draggedItems)
+		{
+			TreeViewItem currentParent = parent;
+			while (currentParent != null)
+			{
+				if (draggedItems.Contains(currentParent))
+					return false;
+				currentParent = currentParent.parent;
+			}
+			return true;
+		}
+	
+	}
+
+}
