458fa8fd8ca3f56a33267e11171fcc93e21436ed
Roger Bocksnick <roger.bocksnick@gmail.com>
2020-07-26 12:38:39 -0700

    make generic manager window classes to simplify adding more of these windows

---
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/Managers/ImageManager.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using VisualPinball.Unity.VPT;
+
+namespace VisualPinball.Unity.Editor.Managers
+{
+	/// <summary>
+	/// Editor UI for VPX images, equivalent to VPX's "Image Manager" window
+	/// </summary>
+	public class ImageEditor : ManagerWindow<ImageListData>
+	{
+		[MenuItem("Visual Pinball/Image Manager", false, 102)]
+		public static void ShowWindow()
+		{
+			GetWindow<ImageEditor>("Image Manager");
+		}
+
+
+		protected override void OnItemDetailGUI()
+		{
+			GUILayout.Button("Test");
+		}
+
+		protected override void RenameExistingItem(ImageListData listItem, string newName)
+		{
+			string oldName = listItem.Texture.Name;
+
+			// give each editable item a chance to update its fields
+			string undoName = "Rename Material";
+			foreach (var item in _table.GetComponentsInChildren<IEditableItemBehavior>()) {
+				item.HandleTextureRenamed(undoName, oldName, newName);
+			}
+			Undo.RecordObject(_table, undoName);
+
+			listItem.Texture.Name = newName;
+		}
+
+		protected override void CollectData(List<ImageListData> data)
+		{
+			foreach (var t in _table.Item.Textures) {
+				data.Add(new ImageListData { Texture = t.Value });
+			}
+		}
+	}
+}
---
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/Managers/ManagerListView.cs
@@ -0,0 +1,200 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
+
+namespace VisualPinball.Unity.Editor.Managers
+{
+	/// <summary>
+	/// Wraps Unity's TreeView to make a simplified generic (1-dimensional) list view type control
+	/// for use by the manager windows
+	/// </summary>
+	/// <typeparam name="T">class of type IManagerListData that represents the data being edited</typeparam>
+	public class ManagerListView<T> : TreeView where T: IManagerListData
+	{
+		public event Action<List<T>> ItemSelected;
+
+		private List<T> _data = new List<T>();
+		private List<MemberInfo> _columnMemberInfos = new List<MemberInfo>();
+
+		public ManagerListView(TreeViewState treeViewState, IEnumerable<T> data, Action<List<T>> itemSelected) : base(treeViewState)
+		{
+			ItemSelected += itemSelected;
+
+			var columns = new List<MultiColumnHeaderState.Column>();
+
+			// collect up all column attribute flagged fields and properties, then cache the associated member info
+			// and build up our column array for the list view
+			foreach (var member in typeof(T).GetMembers(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)) {
+				if (member.MemberType == MemberTypes.Field || member.MemberType == MemberTypes.Property) {
+					var columnAttr = member.GetCustomAttribute<ManagerListColumnAttribute>();
+					if (columnAttr != null) {
+						_columnMemberInfos.Add(member);
+						columns.Add(new MultiColumnHeaderState.Column {
+							headerContent = new GUIContent(columnAttr.HeaderName ?? member.Name),
+							headerTextAlignment = columnAttr.HeaderAlignment,
+							canSort = true,
+							sortedAscending = true,
+							sortingArrowAlignment = TextAlignment.Right,
+							width = columnAttr.Width,
+							minWidth = columnAttr.Width * 0.5f,
+							maxWidth = float.MaxValue,
+							autoResize = true,
+							allowToggleVisibility = false,
+						});
+					}
+				}
+			}
+
+			var headerState = new MultiColumnHeaderState(columns.ToArray());
+			this.multiColumnHeader = new MultiColumnHeader(headerState);
+			this.multiColumnHeader.SetSorting(0, true);
+			this.multiColumnHeader.sortingChanged += SortingChanged;
+			this.showAlternatingRowBackgrounds = true;
+			this.showBorder = true;
+
+			SetData(data);
+			if (GetRows().Count > 0) {
+				SetSelection(new List<int> { 0 }, TreeViewSelectionOptions.FireSelectionChanged);
+			}
+		}
+
+		public void SetData(IEnumerable<T> data)
+		{
+			_data.Clear();
+			if (data != null) {
+				_data.AddRange(data);
+			}
+			Reload();
+		}
+
+		public void SelectItemWithName(string name)
+		{
+			var rows = GetRows();
+			foreach (var row in rows) {
+				if ((row as RowData).Data.Name.ToLower() == name.ToLower()) {
+					SetSelection(new List<int> { row.id }, TreeViewSelectionOptions.FireSelectionChanged);
+					return;
+				}
+			}
+			SetSelection(new List<int>(), TreeViewSelectionOptions.FireSelectionChanged);
+		}
+
+		protected override TreeViewItem BuildRoot()
+		{
+			return new TreeViewItem { id = -1, depth = -1, displayName = "Root" };
+		}
+
+		protected override IList<TreeViewItem> BuildRows(TreeViewItem root)
+		{
+			var items = new List<TreeViewItem>();
+
+			for (int i = 0; i < _data.Count; i++) {
+				items.Add(new RowData(i, _data[i]));
+			}
+
+			var sortedColumns = this.multiColumnHeader.state.sortedColumns;
+			if (sortedColumns.Length > 0) {
+				items.Sort((baseA, baseB) => {
+					var a = baseA as RowData;
+					var b = baseB as RowData;
+					if (a == null) return 1;
+					if (b == null) return -1;
+					// sort based on multiple columns
+					foreach (var column in sortedColumns) {
+						bool ascending = multiColumnHeader.IsSortedAscending(column);
+						// flip for descending
+						if (!ascending) {
+							var tmp = b;
+							b = a;
+							a = tmp;
+						}
+						int compareResult = 0;
+
+						var aVal = GetColumnValue(a, column) as IComparable;
+						var bVal = GetColumnValue(b, column) as IComparable;
+						if (aVal != bVal) {
+							if (aVal == null) {
+								compareResult = 1;
+							} else if (bVal == null) {
+								compareResult = -1;
+							} else {
+								compareResult = aVal.CompareTo(bVal);
+							}
+						}
+						// not equal in this column, then return that
+						if (compareResult != 0) {
+							return compareResult;
+						}
+					}
+					return a.id.CompareTo(b.id);
+				});
+			}
+
+			return items;
+		}
+
+		// not supporting multi select for now
+		protected override bool CanMultiSelect(TreeViewItem item) => false;
+
+		protected override void SelectionChanged(IList<int> selectedIds)
+		{
+			List<T> selectedData = new List<T>();
+			var rows = GetRows();
+			foreach (var row in rows) {
+				if (selectedIds.Contains(row.id)) {
+					selectedData.Add((row as RowData).Data);
+				}
+			}
+			ItemSelected?.Invoke(selectedData);
+		}
+
+		protected override void RowGUI(RowGUIArgs args)
+		{
+			for (int i = 0; i < args.GetNumVisibleColumns(); ++i) {
+				CellGUI(args.GetCellRect(i), args.item, args.GetColumn(i));
+			}
+		}
+
+		private void CellGUI(Rect cellRect, TreeViewItem item, int column)
+		{
+			CenterRectUsingSingleLineHeight(ref cellRect);
+			var val = GetColumnValue(item, column);
+			if (val != null) {
+				if (val is bool bVal) {
+					GUI.Label(cellRect, bVal ? "X" : "");
+				} else {
+					GUI.Label(cellRect, val.ToString());
+				}
+			}
+		}
+
+		// use cached reflection info to get T's instance data for a given column
+		private object GetColumnValue(TreeViewItem item, int column)
+		{
+			if (column < 0 && column >= _columnMemberInfos.Count) {
+				return null;
+			}
+
+			var memberInfo = _columnMemberInfos[column];
+			object val = null;
+			switch (memberInfo) {
+				case FieldInfo fi: val = fi.GetValue((item as RowData).Data); break;
+				case PropertyInfo pi: val = pi.GetValue((item as RowData).Data); break;
+			}
+			return val;
+		}
+
+		private void SortingChanged(MultiColumnHeader multiColumnHeader)
+		{
+			Reload();
+		}
+
+		private class RowData : TreeViewItem
+		{
+			public T Data;
+			public RowData(int id, T data) : base(id, 0) { Data = data; }
+		}
+	}
+}
