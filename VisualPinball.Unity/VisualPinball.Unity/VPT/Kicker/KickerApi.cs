65ec86925483d2d0307ca8f39ee249f520a84bf4
freezy <freezy@kodi.tv>
2020-07-26 22:13:30 +0200

    kicker: Implement event APIs.

--- VisualPinball.Unity/VisualPinball.Unity/VPT/Kicker/KickerApi.cs
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/Kicker/KickerApi.cs
@@ -1,12 +1,29 @@
-﻿using Unity.Entities;
+﻿using System;
+using Unity.Entities;
 using VisualPinball.Engine.VPT.Kicker;
 using VisualPinball.Unity.Game;
 using VisualPinball.Unity.VPT.Ball;
 
 namespace VisualPinball.Unity.VPT.Kicker
 {
-	public class KickerApi : ItemApi<Engine.VPT.Kicker.Kicker, KickerData>
+	public class KickerApi : ItemApi<Engine.VPT.Kicker.Kicker, KickerData>, IApiInitializable, IApiHittable
 	{
+
+		/// <summary>
+		/// Event emitted when the table is started.
+		/// </summary>
+		public event EventHandler Init;
+
+		/// <summary>
+		/// Event emitted when the ball moves into the kicker.
+		/// </summary>
+		public event EventHandler Hit;
+
+		/// <summary>
+		/// Event emitted when the ball leaves the kicker.
+		/// </summary>
+		public event EventHandler UnHit;
+
 		public KickerApi(Engine.VPT.Kicker.Kicker item, Entity entity, Player player) : base(item, entity, player)
 		{
 		}
@@ -25,5 +42,24 @@ namespace VisualPinball.Unity.VPT.Kicker
 		{
 			return Player.CreateBall(Item, radius);
 		}
+
+		#region Events
+
+		void IApiInitializable.OnInit()
+		{
+			Init?.Invoke(this, EventArgs.Empty);
+		}
+
+		void IApiHittable.OnHit(bool isUnHit)
+		{
+			if (isUnHit) {
+				UnHit?.Invoke(this, EventArgs.Empty);
+
+			} else {
+				Hit?.Invoke(this, EventArgs.Empty);
+			}
+		}
+
+		#endregion
 	}
 }
