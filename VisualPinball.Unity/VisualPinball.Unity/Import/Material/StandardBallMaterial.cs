4250fc33d431aa82aae1e54631362a874e00cd44
Roland <10963432+Roland09@users.noreply.github.com>
2020-07-31 16:29:47 +0200

    Dedicated Material classes for Standard, URP and HDRP
    Created dedicated material converters and ball material creators for Standard, URP and HDRP graphic pipelines

---
+++ VisualPinball.Unity/VisualPinball.Unity/Import/Material/StandardBallMaterial.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using VisualPinball.Engine.Resources;
+
+namespace VisualPinball.Unity.Import.Material
+{
+	public class StandardBallMaterial : IBallMaterial
+	{
+		private readonly int MainTex = Shader.PropertyToID("_MainTex");
+		private readonly int Metallic = Shader.PropertyToID("_Metallic");
+		private readonly int Glossiness = Shader.PropertyToID("_Glossiness");
+
+		public Shader GetShader()
+		{
+			return Shader.Find("Standard");
+		}
+
+		public UnityEngine.Material CreateMaterial()
+		{
+			var material = new UnityEngine.Material(GetShader());
+			var texture = new Texture2D(512, 512, TextureFormat.RGBA32, true) { name = "BallDebugTexture" };
+			texture.LoadImage(Resource.BallDebug.Data);
+			material.SetTexture(MainTex, texture);
+			material.SetFloat(Metallic, 1f);
+			material.SetFloat(Glossiness, 1f);
+			return material;
+		}
+	}
+}
