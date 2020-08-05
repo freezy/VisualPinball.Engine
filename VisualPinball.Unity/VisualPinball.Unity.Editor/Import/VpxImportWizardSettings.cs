2ae0d8cf9f47e134d5e8c4e844cfdcf5bfe93464
Roland09 <roland.cidar@gmail.com>
2020-08-05 15:15:43 +0200

    Added import wizard

---
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/Import/VpxImportWizardSettings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace VisualPinball.Unity.Editor.Import
+{
+	[Serializable]
+	public class VpxImportWizardSettings : MonoBehaviour
+	{
+		public static bool ApplyPatch
+		{
+			get => EditorPrefs.GetBool("ApplyPatch", false);
+			set => EditorPrefs.SetBool("ApplyPatch", value);
+		}
+
+		public static string VpxPath
+		{
+			get => EditorPrefs.GetString("VpxPath", "");
+			set => EditorPrefs.SetString("VpxPath", value);
+		}
+
+		public static string TableName
+		{
+			get => EditorPrefs.GetString("TableName", "");
+			set => EditorPrefs.SetString("TableName", value);
+		}
+
+		public static bool IsPathValid()
+		{
+			return !string.IsNullOrEmpty(VpxPath) && File.Exists(VpxPath);
+		}
+
+		public static void Reset()
+		{
+			VpxPath = "";
+			ApplyPatch = false;
+			TableName = "";
+		}
+	}
+}
