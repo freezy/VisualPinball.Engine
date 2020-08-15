3b0ef7864378306b2b4d9e8397172a1eef1d9682
freezy <freezy@kodi.tv>
2020-08-15 13:52:10 +0200

    primitive: Fix default data values.

--- VisualPinball.Engine/VPT/Primitive/PrimitiveData.cs
+++ VisualPinball.Engine/VPT/Primitive/PrimitiveData.cs
@@ -60,51 +60,51 @@ namespace VisualPinball.Engine.VPT.Primitive
 
 		[TextureReference]
 		[BiffString("IMAG", Pos = 12)]
-		public string Image;
+		public string Image = string.Empty;
 
 		[TextureReference]
 		[BiffString("NRMA", Pos = 13)]
-		public string NormalMap;
+		public string NormalMap = string.Empty;
 
 		[BiffInt("SIDS", Pos = 14)]
-		public int Sides;
+		public int Sides = 4;
 
 		[MaterialReference]
 		[BiffString("MATR", Pos = 16)]
-		public string Material;
+		public string Material = string.Empty;
 
 		[BiffColor("SCOL", Pos = 17)]
 		public Color SideColor = new Color(0x0, ColorFormat.Bgr);
 
 		[BiffBool("TVIS", Pos = 18)]
-		public bool IsVisible;
+		public bool IsVisible = true;
 
 		[BiffBool("REEN", Pos = 34)]
-		public bool IsReflectionEnabled;
+		public bool IsReflectionEnabled = true;
 
 		[BiffBool("DTXI", Pos = 19)]
 		public bool DrawTexturesInside;
 
 		[BiffBool("HTEV", Pos = 20)]
-		public bool HitEvent;
+		public bool HitEvent = true;
 
 		[BiffFloat("THRS", Pos = 21)]
-		public float Threshold;
+		public float Threshold = 2f;
 
 		[BiffFloat("ELAS", Pos = 22)]
-		public float Elasticity;
+		public float Elasticity = 0.3f;
 
 		[BiffFloat("ELFO", Pos = 23)]
-		public float ElasticityFalloff;
+		public float ElasticityFalloff = 0.5f;
 
 		[BiffFloat("RFCT", Pos = 24)]
-		public float Friction;
+		public float Friction = 0.3f;
 
 		[BiffFloat("RSCT", Pos = 25)]
 		public float Scatter;
 
 		[BiffFloat("EFUI", Pos = 26)]
-		public float EdgeFactorUi;
+		public float EdgeFactorUi = 0.25f;
 
 		[BiffFloat("CORF", Pos = 27)]
 		public float CollisionReductionFactor = 0;
@@ -117,13 +117,13 @@ namespace VisualPinball.Engine.VPT.Primitive
 
 		[MaterialReference]
 		[BiffString("MAPH", Pos = 36)]
-		public string PhysicsMaterial;
+		public string PhysicsMaterial = string.Empty;
 
 		[BiffBool("OVPH", Pos = 37)]
-		public bool OverwritePhysics;
+		public bool OverwritePhysics = true;
 
 		[BiffBool("STRE", Pos = 31)]
-		public bool StaticRendering;
+		public bool StaticRendering = true;
 
 		[BiffFloat("DILI", QuantizedUnsignedBits = 8, Pos = 32)]
 		public float DisableLightingTop; // m_d.m_fDisableLightingTop = (tmp == 1) ? 1.f : dequantizeUnsigned<8>(tmp); // backwards compatible hacky loading!
@@ -144,7 +144,7 @@ namespace VisualPinball.Engine.VPT.Primitive
 		public bool ObjectSpaceNormalMap;
 
 		[BiffString("M3DN", Pos = 39)]
-		public string MeshFileName;
+		public string MeshFileName = string.Empty;
 
 		[BiffFloat("PIDB", Pos = 46)]
 		public float DepthBias = 0;
