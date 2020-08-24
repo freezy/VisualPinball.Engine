323e98da3b3f9d791acfa2e1165084b9d1ab7575
VrôÕnsh <vroonsh@free.fr>
2020-08-24 14:22:32 +0200

    layers : frame scene view to layers editor selection
    layers: extract SceneView framing code in a helper class.

---
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/Utils/SceneViewFramer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace VisualPinball.Unity.Editor.Utils
+{
+	/// <summary>
+	/// This class is a helper to Frame the scene view using objects boundaries
+	/// </summary>
+	public class SceneViewFramer
+	{
+		public static void FrameObjects(Object[] objects, bool includeChildren = true, bool instant = false)
+		{
+			Bounds bounds = new Bounds();
+			foreach (var obj in objects) {
+				if (obj is UnityEngine.GameObject gameObj) {
+					var renders = includeChildren ? gameObj.GetComponentsInChildren<Renderer>() : gameObj.GetComponents<Renderer>();
+					foreach (var render in renders) {
+						bounds.Encapsulate(render.bounds);
+					}
+				}
+			}
+
+			if (bounds.extents != Vector3.zero) {
+				SceneView.lastActiveSceneView.Frame(bounds, instant);
+			}
+		}
+	}
+}
