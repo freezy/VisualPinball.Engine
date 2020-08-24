bfb543bdc1fa96433a35b105f1a4d70db79b815d
freezy <freezy@kodi.tv>
2020-08-25 00:34:36 +0200

    primitive: Make primitive collidable.

--- VisualPinball.Engine/VPT/Primitive/Primitive.cs
+++ VisualPinball.Engine/VPT/Primitive/Primitive.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using VisualPinball.Engine.Game;
+using VisualPinball.Engine.Physics;
 
 namespace VisualPinball.Engine.VPT.Primitive
 {
@@ -9,19 +10,31 @@ namespace VisualPinball.Engine.VPT.Primitive
 	/// </summary>
 	///
 	/// <see href="https://github.com/vpinball/vpinball/blob/master/primitive.cpp"/>
-	public class Primitive : Item<PrimitiveData>, IRenderable
+	public class Primitive : Item<PrimitiveData>, IRenderable, IHittable
 	{
+		public bool IsCollidable => true;
+		public EventProxy EventProxy { get; private set; }
+
 		public bool UseAsPlayfield;
 
 		private readonly PrimitiveMeshGenerator _meshGenerator;
+		private readonly PrimitiveHitGenerator _hitGenerator;
+		private HitObject[] _hits;
 
 		public Primitive(PrimitiveData data) : base(data)
 		{
 			_meshGenerator = new PrimitiveMeshGenerator(Data);
+			_hitGenerator = new PrimitiveHitGenerator(this);
 		}
 
 		public Primitive(BinaryReader reader, string itemName) : this(new PrimitiveData(reader, itemName))
 		{
+		}
+
+		public void Init(Table.Table table)
+		{
+			EventProxy = new EventProxy(this);
+			_hits = _hitGenerator.GenerateHitObjects(table, Data.Mesh);
 		}
 
 		public static Primitive GetDefault(Table.Table table)
@@ -30,19 +43,15 @@ namespace VisualPinball.Engine.VPT.Primitive
 			return new Primitive(primitiveData);
 		}
 
-		public RenderObjectGroup GetRenderObjects(Table.Table table, Origin origin, bool asRightHanded, string parent, PbrMaterial material)
-		{
-			return _meshGenerator.GetRenderObjects(table, origin, asRightHanded, parent, material);
-		}
+		public RenderObjectGroup GetRenderObjects(Table.Table table, Origin origin, bool asRightHanded, string parent, PbrMaterial material) =>
+			_meshGenerator.GetRenderObjects(table, origin, asRightHanded, parent, material);
 
-		public RenderObjectGroup GetRenderObjects(Table.Table table, Origin origin = Origin.Global, bool asRightHanded = true)
-		{
-			return _meshGenerator.GetRenderObjects(table, origin, asRightHanded);
-		}
+		public RenderObjectGroup GetRenderObjects(Table.Table table, Origin origin = Origin.Global, bool asRightHanded = true) =>
+			_meshGenerator.GetRenderObjects(table, origin, asRightHanded);
 
-		public Mesh GetMesh()
-		{
-			return _meshGenerator.GetMesh();
-		}
+		public HitObject[] GetHitShapes() => _hits;
+
+		public Mesh GetMesh() => _meshGenerator.GetMesh();
+
 	}
 }
