48658dc45a4ad11351b7ce566cf76a8a959151e9
VrôÕnsh <vroonsh@free.fr>
2020-08-26 23:49:32 +0200

    collections : new Collection Manager
    - new Manager based on ManagerWindow
    - new CollectionTreeView for Collection details

---
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/Managers/Collections/CollectionTreeElement.cs
@@ -0,0 +1,14 @@
+﻿namespace VisualPinball.Unity.Editor
+{
+	public class CollectionTreeElement : TreeElement
+	{
+		private string _itemName;
+
+		public override string Name => _itemName ?? "";
+
+		public CollectionTreeElement(string itemName) : base()
+		{
+			_itemName = itemName;
+		}
+	}
+}
---
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/Managers/Collections/CollectionTreeView.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor.IMGUI.Controls;
+
+namespace VisualPinball.Unity.Editor
+{
+	internal class CollectionTreeView : TreeView<CollectionTreeElement>
+	{
+		public CollectionTreeView() : base(new TreeViewState(), new CollectionTreeElement(string.Empty) { Id = -1 })
+		{
+			showBorder = true;
+			showAlternatingRowBackgrounds = true;
+		}
+	}
+}
