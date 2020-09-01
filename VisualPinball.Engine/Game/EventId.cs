34a9ae545907f61d6a555d7b96384847908690c0
freezy <freezy@kodi.tv>
2020-09-02 00:25:59 +0200

    physics: Remove generic kdtree.

--- VisualPinball.Engine/Game/EventId.cs
+++ VisualPinball.Engine/Game/EventId.cs
@@ -2,23 +2,11 @@ namespace VisualPinball.Engine.Game
 {
 	public enum EventId
 	{
-		// Table
-		GameEventsKeyDown = 1000, // DISPID_GameEvents_KeyDown
-		GameEventsKeyUp = 1001, // DISPID_GameEvents_KeyUp
-		GameEventsInit = 1002, // DISPID_GameEvents_Init
-		GameEventsMusicDone = 1003, // DISPID_GameEvents_MusicDone
-		GameEventsExit = 1004, // DISPID_GameEvents_Exit
-		GameEventsPaused = 1005, // DISPID_GameEvents_Paused
-		GameEventsUnPaused = 1006, // DISPID_GameEvents_UnPaused
-
 		// Surface
 		SurfaceEventsSlingshot = 1101, // DISPID_SurfaceEvents_Slingshot
 
 		// Flipper
 		FlipperEventsCollide = 1200, // DISPID_FlipperEvents_Collide
-
-		// Timer
-		TimerEventsTimer = 1300, // DISPID_TimerEvents_Timer
 
 		// Spinner
 		SpinnerEventsSpin = 1301, // DISPID_SpinnerEvents_Spin
@@ -26,24 +14,6 @@ namespace VisualPinball.Engine.Game
 		// HitTarget
 		TargetEventsDropped = 1302, // DISPID_TargetEvents_Dropped
 		TargetEventsRaised = 1303,  // DISPID_TargetEvents_Raised
-
-		// Light Sequencer
-		LightSeqEventsPlayDone = 1320,
-
-		// Plunger
-		// PluFrames = 464,
-		// Width = 465,
-		// ZAdjust = 466,
-		//
-		// RodDiam = 467,
-		// RingDiam = 468,
-		// RingThickness = 469,
-		// SpringDiam = 470,
-		// TipShape = 471,
-		// SpringGauge = 472,
-		// SpringLoops = 473,
-		// RingGap = 474,
-		// SpringEndLoops = 475,
 
 		// Generic
 		HitEventsHit = 1400, // DISPID_HitEvents_Hit
--- VisualPinball.Unity/VisualPinball.Unity/Physics/Collision/QuadTreeConversionSystem.cs
+++ VisualPinball.Unity/VisualPinball.Unity/Physics/Collision/QuadTreeConversionSystem.cs
@@ -38,7 +38,7 @@ namespace VisualPinball.Unity
 			}
 
 			// construct quad tree
-			var quadTree = new HitQuadTree(hitObjects, table.BoundingBox);
+			var quadTree = new Engine.Physics.QuadTree(hitObjects, table.BoundingBox);
 			var quadTreeBlobAssetRef = QuadTreeBlob.CreateBlobAssetReference(
 				quadTree,
 				table.GeneratePlayfieldHit(), // todo use `null` if separate playfield mesh exists
