29173a5c25aa17850bbdb1951c8460e02d2d9499
freezy <freezy@kodi.tv>
2020-09-04 12:01:04 +0200

    project: Update licenses for progmesh and triangulator.2

--- VisualPinball.Engine/Math/Mesh/ProgMesh.cs
+++ VisualPinball.Engine/Math/Mesh/ProgMesh.cs
@@ -1,18 +1,8 @@
-// Visual Pinball Engine
-// Copyright (C) 2020 freezy and VPE Team
+// Progressive Mesh type Polygon Reduction Algorithm
+//   by Stan Melax (c) 1998
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>.
+// Permission to use any of this code wherever you want is granted..
+// Although, please do acknowledge authorship if appropriate.
 
 // ReSharper disable ForeachCanBePartlyConvertedToQueryUsingAnotherGetEnumerator
 // ReSharper disable ForeachCanBeConvertedToQueryUsingAnotherGetEnumerator
