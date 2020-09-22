a4082d825a17df1b45fdfe85a3dfaf9c916ebf6b
VrôÕnsh <vroonsh@free.fr>
2020-09-22 15:20:09 +0200

    gates : draw direction gizmo

--- VisualPinball.Unity/VisualPinball.Unity.Editor/Inspectors/GateInspector.cs
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/Inspectors/GateInspector.cs
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+using UnityEngine;
 using UnityEditor;
 using VisualPinball.Engine.VPT;
 
@@ -37,6 +38,32 @@ namespace VisualPinball.Unity.Editor
 			_gate = target as GateAuthoring;
 		}
 
+		protected virtual void OnSceneGUI()
+		{
+			if (target is IEditableItemAuthoring editable) {
+				var position = editable.GetEditorPosition();
+				var transform = (target as MonoBehaviour).transform;
+				if (transform != null && transform.parent != null) {
+					position = transform.parent.TransformPoint(position);
+					var axis = transform.TransformDirection(Vector3.up);
+					var scale = _gate.Item.Data.Length * 0.0005f;
+					Handles.color = Color.white;
+					Handles.DrawWireDisc(position, axis, scale);
+					Color col = Color.grey;
+					col.a = 0.25f;
+					Handles.color = col;
+					Handles.DrawSolidDisc(position, axis, scale);
+
+					Handles.color = Color.white;
+					var arrowscale = 0.048f + Mathf.PingPong(Time.realtimeSinceStartup * 0.005f, 0.002f);
+					Handles.ArrowHandleCap(-1, position, Quaternion.LookRotation(-axis), arrowscale, EventType.Repaint);
+					if (_gate.Item.Data.TwoWay) {
+						Handles.ArrowHandleCap(-1, position, Quaternion.LookRotation(axis), arrowscale, EventType.Repaint);
+					}
+				}
+				HandleUtility.Repaint();
+			}
+		}
 		public override void OnInspectorGUI()
 		{
 			OnPreInspectorGUI();
@@ -78,5 +105,14 @@ namespace VisualPinball.Unity.Editor
 
 			base.OnInspectorGUI();
 		}
+
+		protected override void FinishEdit(string label, bool dirtyMesh = true)
+		{
+			if (label == "Two Way") {
+				SceneView.RepaintAll();
+			}
+			base.FinishEdit(label, dirtyMesh);
+		}
+
 	}
 }
