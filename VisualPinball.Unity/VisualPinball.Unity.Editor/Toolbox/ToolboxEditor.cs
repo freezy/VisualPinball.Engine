c26acbf10dc79d96dd738f498f2f2418ea367cc1
freezy <freezy@kodi.tv>
2020-09-25 00:52:50 +0200

    import: Remove collider component if overridden.

--- VisualPinball.Unity/VisualPinball.Unity.Editor/Toolbox/ToolboxEditor.cs
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/Toolbox/ToolboxEditor.cs
@@ -185,7 +185,8 @@ namespace VisualPinball.Unity.Editor
 		private GameObject CreateRenderable(IRenderable renderable)
 		{
 			var rog = renderable.GetRenderObjects(_tableAuthoring.Table, Origin.Original, false);
-			VpxConverter.ConvertRenderObjects(renderable, rog, GetOrCreateParent(_tableAuthoring, rog), _tableAuthoring, out var obj, out _);
+			VpxConverter.ConvertRenderObjects(renderable, rog, GetOrCreateParent(_tableAuthoring, rog), _tableAuthoring, out var obj);
+			VpxConverter.SetupGameObjectComponents(renderable, obj, rog, null);
 			return obj;
 		}
 
