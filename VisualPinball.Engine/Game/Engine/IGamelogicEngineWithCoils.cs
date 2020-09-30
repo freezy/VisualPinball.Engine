60cc28a3fc461bcb121b9e02ed4b1419882c98d8
freezy <freezy@kodi.tv>
2020-09-30 17:34:44 +0200

    engine: Add basic interfaces for game logic engine.

---
+++ VisualPinball.Engine/Game/Engine/IGamelogicEngineWithCoils.cs
@@ -0,0 +1,58 @@
+﻿// Visual Pinball Engine
+// Copyright (C) 2020 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+
+namespace VisualPinball.Engine.Game.Engine
+{
+	/// <summary>
+	/// A game logic engine working with coils (solenoids). <p/>
+	///
+	/// It provides a list of available coils and an event handler to trigger
+	/// them.
+	/// </summary>
+	public interface IGamelogicEngineWithCoils
+	{
+		/// <summary>
+		/// A list of available coils.
+		/// </summary>
+		string[] AvailableCoils { get; }
+
+		/// <summary>
+		/// Triggered when a coil is enabled or disabled.
+		/// </summary>
+		event EventHandler<CoilEventArgs> OnCoilChanged;
+	}
+
+	public readonly struct CoilEventArgs
+	{
+		/// <summary>
+		/// Name of the coil, as defined by <see cref="IGamelogicEngineWithCoils.AvailableCoils"/>.
+		/// </summary>
+		public readonly string Name;
+
+		/// <summary>
+		/// State of the coil, true if the coil is under voltage, false if not.
+		/// </summary>
+		public readonly bool IsEnabled;
+
+		public CoilEventArgs(string name, bool isEnabled)
+		{
+			Name = name;
+			IsEnabled = isEnabled;
+		}
+	}
+}
---
+++ VisualPinball.Engine/Game/Engine/IGamelogicEngineWithSwitches.cs
@@ -0,0 +1,39 @@
+﻿// Visual Pinball Engine
+// Copyright (C) 2020 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+namespace VisualPinball.Engine.Game.Engine
+{
+	/// <summary>
+	/// A game logic engine working with switches. <p/>
+	///
+	/// It provides a list of available switches and a method for triggering
+	/// them.
+	/// </summary>
+	public interface IGamelogicEngineWithSwitches
+	{
+		/// <summary>
+		/// A list of available switches supported by the game logic engine.
+		/// </summary>
+		string[] AvailableSwitches { get; }
+
+		/// <summary>
+		/// Enables or disables a switch.
+		/// </summary>
+		/// <param name="id">Name of the switch, as defined by <see cref="AvailableSwitches"/>.</param>
+		/// <param name="normallyClosed">True for normally closed (NC) i.e. contact, a.k.a. "on". False for normally open (NO), i.e. no contact, a.k.a "off".</param>
+		void Switch(string id, bool normallyClosed);
+	}
+}
