ad0439c818d03d679a43d909e473c593236df4e6
freezy <freezy@kodi.tv>
2020-10-14 00:14:14 +0200

    primitive: Create authoring components.

--- VisualPinball.Unity/VisualPinball.Unity/Import/VpxConverter.cs
+++ VisualPinball.Unity/VisualPinball.Unity/Import/VpxConverter.cs
@@ -192,7 +192,7 @@ namespace VisualPinball.Unity
 				case Kicker kicker:             mainAuthoring = kicker.SetupGameObject(obj, parentAuthoring); break;
 				case Engine.VPT.Light.Light lt: lt.SetupGameObject(obj); break;
 				case Plunger plunger:           plunger.SetupGameObject(obj); break;
-				case Primitive primitive:       primitive.SetupGameObject(obj); break;
+				case Primitive primitive:       mainAuthoring = primitive.SetupGameObject(obj, parentAuthoring); break;
 				case Ramp ramp:                 mainAuthoring = ramp.SetupGameObject(obj, parentAuthoring); break;
 				case Rubber rubber:             mainAuthoring = rubber.SetupGameObject(obj, parentAuthoring); break;
 				case Spinner spinner:           mainAuthoring = spinner.SetupGameObject(obj, parentAuthoring); break;
