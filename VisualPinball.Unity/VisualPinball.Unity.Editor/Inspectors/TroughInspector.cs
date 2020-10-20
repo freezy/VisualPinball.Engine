4d0a4b7f9806f13e480f2f5daa6985842b41a0c4
Eli Curtz <eli@nuprometheus.com>
2020-10-19 17:35:00 -0700

    Minimal boilerplate for new trough items.

---
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/Inspectors/TroughInspector.cs
@@ -0,0 +1,56 @@
+﻿// Visual Pinball Engine
+// Copyright (C) 2020 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+using UnityEditor;
+
+namespace VisualPinball.Unity.Editor
+{
+	[CustomEditor(typeof(TroughAuthoring))]
+	public class TroughInspector : ItemInspector
+	{
+		private TroughAuthoring _trough;
+		private bool _foldoutPosition = true;
+		private bool _foldoutMisc = true;
+
+		protected override void OnEnable()
+		{
+			base.OnEnable();
+			_trough = target as TroughAuthoring;
+		}
+
+		public override void OnInspectorGUI()
+		{
+			OnPreInspectorGUI();
+
+			if (_foldoutPosition = EditorGUILayout.BeginFoldoutHeaderGroup(_foldoutPosition, "Position")) {
+				ItemDataField("", ref _trough.data.Entrance);
+				ItemDataField("Rotation", ref _trough.data.Orientation);
+				ItemDataField("Exit Offset", ref _trough.data.ExitOffset);
+				SurfaceField("Surface", ref _trough.data.Surface);
+			}
+			EditorGUILayout.EndFoldoutHeaderGroup();
+
+			if (_foldoutMisc = EditorGUILayout.BeginFoldoutHeaderGroup(_foldoutMisc, "Misc")) {
+				ItemDataField("Max Balls", ref _trough.data.BallCount, dirtyMesh: false);
+				ItemDataField("Switch Count", ref _trough.data.SwitchCount, dirtyMesh: false);
+				ItemDataField("Settle Time", ref _trough.data.SettleTime, dirtyMesh: false);
+			}
+			EditorGUILayout.EndFoldoutHeaderGroup();
+
+			base.OnInspectorGUI();
+		}
+	}
+}
