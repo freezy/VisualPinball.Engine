68e03a489fe006f2f301a139ab3f94ea7743a5f9
freezy <freezy@kodi.tv>
2020-11-03 00:34:28 +0100

    ecs: Provide correct collider data.

--- VisualPinball.Unity/VisualPinball.Unity/VPT/ItemApi.cs
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/ItemApi.cs
@@ -70,17 +70,21 @@ namespace VisualPinball.Unity
 			BallManager.DestroyEntity(ballEntity);
 		}
 
-		internal ColliderInfo GetColliderInfo(int id, ItemType itemType, ColliderType colliderType) {
+		internal ColliderInfo GetColliderInfo(int id, ColliderType colliderType)
+		{
+			if (!(this is IApiCollider c)) {
+				return default;
+			}
 			return new ColliderInfo {
 				Id = id,
 				Type = colliderType,
-				ItemType = itemType,
+				ItemType = c.ItemType,
 				Entity = Entity,
 				ParentEntity = ParentEntity,
-				FireEvents = true,
-				IsEnabled = true,
-				Material = default,
-				Threshold = 0,
+				FireEvents = c.FireEvents,
+				IsEnabled = c.IsColliderEnabled,
+				Material = c.PhysicsMaterial,
+				Threshold = c.Threshold,
 			};
 		}
 
