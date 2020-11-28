3f3165328acc09d387eef02c37c9b0692ebb6142
Pandelii <neilnafus@gmail.com>
2020-11-27 23:35:00 -0800

    Camera controller inspector
    Added custom inspector for the camera controller.  Swapped the camera controller over to being controlled by the inspector.  Added camera preset struct.

---
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/Managers/CameraPreset.cs
@@ -0,0 +1,37 @@
+// Visual Pinball Engine
+// Copyright (C) 2020 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VisualPinball.Unity
+{
+	struct CameraPreset
+	{
+		public string name { get; set; }
+		public float yOffset { get; set; }
+		public float fov { get; set; }
+		public float distance { get; set; }
+		public float angle { get; set; }
+		public float orbit { get; set; }
+
+
+	}
+
+}
+
