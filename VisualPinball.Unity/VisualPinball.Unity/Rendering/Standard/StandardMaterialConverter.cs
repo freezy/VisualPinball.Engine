c3a0a4404f67f59eb7e7252b6eb9528932470018
Roland09 <roland.cidar@gmail.com>
2020-12-06 10:42:58 +0100

    Replaced manual material changes with material default templates

--- VisualPinball.Unity/VisualPinball.Unity/Rendering/Standard/StandardMaterialConverter.cs
+++ VisualPinball.Unity/VisualPinball.Unity/Rendering/Standard/StandardMaterialConverter.cs
@@ -25,11 +25,11 @@ namespace VisualPinball.Unity
 	{
 		#region Shader Properties
 
-		private static readonly int Color = Shader.PropertyToID("_Color");
-		private static readonly int MainTex = Shader.PropertyToID("_MainTex");
-		private static readonly int BumpMap = Shader.PropertyToID("_BumpMap");
+		private static readonly int BaseColor = Shader.PropertyToID("_Color");
+		private static readonly int BaseColorMap = Shader.PropertyToID("_MainTex");
+		private static readonly int NormalMap = Shader.PropertyToID("_BumpMap");
 		private static readonly int Metallic = Shader.PropertyToID("_Metallic");
-		private static readonly int Glossiness = Shader.PropertyToID("_Glossiness");
+		private static readonly int Smoothness = Shader.PropertyToID("_Glossiness");
 		private static readonly int Mode = Shader.PropertyToID("_Mode");
 		private static readonly int SrcBlend = Shader.PropertyToID("_SrcBlend");
 		private static readonly int DstBlend = Shader.PropertyToID("_DstBlend");
@@ -42,12 +42,29 @@ namespace VisualPinball.Unity
 			return Shader.Find("Standard");
 		}
 
+		public static UnityEngine.Material GetDefaultMaterial(BlendMode blendMode)
+		{
+			switch (blendMode)
+			{
+				case Engine.VPT.BlendMode.Opaque:
+					return UnityEngine.Resources.Load<UnityEngine.Material>("Materials/TableOpaque");
+				case Engine.VPT.BlendMode.Cutout:
+					return UnityEngine.Resources.Load<UnityEngine.Material>("Materials/TableCutout");
+				case Engine.VPT.BlendMode.Translucent:
+					return UnityEngine.Resources.Load<UnityEngine.Material>("Materials/TableTranslucent");
+				default:
+					throw new ArgumentOutOfRangeException("Undefined blend mode " + blendMode);
+			}
+
+		}
+
 		public UnityEngine.Material CreateMaterial(PbrMaterial vpxMaterial, TableAuthoring table, Type objectType, StringBuilder debug = null)
 		{
-			var unityMaterial = new UnityEngine.Material(GetShader())
-			{
-				name = vpxMaterial.Id
-			};
+			UnityEngine.Material defaultMaterial = GetDefaultMaterial(vpxMaterial.MapBlendMode);
+
+			var unityMaterial = new UnityEngine.Material(GetShader());
+			unityMaterial.CopyPropertiesFromMaterial(defaultMaterial);
+			unityMaterial.name = vpxMaterial.Id;
 
 			// apply some basic manipulations to the color. this just makes very
 			// very white colors be clipped to 0.8204 aka 204/255 is 0.8
@@ -59,120 +76,46 @@ namespace VisualPinball.Unity
 				debug?.AppendLine("Color manipulation performed, brightness reduced.");
 				col.r = col.g = col.b = 0.8f;
 			}
-			unityMaterial.SetColor(Color, col);
+
+
+			if (vpxMaterial.MapBlendMode == Engine.VPT.BlendMode.Translucent)
+			{
+				col.a = Mathf.Min(1, Mathf.Max(0, vpxMaterial.Opacity));
+			}
+			unityMaterial.SetColor(BaseColor, col);
 
 			// validate IsMetal. if true, set the metallic value.
 			// found VPX authors setting metallic as well as translucent at the
 			// same time, which does not render correctly in unity so we have
 			// to check if this value is true and also if opacity <= 1.
+			float metallicValue = 0f;
 			if (vpxMaterial.IsMetal && (!vpxMaterial.IsOpacityActive || vpxMaterial.Opacity >= 1))
 			{
-				unityMaterial.SetFloat(Metallic, 1f);
+				metallicValue = 1f;
 				debug?.AppendLine("Metallic set to 1.");
 			}
 
-			// roughness / glossiness
-			unityMaterial.SetFloat(Glossiness, vpxMaterial.Roughness);
+			unityMaterial.SetFloat(Metallic, metallicValue);
 
-			// blend mode
-			ApplyBlendMode(unityMaterial, vpxMaterial.MapBlendMode);
-			if (vpxMaterial.MapBlendMode == BlendMode.Translucent)
-			{
-				col.a = Mathf.Min(1, Mathf.Max(0, vpxMaterial.Opacity));
-				unityMaterial.SetColor(Color, col);
-			}
+			// roughness / glossiness
+			unityMaterial.SetFloat(Smoothness, vpxMaterial.Roughness);
 
 			// map
 			if (table != null && vpxMaterial.HasMap)
 			{
-				unityMaterial.SetTexture(
-					MainTex,
-					table.GetTexture(vpxMaterial.Map.Name)
-				);
+				unityMaterial.SetTexture(BaseColorMap, table.GetTexture(vpxMaterial.Map.Name));
 			}
 
 			// normal map
 			if (table != null && vpxMaterial.HasNormalMap)
 			{
 				unityMaterial.EnableKeyword("_NORMALMAP");
-				unityMaterial.SetTexture(
-					BumpMap,
-					table.GetTexture(vpxMaterial.NormalMap.Name)
-				);
+				unityMaterial.EnableKeyword("_NORMALMAP_TANGENT_SPACE");
+
+				unityMaterial.SetTexture(NormalMap, table.GetTexture(vpxMaterial.NormalMap.Name));
 			}
 
 			return unityMaterial;
-		}
-
-		private static void ApplyBlendMode(UnityEngine.Material unityMaterial, BlendMode blendMode)
-		{
-			switch (blendMode)
-			{
-				case BlendMode.Opaque:
-
-					// keywords
-					unityMaterial.DisableKeyword("_ALPHATEST_ON");
-					unityMaterial.DisableKeyword("_ALPHABLEND_ON");
-					unityMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-
-					// required for the blend mode
-					unityMaterial.SetInt(SrcBlend, (int)UnityEngine.Rendering.BlendMode.One);
-					unityMaterial.SetInt(DstBlend, (int)UnityEngine.Rendering.BlendMode.Zero);
-					unityMaterial.SetInt(ZWrite, 1);
-
-					// properties
-					unityMaterial.SetFloat(Mode, 0);
-
-					// render queue
-					unityMaterial.renderQueue = -1;
-
-					break;
-
-				case BlendMode.Cutout:
-
-					// keywords
-					unityMaterial.EnableKeyword("_ALPHATEST_ON");
-					unityMaterial.DisableKeyword("_ALPHABLEND_ON");
-					unityMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-
-					// required for the blend mode
-					unityMaterial.SetInt(SrcBlend, (int)UnityEngine.Rendering.BlendMode.One);
-					unityMaterial.SetInt(DstBlend, (int)UnityEngine.Rendering.BlendMode.Zero);
-					unityMaterial.SetInt(ZWrite, 1);
-
-					// properties
-					unityMaterial.SetFloat(Mode, 1);
-
-					// render queue
-					unityMaterial.renderQueue = 2450;
-
-					break;
-
-				case BlendMode.Translucent:
-
-					// keywords
-					unityMaterial.DisableKeyword("_ALPHATEST_ON");
-					unityMaterial.DisableKeyword("_ALPHABLEND_ON");
-					unityMaterial.EnableKeyword("_ALPHAPREMULTIPLY_ON");
-
-					// required for the blend mode
-					unityMaterial.SetInt(SrcBlend, (int)UnityEngine.Rendering.BlendMode.One);
-					unityMaterial.SetInt(DstBlend, (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-					//!!!!!!! this is normally switched off but somehow enabling it seems to resolve so many issues.. keep an eye out for weirld opacity issues
-					//unityMaterial.SetInt("_ZWrite", 0);
-					unityMaterial.SetInt(ZWrite, 1);
-
-					// properties
-					unityMaterial.SetFloat(Mode, 3);
-
-					// render queue
-					unityMaterial.renderQueue = 3000;
-
-					break;
-
-				default:
-					throw new ArgumentOutOfRangeException();
-			}
 		}
 	}
 }
