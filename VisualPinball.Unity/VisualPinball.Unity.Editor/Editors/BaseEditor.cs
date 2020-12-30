2b3307d0b83b9ed96f6077093f4a74a9ed669817
freezy <freezy@kodi.tv>
2020-12-30 14:24:46 +0100

    style: Add header.

--- VisualPinball.Unity/VisualPinball.Unity.Editor/Editors/BaseEditor.cs
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/Editors/BaseEditor.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿// Visual Pinball Engine
+// Copyright (C) 2020 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+using System;
 using UnityEngine;
 using System.Linq.Expressions;
 using System.Collections.Generic;
@@ -13,10 +29,7 @@ namespace VisualPinball.Unity.Editor
 	/// <typeparam name="T">The editor target</typeparam>
 	public class BaseEditor<T> : UnityEditor.Editor where T : MonoBehaviour
 	{
-		protected T Target
-		{
-			get { return (T)target; }
-		}
+		protected T Target => (T)target;
 
 		/// <summary>
 		/// Get a property using reflection and in that way avoiding having to hardcode members as strings.
@@ -55,8 +68,8 @@ namespace VisualPinball.Unity.Editor
 				memberExpression = memberExpression.Expression as MemberExpression;
 			}
 
-			StringBuilder stringBuilder = new StringBuilder();
-			for (int i = members.Count - 1; i >= 0; i--)
+			var stringBuilder = new StringBuilder();
+			for (var i = members.Count - 1; i >= 0; i--)
 			{
 				stringBuilder.Append(members[i]);
 
