1f0939dcae7a0735092327b9b1c85adfe7d1bce9
freezy <freezy@kodi.tv>
2021-03-30 23:06:04 +0200

    disp: Generate seg display mesh.

---
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/Inspectors/SegDispInspector.cs
@@ -0,0 +1,61 @@
+﻿// Visual Pinball Engine
+// Copyright (C) 2021 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+using Unity.Mathematics;
+using UnityEditor;
+
+namespace VisualPinball.Unity.Editor
+{
+
+	[CustomEditor(typeof(SegDisplayAuthoring)), CanEditMultipleObjects]
+	public class SegDispInspector : DisplayInspector
+	{
+		private SegDisplayAuthoring _mb;
+
+		private float _skewAngle;
+		private float _segmentWidth;
+
+		private void OnEnable()
+		{
+			_mb = target as SegDisplayAuthoring;
+			_skewAngle = math.degrees(_mb.SkewAngle);
+			_segmentWidth = _mb.SegmentWidth;
+		}
+		public override void OnInspectorGUI()
+		{
+			EditorGUI.BeginChangeCheck();
+
+			var width = EditorGUILayout.IntSlider("Width", _mb.Width, 1, 20);
+			if (width != _mb.Width) {
+				_mb.Width = width;
+				_mb.RegenerateMesh();
+			}
+
+			var skew = EditorGUILayout.Slider("Skew Angle", _skewAngle, -45f, 45f);
+			if (skew != _skewAngle) {
+				_mb.SkewAngle = math.radians(skew);
+				_skewAngle = skew;
+			}
+
+			var segWidth = EditorGUILayout.Slider("Segment Width", _segmentWidth, 0.005f, 0.11f);
+			if (segWidth != _segmentWidth) {
+				_mb.SegmentWidth = segWidth;
+			}
+
+			base.OnInspectorGUI();
+		}
+	}
+}
