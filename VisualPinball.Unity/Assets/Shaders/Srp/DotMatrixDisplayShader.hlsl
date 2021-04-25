7eaa7525f501257e3ec5caaa906d87af043eb4ae
freezy <freezy@kodi.tv>
2021-04-25 18:28:53 +0200

    display: Add unlit color to dmd shader.

--- VisualPinball.Unity/Assets/Shaders/Srp/DotMatrixDisplayShader.hlsl
+++ VisualPinball.Unity/Assets/Shaders/Srp/DotMatrixDisplayShader.hlsl
@@ -1,30 +1,22 @@
-﻿void SampleDot_float(float2 uv, UnityTexture2D data, float2 dimensions, out float4 output)
+﻿void SampleDot_float(float2 uv, UnityTexture2D data, float2 dimensions, out float4 dotColor, out float2 dotCenter)
 {
 	float2 dimensionsPerDot = 1. / dimensions;
 	float2 dotPos = floor(uv * dimensions);
-	float2 dotCenter = dotPos * dimensionsPerDot + dimensionsPerDot * 0.5;
-	output = tex2D(data, dotCenter);
+	dotCenter = dotPos * dimensionsPerDot + dimensionsPerDot * 0.5;
+	dotColor = tex2D(data, dotCenter);
 }
 
-void DotMatrixDisplay_float(float2 uv, UnityTexture2D data, float2 dimensions, float dotSize, out float4 output)
+void RoundDot_float(float2 uv, float2 dimensions, float dotSize, float4 dotColor, float2 dotCenter, out float4 output)
 {
-	float2 dimensionsPerDot = 1. / dimensions;
-	float aspectRatio = dimensions.x / dimensions.y;
-
-	// Calculate dot center
-	float2 dotPos = floor(uv * dimensions);
-	float2 dotCenter = dotPos * dimensionsPerDot + dimensionsPerDot * 0.5;
-
 	// Scale coordinates back to original ratio for rounding
+	float aspectRatio = dimensions.x / dimensions.y;
 	float2 uvScaled = float2(uv.x * aspectRatio, uv.y);
 	float2 dotCenterScaled = float2(dotCenter.x * aspectRatio, dotCenter.y);
 
 	// Round the dot by testing the distance of the pixel coordinate to the center
 	float dist = length(uvScaled - dotCenterScaled) * dimensions;
 
-	float4 insideColor = tex2D(data, dotCenter);
-
-	float4 outsideColor = insideColor;
+	float4 outsideColor = dotColor;
 	outsideColor.r = 0;
 	outsideColor.g = 0;
 	outsideColor.b = 0;
@@ -34,5 +26,5 @@ void DotMatrixDisplay_float(float2 uv, UnityTexture2D data, float2 dimensions, f
 	float thresholdWidth = .22;  // a constant you'd tune to get the right level of softness
 	float antialiasedCircle = saturate((distFromEdge / thresholdWidth) + 0.5);
 
-	output = lerp(outsideColor, insideColor, antialiasedCircle);
+	output = lerp(outsideColor, dotColor, antialiasedCircle);
 }
