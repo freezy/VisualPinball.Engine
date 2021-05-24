82ef0aac693451f2395113d28b4963b5d73eef62
freezy <freezy@kodi.tv>
2021-05-25 00:44:03 +0200

    flipper: Create new component without data.

---
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/Flipper/FlipperBaseMeshComponent.cs
@@ -0,0 +1,55 @@
+﻿// Visual Pinball Engine
+// Copyright (C) 2021 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+using UnityEngine;
+using VisualPinball.Engine.Game;
+using VisualPinball.Engine.VPT.Flipper;
+
+namespace VisualPinball.Unity
+{
+	public class FlipperBaseMeshComponent : ItemMeshComponent
+	{
+		protected override string MeshId => FlipperMeshGenerator.Base;
+
+		public override void CreateMesh(IRenderable item, ITextureProvider texProvider, IMaterialProvider matProvider)
+		{
+			var ta = GetComponentInParent<TableAuthoring>();
+			var ro = item.GetRenderObject(ta.Table, MeshId, Origin.Original, false);
+			if (ro?.Mesh == null) {
+				return;
+			}
+			var mesh = ro.Mesh.ToUnityMesh($"{gameObject.name}_Mesh");
+			enabled = ro.IsVisible;
+
+			// apply mesh to game object
+			var mf = gameObject.AddComponent<MeshFilter>();
+			mf.sharedMesh = mesh;
+
+			// apply material
+			if (ro.Mesh.AnimationFrames.Count > 1) { // if number of animations frames are 1, the blend vertices are in the uvs are handle by the lerp shader.
+				var smr = gameObject.AddComponent<SkinnedMeshRenderer>();
+				smr.sharedMaterial = ro.Material.ToUnityMaterial(matProvider, texProvider, typeof(Flipper));
+				smr.sharedMesh = mesh;
+				smr.SetBlendShapeWeight(0, ro.Mesh.AnimationDefaultPosition);
+				smr.enabled = ro.IsVisible;
+			} else {
+				var mr = gameObject.AddComponent<MeshRenderer>();
+				mr.sharedMaterial = ro.Material.ToUnityMaterial(matProvider, texProvider, typeof(Flipper));
+				mr.enabled = ro.IsVisible;
+			}
+		}
+	}
+}
---
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/ItemMainRenderableComponent.cs
@@ -0,0 +1,59 @@
+﻿// Visual Pinball Engine
+// Copyright (C) 2021 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+using UnityEngine;
+
+namespace VisualPinball.Unity
+{
+	public abstract class ItemMainRenderableComponent : MonoBehaviour
+	{
+		protected virtual void OnDrawGizmos()
+		{
+			// handle dirty whenever scene view draws just in case a field or dependant changed and our
+			// custom inspector window isn't up to process it
+			//RebuildMeshIfDirty();
+
+			// Draw invisible gizmos over top of the sub meshes of this item so clicking in the scene view
+			// selects the item itself first, which is most likely what the user would want
+			var mfs = GetComponentsInChildren<MeshFilter>();
+			Gizmos.color = Color.clear;
+			Gizmos.matrix = Matrix4x4.identity;
+			foreach (var mf in mfs) {
+				var t = mf.transform;
+				if (mf.sharedMesh.vertexCount > 0) {
+					Gizmos.DrawMesh(mf.sharedMesh, t.position, t.rotation, t.lossyScale);
+				}
+			}
+		}
+
+		#region Editor
+
+		public virtual ItemDataTransformType EditorPositionType => ItemDataTransformType.None;
+		public virtual Vector3 GetEditorPosition() => Vector3.zero;
+		public virtual void SetEditorPosition(Vector3 pos) { }
+
+		public virtual ItemDataTransformType EditorRotationType => ItemDataTransformType.None;
+		public virtual Vector3 GetEditorRotation() => Vector3.zero;
+		public virtual void SetEditorRotation(Vector3 rot) { }
+
+		public virtual ItemDataTransformType EditorScaleType => ItemDataTransformType.None;
+		public virtual Vector3 GetEditorScale() => Vector3.zero;
+		public virtual void SetEditorScale(Vector3 rot) { }
+
+		#endregion
+
+	}
+}
