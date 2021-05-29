818fe8360e0a770271d51f5daf4deff349b19c72
freezy <freezy@kodi.tv>
2021-05-29 18:01:28 +0200

    scene: Pull game items from hierarchy.

--- VisualPinball.Unity/VisualPinball.Unity/VPT/Table/SceneTableContainer.cs
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/Table/SceneTableContainer.cs
@@ -15,9 +15,31 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
+using VisualPinball.Engine.VPT.Bumper;
+using VisualPinball.Engine.VPT.DispReel;
+using VisualPinball.Engine.VPT.Flasher;
+using VisualPinball.Engine.VPT.Flipper;
+using VisualPinball.Engine.VPT.Gate;
+using VisualPinball.Engine.VPT.HitTarget;
+using VisualPinball.Engine.VPT.Kicker;
+using VisualPinball.Engine.VPT.LightSeq;
+using VisualPinball.Engine.VPT.Plunger;
+using VisualPinball.Engine.VPT.Primitive;
+using VisualPinball.Engine.VPT.Ramp;
+using VisualPinball.Engine.VPT.Rubber;
+using VisualPinball.Engine.VPT.Spinner;
+using VisualPinball.Engine.VPT.Surface;
 using VisualPinball.Engine.VPT.Table;
+using VisualPinball.Engine.VPT.TextBox;
+using VisualPinball.Engine.VPT.Trigger;
+using VisualPinball.Engine.VPT.Trough;
+using Light = VisualPinball.Engine.VPT.Light.Light;
 using Material = VisualPinball.Engine.VPT.Material;
 using Texture = VisualPinball.Engine.VPT.Texture;
 
@@ -54,10 +76,14 @@ namespace VisualPinball.Unity
 
 		private void OnHierarchyChanged()
 		{
+			var stopWatch = Stopwatch.StartNew();
+			Clear();
 			WalkChildren(_tableAuthoring.transform);
+
+			Logger.Info($"Refreshed {GameItems.Count()} game items in {stopWatch.ElapsedMilliseconds}ms.");
 		}
 
-		private void WalkChildren(Transform node)
+		private void WalkChildren(IEnumerable node)
 		{
 			foreach (Transform childTransform in node) {
 				RefreshChild(childTransform);
@@ -65,9 +91,60 @@ namespace VisualPinball.Unity
 			}
 		}
 
-		private void RefreshChild(Transform node)
+		private void RefreshChild(Component node)
 		{
-			Debug.Log(node.gameObject.name);
+			Add(node.GetComponent<IItemMainAuthoring>());
+		}
+
+		private void Add(IItemMainAuthoring comp)
+		{
+			if (comp == null) {
+				return;
+			}
+			switch (comp) {
+				case BumperAuthoring bumperAuthoring:
+					GetItemDictionary<Bumper>().Add(comp.Name, bumperAuthoring.Item);
+					break;
+				case FlipperAuthoring flipperAuthoring:
+					GetItemDictionary<Flipper>().Add(comp.Name, flipperAuthoring.Item);
+					break;
+				case GateAuthoring gateAuthoring:
+					GetItemDictionary<Gate>().Add(comp.Name, gateAuthoring.Item);
+					break;
+				case HitTargetAuthoring hitTargetAuthoring:
+					GetItemDictionary<HitTarget>().Add(comp.Name, hitTargetAuthoring.Item);
+					break;
+				case KickerAuthoring kickerAuthoring:
+					GetItemDictionary<Kicker>().Add(comp.Name, kickerAuthoring.Item);
+					break;
+				case LightAuthoring lightAuthoring:
+					GetItemDictionary<Light>().Add(comp.Name, lightAuthoring.Item);
+					break;
+				case PlungerAuthoring plungerAuthoring:
+					GetItemDictionary<Plunger>().Add(comp.Name, plungerAuthoring.Item);
+					break;
+				case PrimitiveAuthoring primitiveAuthoring:
+					GetItemDictionary<Primitive>().Add(comp.Name, primitiveAuthoring.Item);
+					break;
+				case RampAuthoring rampAuthoring:
+					GetItemDictionary<Ramp>().Add(comp.Name, rampAuthoring.Item);
+					break;
+				case RubberAuthoring rubberAuthoring:
+					GetItemDictionary<Rubber>().Add(comp.Name, rubberAuthoring.Item);
+					break;
+				case SpinnerAuthoring spinnerAuthoring:
+					GetItemDictionary<Spinner>().Add(comp.Name, spinnerAuthoring.Item);
+					break;
+				case SurfaceAuthoring surfaceAuthoring:
+					GetItemDictionary<Surface>().Add(comp.Name, surfaceAuthoring.Item);
+					break;
+				case TriggerAuthoring triggerAuthoring:
+					GetItemDictionary<Trigger>().Add(comp.Name, triggerAuthoring.Item);
+					break;
+				case TroughAuthoring troughAuthoring:
+					GetItemDictionary<Trough>().Add(comp.Name, troughAuthoring.Item);
+					break;
+			}
 		}
 
 		public void Dispose()
