c646f8df90c5dd7c3863d462616760827cbafccf
freezy <freezy@kodi.tv>
2021-06-01 23:36:59 +0200

    import: Write VP materials to disk as well.

---
+++ VisualPinball.Unity/VisualPinball.Unity/Scene/MaterialAsset.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Material = VisualPinball.Engine.VPT.Material;
+
+namespace VisualPinball.Unity.VisualPinball.Unity.Scene
+{
+	public class MaterialAsset : ScriptableObject
+	{
+		public Material Material;
+
+		public MaterialAsset(Material material)
+		{
+			Material = material;
+		}
+	}
+}
