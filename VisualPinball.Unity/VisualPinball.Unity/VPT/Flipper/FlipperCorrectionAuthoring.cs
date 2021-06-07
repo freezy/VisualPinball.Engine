e5d8a4dbf468019a9b4d1b2b6e1251250220f283
freezy <freezy@kodi.tv>
2021-06-08 00:36:34 +0200

    nfp: Add DOTS structure.

---
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/Flipper/FlipperCorrectionAuthoring.cs
@@ -0,0 +1,29 @@
+﻿// Visual Pinball Engine
+// Copyright (C) 2021 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+using UnityEngine;
+
+namespace VisualPinball.Unity
+{
+	/// <summary>
+	/// Makes flippers flip better, A.K.A. nFozzy physics.
+	/// </summary>
+	public class FlipperCorrectionAuthoring : MonoBehaviour
+	{
+		public Vector2[] Polarities;
+		public Vector2[] Velocities;
+	}
+}
