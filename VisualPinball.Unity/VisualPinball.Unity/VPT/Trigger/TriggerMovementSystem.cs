38f1fb09b8f17b62aeef07779460368ffd823de4
freezy <freezy@kodi.tv>
2021-07-24 22:50:43 +0200

    renderer: Migrate trigger animations to non-hybrid.

--- VisualPinball.Unity/VisualPinball.Unity/VPT/Trigger/TriggerMovementSystem.cs
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/Trigger/TriggerMovementSystem.cs
@@ -25,31 +25,30 @@ namespace VisualPinball.Unity
 	[UpdateInGroup(typeof(TransformMeshesSystemGroup))]
 	internal class TriggerMovementSystem : SystemBase
 	{
-		private float4x4 _baseTransform;
 		private static readonly ProfilerMarker PerfMarker = new ProfilerMarker("TriggerMovementSystem");
+
+		private Player _player;
 
 		protected override void OnStartRunning()
 		{
-			var root = Object.FindObjectOfType<TablePlayfieldAuthoring>();
-			var ltw = root.gameObject.transform.localToWorldMatrix;
-			_baseTransform = new float4x4(
-				ltw.m00, ltw.m01, ltw.m02, ltw.m03,
-				ltw.m10, ltw.m11, ltw.m12, ltw.m13,
-				ltw.m20, ltw.m21, ltw.m22, ltw.m23,
-				ltw.m30, ltw.m31, ltw.m32, ltw.m33
-			);
+			base.OnStartRunning();
+			_player = Object.FindObjectOfType<Player>();
 		}
 
 		protected override void OnUpdate()
 		{
-			var ltw = _baseTransform;
 			var marker = PerfMarker;
-			Entities.WithName("TriggerMovementJob").ForEach((ref Translation translation, in TriggerMovementData data) => {
+
+			Entities.WithoutBurst().WithName("TriggerMovementJob").ForEach((Entity entity, in TriggerMovementData data) => {
 
 				marker.Begin();
 
-				var t = math.transform(math.inverse(ltw), translation.Value);
-				translation.Value = math.transform(ltw, new float3(t.x, t.y, data.HeightOffset));
+				var localPos = _player.TriggerTransforms[entity].transform.localPosition;
+				_player.TriggerTransforms[entity].transform.localPosition= new Vector3(
+				    localPos.x,
+				    localPos.y,
+				    data.HeightOffset
+				);
 
 				marker.End();
 
