82b71698063ec9d8812f88a5f6508af939284e34
freezy <freezy@kodi.tv>
2021-09-13 23:04:37 +0200

    fix: Don't look for trough kicker item when non kicker is set.

--- VisualPinball.Unity/VisualPinball.Unity/VPT/Trough/TroughComponent.cs
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/Trough/TroughComponent.cs
@@ -171,7 +171,9 @@ namespace VisualPinball.Unity
 		{
 			PlayfieldEntrySwitch = FindComponent<ITriggerComponent>(components, data.PlayfieldEntrySwitch);
 			PlayfieldExitKicker = FindComponent<KickerComponent>(components, data.PlayfieldExitKicker);
-			PlayfieldExitKickerItem = PlayfieldExitKicker.AvailableCoils.First().Id;
+			if (PlayfieldExitKicker != null) {
+				PlayfieldExitKickerItem = PlayfieldExitKicker.AvailableCoils.First().Id;
+			}
 
 			return Array.Empty<MonoBehaviour>();
 		}
