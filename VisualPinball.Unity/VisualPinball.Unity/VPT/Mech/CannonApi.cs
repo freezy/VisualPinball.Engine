9b271d92df64c58328db354b11f51e89093f599d
jsm174 <jsm174@gmail.com>
2021-10-16 08:21:28 -0400

    cannon: move business logic from component to api

--- VisualPinball.Unity/VisualPinball.Unity/VPT/Mech/CannonApi.cs
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/Mech/CannonApi.cs
@@ -2,13 +2,20 @@ using System;
 using Logger = NLog.Logger;
 using NLog;
 using UnityEngine;
-using Unity.Mathematics;
 
 namespace VisualPinball.Unity
 {
 	public class CannonApi : IApi, IApiSwitchDevice, IApiCoilDevice
 	{
 		private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+		public const int Length = 240;
+
+		private enum Direction
+		{
+			Forward = 0,
+			Reverse = 1
+		}
 
 		private readonly CannonComponent _cannonComponent;
 		private Player _player;
@@ -20,13 +27,12 @@ namespace VisualPinball.Unity
 
 		public event EventHandler Init;
 
-		public int position = 0;
-		public const int Length = 240;
+		private Direction _direction;
+		private float _position;
 
 		internal CannonApi(GameObject go, Player player)
 		{
 			_cannonComponent = go.GetComponentInChildren<CannonComponent>();
-			_cannonComponent.SetAPI(this);
 			_player = player;
 		}
 
@@ -54,9 +60,15 @@ namespace VisualPinball.Unity
 			GunMotorCoil = new DeviceCoil(OnGunMotorCoilEnabled, OnGunMotorCoilDisabled);
 
 			GunHomeSwitch = new DeviceSwitch(CannonComponent.GunHomeSwitchItem, false, SwitchDefault.NormallyOpen, _player);
-			GunMarkSwitch = new DeviceSwitch(CannonComponent.GunMarkSwitchItem, false, SwitchDefault.NormallyOpen, _player);
+			GunHomeSwitch.SetSwitch(true);
 
-			UpdatePosition();
+			GunMarkSwitch = new DeviceSwitch(CannonComponent.GunMarkSwitchItem, false, SwitchDefault.NormallyOpen, _player);
+			GunMarkSwitch.SetSwitch(false);
+
+			_direction = Direction.Forward;
+			_position = 0;
+
+			_cannonComponent.OnGunMotorUpdatePosition += OnGunMotorUpdatePosition;
 
 			Init?.Invoke(this, EventArgs.Empty);
 		}
@@ -75,24 +87,67 @@ namespace VisualPinball.Unity
 			Logger.Info("OnGunMotorCoilDisabled");
 		}
 
-		public void UpdatePosition(float y = 0)
+		public float OnGunMotorUpdatePosition(float delta)
 		{
-			position = (int)(math.abs(y / 0.75) * Length);
+			float speed = (Length * 2 / 6.5f) * delta;
 
-			Logger.Info($"UpdatePosition: y={y}, position={position}");
+			if (_direction == Direction.Forward)
+			{
+				_position += speed;
 
-			GunHomeSwitch.SetSwitch(position >= 0 && position <= 5);
-			GunMarkSwitch.SetSwitch(position >= 98 && position <= 105);
+				if (_position >= Length)
+				{
+					_position = Length - (_position - Length);
+
+					_direction = Direction.Reverse;
+				}
+			}
+			else
+			{
+				_position -= speed;
+
+				if (_position <= 0)
+				{
+					_position = -_position;
+
+					_direction = Direction.Forward;
+				}
+			}
+
+			if (_position >= 0 && _position <= 5)
+			{
+				if (!GunHomeSwitch.IsEnabled)
+				{
+					GunHomeSwitch.SetSwitch(true);
+				}
+			}
+			else if (GunHomeSwitch.IsEnabled)
+			{
+				GunHomeSwitch.SetSwitch(false);
+			}
+
+			if (_position >= 98 && _position <= 105)
+			{
+				if (!GunMarkSwitch.IsEnabled)
+				{
+					GunMarkSwitch.SetSwitch(true);
+				}
+			}
+			else if (GunMarkSwitch.IsEnabled)
+			{
+				GunMarkSwitch.SetSwitch(false);
+			}
+
+			Logger.Info($"Position={_position}");
+
+			return _position / Length;
 		}
 
 		void IApi.OnDestroy()
 		{
-			Logger.Info("Destroying cannon!");
-		}
+			Logger.Info("Destroying cannon api!");
 
-		public void OnChange(bool enabled)
-		{
-			throw new NotImplementedException();
+			_cannonComponent.OnGunMotorUpdatePosition -= OnGunMotorUpdatePosition;
 		}
 	}
 }
--- VisualPinball.Unity/VisualPinball.Unity/VPT/Mech/CannonComponent.cs
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/Mech/CannonComponent.cs
@@ -7,97 +7,66 @@ using System;
 
 namespace VisualPinball.Unity
 {
-    [AddComponentMenu("Visual Pinball/Game Item/Cannon")]
-    public class CannonComponent : MonoBehaviour, ISwitchDeviceComponent, ICoilDeviceComponent
-    {
-        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+	[AddComponentMenu("Visual Pinball/Game Item/Cannon")]
+	public class CannonComponent : MonoBehaviour, ISwitchDeviceComponent, ICoilDeviceComponent
+	{
+		public delegate float OnGunMotorUpdatePositionHandler(float delta);
 
-        private enum Direction
-        {
-            CounterClockwise = 0,
-            Clockwise = 1
-        }
+		private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-        public const string GunMotorCoilItem = "gun_motor_coil";
+		public const string GunMotorCoilItem = "gun_motor_coil";
 
-        public IEnumerable<GamelogicEngineCoil> AvailableCoils => new[] {
-            new GamelogicEngineCoil(GunMotorCoilItem) {
-                Description = "Gun Motor"
-            }
-        };
+		public IEnumerable<GamelogicEngineCoil> AvailableCoils => new[] {
+			new GamelogicEngineCoil(GunMotorCoilItem) {
+				Description = "Gun Motor"
+			}
+		};
 
-        public const string GunMarkSwitchItem = "gun_mark_switch";
-        public const string GunHomeSwitchItem = "gun_home_switch";
+		public const string GunMarkSwitchItem = "gun_mark_switch";
+		public const string GunHomeSwitchItem = "gun_home_switch";
 
-        public IEnumerable<GamelogicEngineSwitch> AvailableSwitches => new[] {
-            new GamelogicEngineSwitch(GunMarkSwitchItem) {
-                Description = "Gun Mark"
-            },
-             new GamelogicEngineSwitch(GunHomeSwitchItem) {
-                Description = "Gun Home"
-            }
-        };
+		public IEnumerable<GamelogicEngineSwitch> AvailableSwitches => new[] {
+			new GamelogicEngineSwitch(GunMarkSwitchItem) {
+				Description = "Gun Mark"
+			},
+			 new GamelogicEngineSwitch(GunHomeSwitchItem) {
+				Description = "Gun Home"
+			}
+		};
 
-        public SwitchDefault SwitchDefault => SwitchDefault.NormallyOpen;
+		public SwitchDefault SwitchDefault => SwitchDefault.NormallyOpen;
 
-        IEnumerable<GamelogicEngineCoil> IDeviceComponent<GamelogicEngineCoil>.AvailableDeviceItems => AvailableCoils;
-        IEnumerable<GamelogicEngineSwitch> IDeviceComponent<GamelogicEngineSwitch>.AvailableDeviceItems => AvailableSwitches;
-        IEnumerable<IGamelogicEngineDeviceItem> IWireableComponent.AvailableWireDestinations => AvailableCoils;
-        IEnumerable<IGamelogicEngineDeviceItem> IDeviceComponent<IGamelogicEngineDeviceItem>.AvailableDeviceItems => AvailableCoils;
-       
-        public event EventHandler Init;
-        public bool IsEnabled = false;
-        private CannonApi _api;
-        private Direction _direction = Direction.CounterClockwise;
+		IEnumerable<GamelogicEngineCoil> IDeviceComponent<GamelogicEngineCoil>.AvailableDeviceItems => AvailableCoils;
+		IEnumerable<GamelogicEngineSwitch> IDeviceComponent<GamelogicEngineSwitch>.AvailableDeviceItems => AvailableSwitches;
+		IEnumerable<IGamelogicEngineDeviceItem> IWireableComponent.AvailableWireDestinations => AvailableCoils;
+		IEnumerable<IGamelogicEngineDeviceItem> IDeviceComponent<IGamelogicEngineDeviceItem>.AvailableDeviceItems => AvailableCoils;
 
-        public void SetAPI(CannonApi api)
+		public event OnGunMotorUpdatePositionHandler OnGunMotorUpdatePosition;
+
+		public bool IsEnabled = false;
+
+		private void Awake()
 		{
-            _api = api;
-		}
-
-        void Update()
-        {
-            if (!IsEnabled)
+			var player = GetComponentInParent<Player>();
+			if (player == null)
 			{
-                return;
+				Logger.Error($"Cannot find player for cannon {name}.");
+				return;
 			}
 
-            var rotation = transform.rotation;
+			player.RegisterMech(this);
+		}
 
-            if (_direction == Direction.CounterClockwise)
-            {
-                rotation.y -= 0.02f;
+		private void Update()
+		{
+			if (!IsEnabled) return;
 
-                if (rotation.y <= -.75)
-                {
-                    _direction = Direction.Clockwise;
-                }
-            }
-            else if (_direction == Direction.Clockwise)
-            {
-                rotation.y += 0.02f;
+			float position = OnGunMotorUpdatePosition(Time.deltaTime);
 
-                if (rotation.y >= 0)
-                {
-                    _direction = Direction.CounterClockwise;
-                }
-            }
+			var rotation = transform.rotation;
+			rotation.y = -(position * 0.65f);
 
-            _api.UpdatePosition(rotation.y);
-
-            transform.rotation = rotation;
-        }
-
-        private void Awake()
-        {
-            var player = GetComponentInParent<Player>();
-            if (player == null)
-            {
-                Logger.Error($"Cannot find player for cannon {name}.");
-                return;
-            }
-
-            player.RegisterMech(this);
-        }
+			transform.rotation = rotation;
+		}
 	}
 }
