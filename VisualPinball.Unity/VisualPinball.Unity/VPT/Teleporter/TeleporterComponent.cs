353da7665a71c5fdc381851cf8eb3f715697e3e3
freezy <freezy@kodi.tv>
2021-10-23 00:38:35 +0200

    teleporter: Add component and API.

--- VisualPinball.Unity/VisualPinball.Unity/VPT/Teleporter/TeleporterComponent.cs
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/Teleporter/TeleporterComponent.cs
@@ -22,37 +22,40 @@ using NLog;
 
 namespace VisualPinball.Unity
 {
-	[AddComponentMenu("Visual Pinball/Game Item/Cannon")]
-	public class CannonComponent : MonoBehaviour, ISwitchDeviceComponent, ICoilDeviceComponent
+	[AddComponentMenu("Visual Pinball/Game Item/Teleporter")]
+	public class TeleporterComponent : MonoBehaviour, ICoilDeviceComponent
 	{
+		#region Data
+
+		public KickerComponent PortalA;
+
+		public KickerComponent PortalB;
+
+		public float TimeMs;
+
+		#endregion
+
+		#region Overrides and Constants
+
+		public const string CoilItem = "teleport";
+
 		private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-		public const string GunMotorCoilItem = "gun_motor_coil";
+		#endregion
 
-		public IEnumerable<GamelogicEngineCoil> AvailableCoils => new[] {
-			new GamelogicEngineCoil(GunMotorCoilItem) {
-				Description = "Gun Motor"
-			}
-		};
-
-		public const string GunMarkSwitchItem = "gun_mark_switch";
-		public const string GunHomeSwitchItem = "gun_home_switch";
-
-		public IEnumerable<GamelogicEngineSwitch> AvailableSwitches => new[] {
-			new GamelogicEngineSwitch(GunMarkSwitchItem) {
-				Description = "Gun Mark"
-			},
-			 new GamelogicEngineSwitch(GunHomeSwitchItem) {
-				Description = "Gun Home"
-			}
-		};
-
-		public SwitchDefault SwitchDefault => SwitchDefault.NormallyOpen;
+		#region Wiring
 
 		IEnumerable<GamelogicEngineCoil> IDeviceComponent<GamelogicEngineCoil>.AvailableDeviceItems => AvailableCoils;
-		IEnumerable<GamelogicEngineSwitch> IDeviceComponent<GamelogicEngineSwitch>.AvailableDeviceItems => AvailableSwitches;
 		IEnumerable<IGamelogicEngineDeviceItem> IWireableComponent.AvailableWireDestinations => AvailableCoils;
 		IEnumerable<IGamelogicEngineDeviceItem> IDeviceComponent<IGamelogicEngineDeviceItem>.AvailableDeviceItems => AvailableCoils;
+
+		public IEnumerable<GamelogicEngineCoil> AvailableCoils => new[] {
+			new GamelogicEngineCoil(CoilItem) {
+				Description = "Teleport Ball"
+			}
+		};
+
+		#endregion
 
 		private void Awake()
 		{
@@ -63,15 +66,7 @@ namespace VisualPinball.Unity
 				return;
 			}
 
-			player.RegisterMech(this);
-		}
-
-		public void UpdateRotation(float y)
-		{
-			var rotation = transform.rotation;
-			rotation.y = -(y * 0.65f);
-
-			transform.rotation = rotation;
+			player.RegisterTeleporter(this);
 		}
 	}
 }
