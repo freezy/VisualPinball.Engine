ef3de5151cf3a09f9063ee74730c8b7ca4ed1bcc
freezy <freezy@kodi.tv>
2021-11-02 22:52:21 +0100

    editor: Add Unit attribute for suffixing labels.

---
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/Common/UnitPropertyDrawer.cs
@@ -0,0 +1,40 @@
+﻿// Visual Pinball Engine
+// Copyright (C) 2021 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace VisualPinball.Unity.Editor
+{
+	[CustomPropertyDrawer(typeof(UnitAttribute))]
+	public class UnitPropertyDrawer : PropertyDrawer
+	{
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			var labelAttribute = attribute as UnitAttribute;
+			var labelWidth = labelAttribute!.width + 2f;
+			var leftRect = position;
+			var rightRect = position;
+			leftRect.width -= labelWidth;
+			rightRect.x = position.width;
+			rightRect.width = labelWidth;
+
+			EditorGUI.PropertyField(leftRect, property, label);
+			GUI.Label(rightRect, labelAttribute.label, labelAttribute.labelStyle);
+		}
+	}
+}
---
+++ VisualPinball.Unity/VisualPinball.Unity/Common/UnitAttribute.cs
@@ -0,0 +1,46 @@
+﻿// Visual Pinball Engine
+// Copyright (C) 2021 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using UnityEngine;
+
+namespace VisualPinball.Unity
+{
+	/// <summary>
+	/// Put this attribute above any field
+	/// It adds a small label to the right of the property field.
+	/// This small label can be used to add information about the assumed unit.
+	///
+	/// <code>
+	/// <para>[Unit("m/s²")]</para>
+	/// <para>public float gravity = 9.80665f;</para>
+	/// </code>
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
+	public class UnitAttribute : PropertyAttribute
+	{
+		public string label;
+		public GUIStyle labelStyle;
+		public float width;
+
+		public UnitAttribute(string label)
+		{
+			this.label = label;
+			labelStyle = GUI.skin.GetStyle("miniLabel");
+			width = labelStyle.CalcSize(new GUIContent(label)).x;
+		}
+	}
+}
