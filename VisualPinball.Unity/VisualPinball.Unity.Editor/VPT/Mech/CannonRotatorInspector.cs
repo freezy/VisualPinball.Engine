346bffa02a7ca862ba7f2e466ed27ec1f0c5528c
freezy <freezy@kodi.tv>
2021-11-03 21:00:57 +0100

    mech: Fix cannon icon and add inspector.

---
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/VPT/Mech/CannonRotatorInspector.cs
@@ -0,0 +1,55 @@
+﻿// Visual Pinball Engine
+// Copyright (C) 2021 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+// ReSharper disable AssignmentInConditionalExpression
+
+using UnityEditor;
+using UnityEngine;
+using VisualPinball.Engine.VPT.Primitive;
+
+namespace VisualPinball.Unity.Editor
+{
+	[CustomEditor(typeof(CannonRotatorComponent)), CanEditMultipleObjects]
+	public class CannonRotatorInspector : ItemInspector
+	{
+		private SerializedProperty _mechProperty;
+		private SerializedProperty _factorWithProperty;
+
+		protected override MonoBehaviour UndoTarget => target as MonoBehaviour;
+
+		protected override void OnEnable()
+		{
+			base.OnEnable();
+
+			_mechProperty = serializedObject.FindProperty(nameof(CannonRotatorComponent._mech));
+			_factorWithProperty = serializedObject.FindProperty(nameof(CannonRotatorComponent.Factor));
+		}
+
+		public override void OnInspectorGUI()
+		{
+			BeginEditing();
+
+			OnPreInspectorGUI();
+
+			PropertyField(_mechProperty, "Mech Handler");
+			PropertyField(_factorWithProperty);
+
+			base.OnInspectorGUI();
+
+			EndEditing();
+		}
+	}
+}
