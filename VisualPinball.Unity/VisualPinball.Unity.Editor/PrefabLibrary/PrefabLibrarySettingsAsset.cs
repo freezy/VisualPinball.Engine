ec0a946d5770435ea648985dfafdf5b647672f72
VrôÕnsh <vroonsh@free.fr>
2021-11-19 18:50:55 +0100

    prefabs-library : first version
    - PrefabLibrarySettingsAsset
    - PrefabLibraryEditor
    - First try with global PinballMetaData embedded into assets .meta userData field
    - new JsonSerializableList object to allow save/load of polymorphic lists into Json format
    - new ProjectFolderPropertyDrawer for setting folders into PrefabLibrarySettingsAsset

---
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/PrefabLibrary/PrefabLibrarySettingsAsset.cs
@@ -0,0 +1,67 @@
+﻿// Visual Pinball Engine
+// Copyright (C) 2021 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEditor;
+using UnityEngine;
+
+namespace VisualPinball.Unity.Editor
+{
+	/// <summary>
+	/// An asset containing somme prefab library settings.
+	/// Prefabs are populated to the <see cref="PrefabLibraryEditor"/> using these settings.
+	/// </summary>
+	/// <remarks>
+	/// You can have several PrefabLibrarySettingsAssets, the editor will find all of them into the AssetDatabase.
+	/// </remarks>
+	[CreateAssetMenu(fileName = "Prefab Library Settings", menuName = "Visual Pinball/Prefab Library Settings", order = 101)]
+	public class PrefabLibrarySettingsAsset : ScriptableObject
+	{
+		public string Name = "Prefab Library";
+
+		[Serializable]
+		public class FolderSettings
+		{
+			public ProjectFolderReference FolderReference = new ProjectFolderReference();
+			[Tooltip("Prefabs will be searched recursively into this path.")]
+			public bool Recursive = true;
+		}
+
+		public bool Locked = false;
+
+		public List<string> AvailableTags = new List<string>();
+
+		public List<string> Categories
+		{
+			get {
+				var categories = new List<string>();
+				foreach(var tag in AvailableTags) {
+					var subtags = tag.Split('.');
+					if (subtags.Length > 1 && !categories.Contains(subtags[0])) {
+						categories.Add(subtags[0]);
+					}
+				}
+				return categories;
+			}
+		}
+
+		public List<FolderSettings> Folders = new List<FolderSettings>();
+
+		private Dictionary<string, List<string>> AssetTags = new Dictionary<string, List<string>>();
+	}
+}
---
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/Utils/PropertyDrawers/ProjectFolderPropertyDrawer.cs
@@ -0,0 +1,91 @@
+﻿// Visual Pinball Engine
+// Copyright (C) 2021 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace VisualPinball.Unity.Editor
+{
+	[Serializable]
+	public class ProjectFolderReference
+	{
+		public string Guid = string.Empty;
+	}
+
+	[CustomPropertyDrawer(typeof(ProjectFolderReference))]
+	public class ProjectFolderPropertyDrawer : PropertyDrawer
+	{
+		private SerializedProperty guid;
+		private Object obj;
+
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			guid = property.FindPropertyRelative("Guid");
+			obj = AssetDatabase.LoadAssetAtPath<Object>(AssetDatabase.GUIDToAssetPath(guid.stringValue));
+
+			GUIContent guiContent = EditorGUIUtility.ObjectContent(obj, typeof(DefaultAsset));
+
+			Rect r = EditorGUI.PrefixLabel(position, label);
+
+			Rect textFieldRect = r;
+			textFieldRect.width -= 19f;
+
+			GUIStyle textFieldStyle = new GUIStyle("TextField") {
+				imagePosition = obj ? ImagePosition.ImageLeft : ImagePosition.TextOnly
+			};
+
+			if (GUI.Button(textFieldRect, guiContent, textFieldStyle) && obj)
+				EditorGUIUtility.PingObject(obj);
+
+			if (textFieldRect.Contains(Event.current.mousePosition)) {
+				if (Event.current.type == EventType.DragUpdated) {
+					Object reference = DragAndDrop.objectReferences[0];
+					string path = AssetDatabase.GetAssetPath(reference);
+					DragAndDrop.visualMode = Directory.Exists(path) ? DragAndDropVisualMode.Copy : DragAndDropVisualMode.Rejected;
+					Event.current.Use();
+				} else if (Event.current.type == EventType.DragPerform) {
+					Object reference = DragAndDrop.objectReferences[0];
+					string path = AssetDatabase.GetAssetPath(reference);
+					if (Directory.Exists(path)) {
+						obj = reference;
+						guid.stringValue = AssetDatabase.AssetPathToGUID(path);
+					}
+					Event.current.Use();
+				}
+			}
+
+			Rect objectFieldRect = r;
+			objectFieldRect.x = textFieldRect.xMax + 1f;
+			objectFieldRect.width = 19f;
+
+			if (GUI.Button(objectFieldRect, "", GUI.skin.GetStyle("IN ObjectField"))) {
+				string path = EditorUtility.OpenFolderPanel("Select a folder", "", "");
+				if (path != string.Empty) {
+					if (path.Contains(Application.dataPath)) {
+						path = "Assets" + path.Substring(Application.dataPath.Length);
+						obj = AssetDatabase.LoadAssetAtPath(path, typeof(DefaultAsset));
+						guid.stringValue = AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(obj));
+					} else Debug.LogError("The path must be in the Assets folder");
+				}
+			}
+		}
+	}
+}
