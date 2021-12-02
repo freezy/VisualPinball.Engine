c8fe0abb4a9603f3a2abfadbfc3d927645eacaea
freezy <freezy@kodi.tv>
2021-12-02 22:36:01 +0100

    fix: Remove unused import.

--- VisualPinball.Unity/VisualPinball.Unity/VPT/Light/LightComponent.cs
+++ VisualPinball.Unity/VisualPinball.Unity/VPT/Light/LightComponent.cs
@@ -25,7 +25,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Codice.CM.Client.Gui;
 using NLog;
 using UnityEngine;
 using VisualPinball.Engine.Game.Engines;
