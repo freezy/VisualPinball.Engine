ea419727e11d55267350f1ea0d8504aa4ebd0f36
Andreas Derr <andreas_derr@web.de>
2022-01-14 19:00:31 +0100

    Editor: MWG: Correct Tests, deleted todo

--- VisualPinball.Engine.Test/VPT/MetalWireGuide/MetalWireGuideDataTests.cs
+++ VisualPinball.Engine.Test/VPT/MetalWireGuide/MetalWireGuideDataTests.cs
@@ -47,44 +47,16 @@ namespace VisualPinball.Engine.Test.VPT.MetalWireGuide
 
 		public static void ValidateMetalWireGuideData1(MetalWireGuideData data)
 		{
-			data.DragPoints.Length.Should().Be(3);
-			/* Todo: Test :-)
-			data.Elasticity.Should().Be(0.832f);
-			data.ElasticityFalloff.Should().Be(0.321f);
-			data.Friction.Should().Be(0.685f);
-			data.Height.Should().Be(25.556f);
-			data.HitEvent.Should().Be(false);
-			data.HitHeight.Should().Be(25.193f);
-			data.Image.Should().Be("test_pattern");
-			data.IsCollidable.Should().Be(true);
-			data.IsReflectionEnabled.Should().Be(true);
-			data.IsVisible.Should().Be(true);
-			data.Material.Should().Be("Playfield");
-			data.OverwritePhysics.Should().Be(true);
-			data.PhysicsMaterial.Should().Be("");
-			data.RotX.Should().Be(65.23f);
-			data.RotY.Should().Be(75.273f);
-			data.RotZ.Should().Be(70.962f);
-			data.Scatter.Should().Be(5.225f);
-			data.ShowInEditor.Should().Be(false);
-			data.StaticRendering.Should().Be(true);
-			data.Thickness.Should().Be(12);
-			data.Points.Should().Be(true);
-			*/
-		}
-
-		public static void ValidateMetalWireGuideData2(MetalWireGuideData data)
-		{
 			data.DragPoints.Length.Should().Be(2);
-			/* Todo Tests... :-)
-			data.Elasticity.Should().Be(0.8f);
-			data.ElasticityFalloff.Should().Be(0.3f);
-			data.Friction.Should().Be(0.6f);
+			data.Elasticity.Should().Be(0.99f);
+			data.ElasticityFalloff.Should().Be(0.33f);
+			data.Friction.Should().Be(0.55f);
 			data.Height.Should().Be(25f);
 			data.HitEvent.Should().Be(false);
-			data.HitHeight.Should().Be(25f);
+			data.HitHeight.Should().Be(25);
+			// i dont know where to set this...
 			data.Image.Should().Be("");
-			data.IsCollidable.Should().Be(false);
+			data.IsCollidable.Should().Be(true);
 			data.IsReflectionEnabled.Should().Be(true);
 			data.IsVisible.Should().Be(true);
 			data.Material.Should().Be("");
@@ -93,11 +65,36 @@ namespace VisualPinball.Engine.Test.VPT.MetalWireGuide
 			data.RotX.Should().Be(0f);
 			data.RotY.Should().Be(0f);
 			data.RotZ.Should().Be(0f);
+			data.Scatter.Should().Be(0f);
+			data.ShowInEditor.Should().Be(true);
+			data.StaticRendering.Should().Be(true);
+			data.Thickness.Should().Be(3f);
+			data.Points.Should().Be(true);
+		}
+
+		public static void ValidateMetalWireGuideData2(MetalWireGuideData data)
+		{
+			data.DragPoints.Length.Should().Be(3);
+			data.Elasticity.Should().Be(0.25f);
+			data.ElasticityFalloff.Should().Be(0.27f);
+			data.Friction.Should().Be(0.42f);
+			data.Height.Should().Be(30.2f);
+			data.HitEvent.Should().Be(false);
+			data.HitHeight.Should().Be(25.3f);
+			data.Image.Should().Be("");
+			data.IsCollidable.Should().Be(true);
+			data.IsReflectionEnabled.Should().Be(true);
+			data.IsVisible.Should().Be(false);
+			data.Material.Should().Be("");
+			data.OverwritePhysics.Should().Be(true);
+			data.PhysicsMaterial.Should().Be("");
+			data.RotX.Should().Be(1.57f);
+			data.RotY.Should().Be(1.73f);
+			data.RotZ.Should().Be(1.61f);
 			data.Scatter.Should().Be(5f);
-			data.ShowInEditor.Should().Be(false);
-			data.StaticRendering.Should().Be(false);
-			data.Thickness.Should().Be(8);
-			*/
+			data.ShowInEditor.Should().Be(true);
+			data.StaticRendering.Should().Be(true);
+			data.Thickness.Should().Be(2.5f);
 		}
 	}
 }
