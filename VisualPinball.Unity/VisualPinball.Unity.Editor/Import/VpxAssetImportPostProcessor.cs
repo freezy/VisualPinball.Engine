e190b7c99c90afefda179031da50fc6b8f963c19
Pandelii <neilnafus@gmail.com>
2022-03-25 12:59:22 -0700

    Added a texture processor.
    Added an asset import pre-processor that sets the correct settings based on postfix keywords.
    Case is ignored, postfix "Map" is also ignored since it seems common.
    
    Normal map identifiers = _normal, _nrm  _nrml
    Mask map identifiers = _mask, _msk, _madr, _mads
    Thick map identifiers = _thickness, _thick, _th
    Tangent map identifiers = _tangent, _tan, _tng
    Coat roughness map identifiers = _coat, _coatmask
    Emission map identifiers = _emission, _emit, _em, _emm

---
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/Import/VpxAssetImportPostProcessor.cs
@@ -0,0 +1,136 @@
+// Visual Pinball Engine
+// Copyright (C) 2022 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
+//TODO:		It would be benefitial to extend this out to a more fully featured asset pipeline
+//			in the future.  See: https://tech.innogames.com/building-a-custom-asset-pipeline-for-a-unity-project/
+// -pandeli 
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Linq; 
+
+namespace VisualPinball.Unity.Editor
+{
+	public class VpxAssetImportPostProcessor : AssetPostprocessor
+	{
+		private string[] normalIDs = new string[] { "_normal", "_nrm", "_nrml" };
+		private string[] maskIDs = new string[] { "_mask", "_msk", "_madr", "_mads" };
+		private string[] thickIDs = new string[] { "_thickness", "_thick", "_th" };
+		private string[] tangentIDs = new string[] { "_tangent", "_tan", "_tng" };
+		private string[] coatIDs = new string[] { "_coat", "_coatmask" };
+		private string[] emitIDs = new string[] { "_emission", "_emit", "_em", "_emm" };
+
+		void OnPreprocessTexture()
+		{
+
+			TextureImporter importer = assetImporter as TextureImporter;
+			string iName = PrepareName(assetPath);  //Prepare string for evaluation. 
+
+			//Common settings to apply to all textures.  
+			//importer.mipmapEnabled = true;
+			//importer.streamingMipmaps = true;
+
+			//Process normal map inputs and set normal map and compression flags.  
+			if(normalIDs.Any(iName.EndsWith))
+			{
+				importer.textureType = TextureImporterType.NormalMap;
+				importer.sRGBTexture = false;
+				importer.textureCompression = TextureImporterCompression.CompressedHQ;
+				Debug.Log("Processed normal map: " + iName);
+			}
+
+			//Process mask files to ensure they are linear.  
+			if(maskIDs.Any(iName.EndsWith))
+			{
+				importer.sRGBTexture = false;
+				Debug.Log("Processed mask map: " + iName);
+			}
+
+			//Emsure thickness maps are linear. 
+			if(thickIDs.Any(iName.EndsWith))
+			{
+				importer.sRGBTexture = false;
+				Debug.Log("Processed thickness map: " + iName);
+			}
+
+			//Ensure tangent maps are set to normal
+			if(tangentIDs.Any(iName.EndsWith))
+			{
+				importer.textureType = TextureImporterType.NormalMap;
+				importer.sRGBTexture = false;
+				Debug.Log("Processed tangent map: " + iName);
+			}
+
+			//Ensure coat roughness maps are linear and single channel.  
+			if(coatIDs.Any(iName.EndsWith))
+			{
+				importer.textureType = TextureImporterType.SingleChannel;
+				importer.sRGBTexture = false; 
+				Debug.Log("Processed coat roughness map: " + iName);
+			}
+			//Ensure emission maps are high quality to avoid compression artifacts.  
+			if(emitIDs.Any(iName.EndsWith))
+			{
+				importer.textureCompression = TextureImporterCompression.CompressedHQ;
+				Debug.Log("Processed emission map: " + iName);
+			}
+
+			
+		}
+
+		/// <summary>
+		/// Prepares a texture asset name for sorting.  
+		/// Removes MAP from name
+		/// </summary>
+		/// <param name="name">Input name from asset importer</param>
+		/// <returns>A clean name for sorting</returns>
+		private string PrepareName(string srcString)
+		{
+			string retName = "";
+			//Supported extention names to cull.  
+			string[] fileTypes = new string[] { ".png", ".tga", ".tif", ".tiff", ".psd", ".jpg", ".bmp", ".svg", ".hdr", ".exr", ".hdri"};
+			
+			retName = Path.GetFileName(assetPath);  //Remove unimportant path portion.  
+			retName = srcString.ToLowerInvariant();  //Drop to lower ignoring cultural variants. 
+			
+			foreach(string ext in fileTypes)
+			{
+				if(retName.EndsWith(ext))
+				{
+					retName = retName.Substring(0, retName.LastIndexOf(ext));
+					break; 
+				}
+			}
+
+			if(retName.EndsWith("map"))
+			{
+				string m = "map"; 
+				retName = retName.Substring(0, retName.LastIndexOf(m)); 
+			}
+
+			return retName; 
+		}
+		
+	}
+
+}
+
+
