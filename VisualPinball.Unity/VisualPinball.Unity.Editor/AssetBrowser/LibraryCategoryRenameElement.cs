7ff6d6f1f9466e611094635ec4df0c9f0abb80be
freezy <freezy@kodi.tv>
2022-03-28 00:42:15 +0200

    assets: Add element for renaming

---
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/AssetBrowser/LibraryCategoryRenameElement.cs
@@ -0,0 +1,33 @@
+﻿// Visual Pinball Engine
+// Copyright (C) 2022 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace VisualPinball.Unity.Editor
+{
+	public class LibraryCategoryRenameElement : VisualElement
+	{
+		public new class UxmlFactory : UxmlFactory<LibraryCategoryRenameElement, UxmlTraits> { }
+
+		public LibraryCategoryRenameElement()
+		{
+			var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Packages/org.visualpinball.engine.unity/VisualPinball.Unity/VisualPinball.Unity.Editor/AssetBrowser/LibraryCategoryRenameElement.uxml");
+			var ui = visualTree.CloneTree();
+			Add(ui);
+		}
+	}
+}
