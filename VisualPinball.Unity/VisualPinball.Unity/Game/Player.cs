0bf2cc1be9aacda4a2a3a5b235d1555387ec068f
Mr-Jay-Gatsby <jeremy.ostrowski.illuminate@gmail.com>
2023-02-16 13:58:55 -0500

    SoundInspector updates including refactor playroundrobin and playrandom methods, refactor handling of audio clip playing using an established gameobject's audiosource, and moved call to update event into OnEnable method. Also deleted mechsounddata and some other minor cleanup.

--- VisualPinball.Unity/VisualPinball.Unity/Game/Player.cs
+++ VisualPinball.Unity/VisualPinball.Unity/Game/Player.cs
@@ -24,8 +24,6 @@ using UnityEngine.InputSystem;
 using VisualPinball.Engine.Common;
 using VisualPinball.Engine.Game;
 using VisualPinball.Engine.Game.Engines;
-using VisualPinball.Engine.VPT.Flipper;
-using VisualPinball.Engine.VPT.MechSounds;
 using VisualPinball.Engine.VPT.Trigger;
 using Color = VisualPinball.Engine.Math.Color;
 using Logger = NLog.Logger;
