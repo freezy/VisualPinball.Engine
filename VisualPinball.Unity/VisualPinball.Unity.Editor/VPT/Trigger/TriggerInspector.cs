a022f4d46fbb86fdb6a678091badf72861280e06
freezy <freezy@kodi.tv>
2023-04-09 21:48:21 +0200

    ramps: Move upper/lower height when z-moving start and end drag points.

--- VisualPinball.Unity/VisualPinball.Unity.Editor/VPT/Trigger/TriggerInspector.cs
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/VPT/Trigger/TriggerInspector.cs
@@ -94,6 +94,9 @@ namespace VisualPinball.Unity.Editor
 		public ItemDataTransformType HandleType => ItemDataTransformType.TwoD;
 		public DragPointsInspectorHelper DragPointsHelper { get; private set; }
 		public float ZOffset => MainComponent.PositionZ;
+		public float[] TopBottomZ => null;
+		public void SetDragPointPosition(DragPointData dragPoint, Vertex3D value, int numSelectedDragPoints,
+			float[] topBottomZ) => dragPoint.Center = value;
 
 		#endregion
 	}
