5d3627ae5948a344f4085d19804b8d89a4751408
arthurkehrwald <50906979+arthurkehrwald@users.noreply.github.com>
2024-12-03 16:31:39 +0100

    Rewrite inspectors and sound playback

---
+++ VisualPinball.Unity/VisualPinball.Unity.Editor/Sound/SoundsComponentInspector.cs
@@ -0,0 +1,25 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+
+namespace VisualPinball.Unity.Editor
+{
+	[CustomEditor(typeof(MechSoundsComponent)), CanEditMultipleObjects]
+	public class SoundsComponentInspector : UnityEditor.Editor
+	{
+		[SerializeField]
+		private VisualTreeAsset inspectorXml;
+
+		public override VisualElement CreateInspectorGUI()
+		{
+			VisualElement inspector = new VisualElement();
+			var comp = target as MechSoundsComponent;
+			if (!comp!.TryGetComponent<ISoundEmitter>(out var _))
+				inspector.Add(new HelpBox("Cannot find sound emitter. This component only works with a sound emitter on the same GameObject.", HelpBoxMessageType.Warning));
+			inspectorXml.CloneTree(inspector);
+			return inspector;
+		}
+	}
+}
--- VisualPinball.Unity/VisualPinball.Unity/Sound/MechSoundsComponent.cs
+++ VisualPinball.Unity/VisualPinball.Unity/Sound/MechSoundsComponent.cs
@@ -19,6 +19,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using NLog;
 using UnityEngine;
 using UnityEngine.Audio;
@@ -30,130 +32,60 @@ namespace VisualPinball.Unity
 	public class MechSoundsComponent : MonoBehaviour
 	{
 		[SerializeField]
-		public List<MechSound> Sounds = new();
+		private List<MechSound> _sounds = new();
 
 		[SerializeField]
-		[Tooltip("If left blank, looks for an Audio Mixer in closest parent up the hierarchy.")]
-		public AudioMixerGroup AudioMixer;
+		private AudioMixerGroup _audioMixerGroup;
 
-		[NonSerialized]
 		private ISoundEmitter _soundEmitter;
-		[NonSerialized]
-		private SerializableDictionary<SoundAsset, AudioSource> _audioSources = new SerializableDictionary<SoundAsset, AudioSource>(); 
-		
-		private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
-		private Coroutine _co;
+		private CancellationTokenSource tcs;
 
-		private void Awake()
+		private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+		private void OnEnable()
 		{
 			_soundEmitter = GetComponent<ISoundEmitter>();
+			_soundEmitter.OnSound += HandleSoundEmitterOnSound;
+			tcs = new();
 		}
 
-		private void Start()
-		{
-			if (AudioMixer == null)
-			{
-				// find an Audio Mixer by searching up the hierarchy
-				AudioSource audioSource = GetComponentInParent<AudioSource>();
-				if (audioSource != null)
-				{
-					AudioMixer = audioSource.outputAudioMixerGroup;
-				}
-				else
-				{
-					Logger.Warn($"Sounds will not play without an Audio Mixer.");
-				}
-			}
-
-			if (_soundEmitter != null) {
-				_soundEmitter.OnSound += EmitSound;
-
-			} else {
-				// ? is AudioSource really a dependency here??
-				Logger.Warn($"Cannot initialize mech sound for {name} due to missing ISoundEmitter or AudioSource.");
-			}
-		}
-
-		private void OnDestroy()
+		private void OnDisable()
 		{
 			if (_soundEmitter != null) {
-				_soundEmitter.OnSound -= EmitSound;
+				_soundEmitter.OnSound -= HandleSoundEmitterOnSound;
 			}
+			tcs.Cancel();
+			tcs.Dispose();
+			tcs = null;
 		}
 
-		private void EmitSound(object sender, SoundEventArgs e)
+		// Async void is ok here because it's an event callback
+		private async void HandleSoundEmitterOnSound(object sender, SoundEventArgs e)
 		{
-			int clipCount = 0;
+			List<Task> playTasks = new();
+			foreach (MechSound sound in _sounds.Where(s => s.TriggerId == e.TriggerId))
+				playTasks.Add(Play(sound, tcs.Token));
+			await Task.WhenAll(playTasks);
+		}
 
-			foreach(MechSound sound in Sounds)
+		private async Task Play(MechSound sound, CancellationToken ct)
+		{
+			var audioSource = gameObject.AddComponent<AudioSource>();
+			try {
+				sound.Sound.Play(audioSource, sound.Volume);
+				_soundEmitter.OnSound += SoundEmitter_OnSound;
+				while (audioSource.isPlaying && !ct.IsCancellationRequested)
+					await Task.Yield();
+			} finally {
+				if (audioSource)
+					Destroy(audioSource);
+				_soundEmitter.OnSound -= SoundEmitter_OnSound;
+			}
+
+			void SoundEmitter_OnSound(object sender, SoundEventArgs eventArgs)
 			{
-				// filter for the TriggerId
-				if (sound.TriggerId != e.TriggerId) continue;
-
-				// get or create the AudioSource
-				AudioSource audioSource;
-				if (_audioSources.ContainsKey(sound.Sound))
-				{
-					audioSource = _audioSources[sound.Sound];
-				}
-				else
-				{
-					audioSource = gameObject.AddComponent<AudioSource>();
-					audioSource.outputAudioMixerGroup = AudioMixer;
-					_audioSources.Add(sound.Sound, audioSource);
-				}
-
-				if (sound.Action == MechSoundAction.Stop)
-				{
-					sound.Sound.Stop(audioSource);
-					Debug.Log($"Stopping sound {e.TriggerId} for {name}");
-					// we're done
-					continue;
-				}
-
-				// else sound.Action == MechSoundAction.Play
-				float fade = sound.Fade;
-				bool fadeVolume = false;
-
-				//convert fade duration from milliseconds to seconds for use with StartFade method
-				if (fade > 0)
-				{
-					//dont have a fade minimum of less than 1 second, if there is a fade. Less than 1 second fade,
-					//and the underlying method 'FadeMixerGroup.StartFade' will break down and not work correctly
-					if (fade < 1000)
-					{ fade = 1000; }
-
-				    fade = fade / 1000;
-					fadeVolume = true;
-				}
-
-				float volume = e.Volume;
-
-				AudioMixer audioMixer = GetComponent<AudioSource>().outputAudioMixerGroup.audioMixer;
-				sound.Sound.Play(audioSource, volume);
-
-				/* set audio mixer volume to decibel equivalent of volume slider value
-				   mixer volume is set at 0 dB when added to audiosource
-				   volume of 1 in slider is equivalent to 0 dB
-				*/
-				string exposedParameter = "vol1";
-				float sliderDBVolume = Mathf.Log10(volume) * 20;
-				float mixerVolume;
-				
-				audioMixer.GetFloat(exposedParameter, out mixerVolume);
-
-				//current coroutine is still fading the audio clip and needs to be stopped and volume reset
-				if (mixerVolume < sliderDBVolume)
-				{
-					StopCoroutine(_co);
-					audioMixer.SetFloat(exposedParameter, sliderDBVolume);
-				}
-
-				if (fadeVolume)
-				{ _co = StartCoroutine(FadeMixerGroup.StartFade(audioMixer, exposedParameter, 1, 0)); }
-				
-
-				Debug.Log($"Playing sound {e.TriggerId} for {name}");
+				if (sound.HasStopTrigger && eventArgs.TriggerId == sound.StopTriggerId)
+					audioSource.Stop();
 			}
 		}
 	}
