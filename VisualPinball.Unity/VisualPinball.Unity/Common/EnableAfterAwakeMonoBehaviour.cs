cc2f716ac0cfea8071b361c231d270b0d1381094
arthurkehrwald <50906979+arthurkehrwald@users.noreply.github.com>
2024-12-20 13:37:47 +0100

    Break up sound component into base and derived

---
+++ VisualPinball.Unity/VisualPinball.Unity/Common/EnableAfterAwakeMonoBehaviour.cs
@@ -0,0 +1,48 @@
+// Visual Pinball Engine
+// Copyright (C) 2023 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+using UnityEngine;
+
+namespace VisualPinball.Unity
+{
+	/// <summary>
+	/// MonoBehaviour.OnEnable is always called after Awake on the same instance,
+	/// but not across all instances. This is often inconvenient. This base class
+	/// provides the virtual method OnEnableAfterAwake, which always runs after all
+	/// instances have run their Awake methods.
+	/// </summary>
+	public abstract class EnableAfterAwakeMonoBehaviour : MonoBehaviour
+	{
+		private bool _wasStartCalled;
+
+		private void Start()
+		{
+			_wasStartCalled = true;
+			OnEnableAfterAfterAwake();
+		}
+
+		private void OnEnable()
+		{
+			if (_wasStartCalled)
+				OnEnableAfterAfterAwake();
+		}
+
+		protected virtual void OnEnableAfterAfterAwake()
+		{
+
+		}
+	}
+}
---
+++ VisualPinball.Unity/VisualPinball.Unity/Sound/TriggerSoundComponent.cs
@@ -0,0 +1,63 @@
+﻿// Visual Pinball Engine
+// Copyright (C) 2023 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+// ReSharper disable InconsistentNaming
+
+using System;
+using UnityEngine;
+
+namespace VisualPinball.Unity
+{
+	[AddComponentMenu("Visual Pinball/Sound/Trigger Sound")]
+	public class TriggerSoundComponent : SoundComponent
+	{
+		[SerializeField, HideInInspector]
+		private string _triggerId;
+		[SerializeField, HideInInspector]
+		private bool _hasStopTrigger;
+		[SerializeField, HideInInspector]
+		private string _stopTriggerId;
+
+		private ISoundEmitter _emitter;
+
+		protected override void OnEnable()
+		{
+			base.OnEnable();
+			_emitter = GetComponent<ISoundEmitter>();
+			_emitter.OnSound += Emitter_OnSound;
+		}
+
+		protected override void OnDisable()
+		{
+			base.OnDisable();
+			_emitter.OnSound -= Emitter_OnSound;
+			_emitter = null;
+		}
+
+		private async void Emitter_OnSound(object sender, SoundEventArgs e)
+		{
+			if (_hasStopTrigger && e.TriggerId == _stopTriggerId)
+				Stop(allowFade: true);
+
+			if (e.TriggerId == _triggerId) {
+				await Play(e.Volume);
+			}
+		}
+
+		public override Type GetRequiredType() => typeof(ISoundEmitter);
+		public override bool SupportsLoopingSoundAssets() => _hasStopTrigger;
+	}
+}
