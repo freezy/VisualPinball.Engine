1b156d287fc548b48baff091de234aee2f37b77c
arthurkehrwald <50906979+arthurkehrwald@users.noreply.github.com>
2024-12-20 14:40:16 +0100

    Add switch device sound

---
+++ VisualPinball.Unity/VisualPinball.Unity/Sound/SwitchDeviceSoundComponent.cs
@@ -0,0 +1,82 @@
+// Visual Pinball Engine
+// Copyright (C) 2023 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using UnityEngine;
+
+namespace VisualPinball.Unity
+{
+	[AddComponentMenu("Visual Pinball/Sound/Switch Device Sound")]
+	public class SwitchDeviceSoundComponent : SoundComponent
+	{
+
+		public enum StartWhen { SwitchEnabled, SwitchDisabled };
+		public enum StopWhen { Never, SwitchEnabled, SwitchDisabled };
+
+		[SerializeField] private StartWhen _startWhen = StartWhen.SwitchEnabled;
+		[SerializeField] private StopWhen _stopWhen = StopWhen.Never;
+		[SerializeField, HideInInspector] private string _switchName;
+		private IApiSwitch _switch;
+
+		protected override void OnEnableAfterAfterAwake()
+		{
+			base.OnEnableAfterAfterAwake();
+			if (TryFindSwitch(out _switch))
+				_switch.Switch += OnSwitch;
+			else
+				Logger.Warn("Could not find switch. Make sure an appropriate " +
+					"component is attached");
+		}
+
+		protected bool TryFindSwitch(out IApiSwitch @switch)
+		{
+			@switch = null;
+			var player = GetComponentInParent<Player>();
+			if (player == null)
+				return false;
+			foreach (var component in GetComponents<ISwitchDeviceComponent>()) {
+				@switch = player.Switch(component, _switchName);
+				if (@switch != null)
+					return true;
+			}
+			return @switch != null;
+		}
+
+		protected override void OnDisable()
+		{
+			base.OnDisable();
+			if (_switch != null) {
+				_switch.Switch -= OnSwitch;
+				_switch = null;
+			}
+		}
+
+		private async void OnSwitch(object sender, SwitchEventArgs e)
+		{
+			if ((e.IsEnabled && _stopWhen == StopWhen.SwitchEnabled) ||
+				(!e.IsEnabled && _stopWhen == StopWhen.SwitchDisabled))
+				Stop(allowFade: true);
+
+			if ((e.IsEnabled && _startWhen == StartWhen.SwitchEnabled) ||
+				(!e.IsEnabled && _startWhen == StartWhen.SwitchDisabled))
+				await Play();
+		}
+
+		public override bool SupportsLoopingSoundAssets() => _stopWhen != StopWhen.Never;
+
+		public override Type GetRequiredType() => typeof(ISwitchDeviceComponent);
+	}
+}
