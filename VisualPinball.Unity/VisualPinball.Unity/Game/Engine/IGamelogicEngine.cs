f2c9f07fc1ae46089151f1e91816cb65ecf0d56c
arthurkehrwald <50906979+arthurkehrwald@users.noreply.github.com>
2025-01-16 13:25:16 +0100

    Make IGameLogicEngine.Init asynchronous

--- VisualPinball.Unity/VisualPinball.Unity/Game/Engine/IGamelogicEngine.cs
+++ VisualPinball.Unity/VisualPinball.Unity/Game/Engine/IGamelogicEngine.cs
@@ -15,6 +15,7 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 using System;
+using System.Threading.Tasks;
 using UnityEngine;
 using VisualPinball.Engine.Game.Engines;
 
@@ -29,7 +30,7 @@ namespace VisualPinball.Unity
 	{
 		string Name { get; }
 
-		void OnInit(Player player, TableApi tableApi, BallManager ballManager);
+		Task OnInit(Player player, TableApi tableApi, BallManager ballManager);
 
 		#region Displays
 
