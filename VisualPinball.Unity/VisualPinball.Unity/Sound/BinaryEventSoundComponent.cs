5fb549bb3cce3f24bbbf0df94a8a911e57c47f0e
freezy <freezy@kodi.tv>
2025-02-09 00:37:09 +0100

    packaging: Properly pack coil and switch sound components.

--- VisualPinball.Unity/VisualPinball.Unity/Sound/BinaryEventSoundComponent.cs
+++ VisualPinball.Unity/VisualPinball.Unity/Sound/BinaryEventSoundComponent.cs
@@ -15,7 +15,9 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 // ReSharper disable InconsistentNaming
+
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace VisualPinball.Unity
 {
@@ -23,27 +25,25 @@ namespace VisualPinball.Unity
 	/// Start and or stop a sound when an event occurs that represents a binary state change,
 	/// such as a switch closing or a coil being energized.
 	/// </summary>
-	public abstract class BinaryEventSoundComponent<TEventSource, TEventArgs>
-		: EventSoundComponent<TEventSource, TEventArgs>
-		where TEventSource : class
+	public abstract class BinaryEventSoundComponent<TEventSource, TEventArgs> : EventSoundComponent<TEventSource, TEventArgs> where TEventSource : class
 	{
-		public enum StartWhen { TurnedOn, TurnedOff };
-		public enum StopWhen { Never, TurnedOn, TurnedOff };
+		[FormerlySerializedAs("_startWhen")]
+		public StartWhen StartWhen = StartWhen.TurnedOn;
 
-		[SerializeField] private StartWhen _startWhen = StartWhen.TurnedOn;
-		[SerializeField] private StopWhen _stopWhen = StopWhen.Never;
+		[FormerlySerializedAs("_stopWhen")]
+		public StopWhen StopWhen = StopWhen.Never;
 
 		protected override async void OnEvent(object sender, TEventArgs e)
 		{
 			bool isEnabled = InterpretAsBinary(e);
-			if ((isEnabled && _stopWhen == StopWhen.TurnedOn) ||
-			    (!isEnabled && _stopWhen == StopWhen.TurnedOff))
+			if ((isEnabled && StopWhen == StopWhen.TurnedOn) ||
+			    (!isEnabled && StopWhen == StopWhen.TurnedOff))
 			{
 				Stop(allowFade: true);
 			}
 
-			if ((isEnabled && _startWhen == StartWhen.TurnedOn) ||
-			    (!isEnabled && _startWhen == StartWhen.TurnedOff))
+			if ((isEnabled && StartWhen == StartWhen.TurnedOn) ||
+			    (!isEnabled && StartWhen == StartWhen.TurnedOff))
 			{
 				await Play();
 			}
@@ -51,7 +51,9 @@ namespace VisualPinball.Unity
 
 		protected abstract bool InterpretAsBinary(TEventArgs e);
 
-		public override bool SupportsLoopingSoundAssets()
-			=> _stopWhen != StopWhen.Never;
+		public override bool SupportsLoopingSoundAssets() => StopWhen != StopWhen.Never;
 	}
+
+	public enum StartWhen { TurnedOn, TurnedOff }
+	public enum StopWhen { Never, TurnedOn, TurnedOff }
 }
