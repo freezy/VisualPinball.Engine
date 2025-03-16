freezy/VisualPinball.Engine

commit b890c47341c82e10ba6ebafc6deab58e6e8ca7e5

Move bumper impulse into physics loop

VisualPinball.Unity/VisualPinball.Unity/VPT/Bumper/BumperApi.cs
@@ -18,6 +18,8 @@ using System;
 using Unity.Mathematics;
 using UnityEngine;
 using VisualPinball.Engine.VPT.Bumper;
+using System.Collections.Generic;
+using Unity.Mathematics;
 
 namespace VisualPinball.Unity
 {
@@ -93,17 +95,15 @@ namespace VisualPinball.Unity
 						HitOrgNormalVelocity = math.dot(bumpDirection, math.normalize(ballState.Velocity)),
 						IsContact = true,
 						ColliderId = _switchColliderId,
-						IsKinematic = false,
-						BallId = ballId
+								IsKinematic = false,
+								BallId = ballId
 					};
-					//var physicsMaterialData = ColliderComponent.PhysicsMaterialData;
-					//var random = PhysicsEngine.;
-					//BallCollider.Collide3DWall(ref ballState, in physicsMaterialData, in collEvent, in bumpDirection, ref state);
+					var physicsMaterialData = ColliderComponent.PhysicsMaterialData;
+					var random = PhysicsEngine.Random;
+					BallCollider.Collide3DWall(ref ballState, in physicsMaterialData, in collEvent, in bumpDirection, ref random);
 					ballState.Velocity += bumpDirection * ColliderComponent.Force;
 				}
 			}
-
-			CoilStatusChanged?.Invoke(this, new NoIdCoilEventArgs(enabled));
 		}
 
 		void IApiWireDest.OnChange(bool enabled) => (this as IApiCoil).OnCoil(enabled);

commit 1642ffc3853dc4dad2fe5c3491627e291a101f34

Fix: Bumper coil was not  triggering status events
This was broken in https://github.com/freezy/VisualPinball.Engine/commit/b890c47341c82e10ba6ebafc6deab58e6e8ca7e5.

VisualPinball.Unity/VisualPinball.Unity/VPT/Bumper/BumperApi.cs
@@ -102,6 +102,8 @@ namespace VisualPinball.Unity
 					BumperCollider.PushBallAway(ref ballState, in bumperState.Static, ref collEvent, in physicsMaterialData, ref state);
 				}
 			}
+			
+			CoilStatusChanged?.Invoke(this, new NoIdCoilEventArgs(enabled));
 		}
 
 		void IApiWireDest.OnChange(bool enabled) => (this as IApiCoil).OnCoil(enabled);