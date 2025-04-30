0693a9c9fa99d3a3501e70db90289012eca7ca4d
freezy <freezy@kodi.tv>
2025-04-30 21:22:53 +0200

    cam: Adjust parameters and drop touch support.

--- VisualPinball.Unity/VisualPinball.Unity/Game/CameraTranslateAndOrbit.cs
+++ VisualPinball.Unity/VisualPinball.Unity/Game/CameraTranslateAndOrbit.cs
@@ -1,4 +1,20 @@
-﻿using UnityEngine;
+﻿// Visual Pinball Engine
+// Copyright (C) 2023 freezy and VPE Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.EnhancedTouch;
 using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
@@ -9,11 +25,11 @@ using TouchPhase = UnityEngine.InputSystem.TouchPhase;
 /// </summary>
 public class CameraTranslateAndOrbit : MonoBehaviour
 {
-	public float panSpeed = 0.5f;
-	public float orbitSpeed = 0.4f;
-	public float zoomSpeed = 0.1f;
+	public float panSpeed = 10f;
+	public float orbitSpeed = 10f;
+	public float zoomSpeed = 10f;
 
-	public float smoothing = 1f;
+	public float smoothing = 10f;
 
 	public Transform initialOrbit;
 
@@ -72,96 +88,12 @@ public class CameraTranslateAndOrbit : MonoBehaviour
 		_rot2 = _transformCache.rotation;
 	}
 
-	private void OrbitAroundObject(Vector3 newOffset, float radiusRef)
-	{
-		positionOffset = newOffset;
-		_positionOffsetCurrent = positionOffset;
-		_radius = radiusRef;
-		_radiusCurrent = _radius;
-	}
-
 	// Update is called once per frame
 	private void Update()
 	{
-		if (Touchscreen.current != null) {
-			UpdateTouchscreen();
-
-			return;
-		}
-
 		if (Mouse.current != null) {
 			UpdateMouse();
 		}
-	}
-
-	private void UpdateTouchscreen()
-	{
-		if (Touch.activeFingers.Count == 1) {
-			Touch touch = Touch.activeTouches[0];
-
-			_transformCache.position = _dummyTransform.position = Vector3.zero;
-
-			var hasHitRestrictedHitArea = false;
-
-			if (touch.phase == TouchPhase.Began) {
-				_touch0StartPosition = touch.screenPosition;
-				_isTrackingTouch0 = true;
-			}
-
-			if (_touch0StartPosition.x < 200f && _touch0StartPosition.y > Screen.height - 200f) {
-				hasHitRestrictedHitArea = true;
-			}
-
-			if (!hasHitRestrictedHitArea) {
-				if (_isTrackingTouch0) {
-					Vector3 touchPositionDifference = touch.screenPosition - _touch0StartPosition;
-					_dummyTransform.Rotate(Vector3.up, touchPositionDifference.x * orbitSpeed / 4, Space.World);
-
-					_dummyTransform.Rotate(_dummyTransform.right.normalized, touchPositionDifference.y * -orbitSpeed,
-						Space.World);
-					_rot2 = _dummyTransform.rotation;
-					_touch0StartPosition = touch.screenPosition;
-				}
-				_transformCache.rotation = Quaternion.Lerp(_transformCache.rotation, _rot2, Time.deltaTime / smoothing * 4);
-			}
-
-			if (touch.phase == TouchPhase.Ended || touch.phase == TouchPhase.Canceled) {
-				_isTrackingTouch0 = false;
-			}
-		}
-		else if (Touch.activeFingers.Count == 2) {
-			var firstTouch = Touch.activeTouches[0];
-			var secondTouch = Touch.activeTouches[1];
-
-			_transformCache.position = _dummyTransform.position = Vector3.zero;
-
-			if (firstTouch.phase == TouchPhase.Began || secondTouch.phase == TouchPhase.Began) {
-				_startMultiTouchDistance = Vector2.Distance(firstTouch.screenPosition, secondTouch.screenPosition);
-
-				_startMultiTouchRadius = _radius;
-			}
-
-			if (firstTouch.phase == TouchPhase.Moved || secondTouch.phase == TouchPhase.Moved) {
-				_radius = _startMultiTouchRadius;
-
-				var distance = Vector2.Distance(firstTouch.screenPosition, secondTouch.screenPosition) - _startMultiTouchDistance;
-				var delta = distance / 250 * zoomSpeed * -_radius;
-
-				_radius += delta;
-
-				if (_radius < RadiusMin) {
-					var radDiff = RadiusMin - _radius;
-					positionOffset += _transformCache.forward * (radDiff * 4f);
-					_radius = RadiusMin;
-				}
-			}
-		}
-
-		_positionOffsetCurrent = Vector3.Lerp(_positionOffsetCurrent, positionOffset, Time.deltaTime * 4f);
-		_radiusCurrent = Mathf.Lerp(_radiusCurrent, _radius, Time.deltaTime * 4f);
-		_focusPoint = _transformCache.forward * -1f * _radiusCurrent;
-		_transformCache.position = _focusPoint + _positionOffsetCurrent;
-		_dummyTransform.position = _transformCache.position;
 	}
 
 	private void UpdateMouse()
@@ -182,14 +114,12 @@ public class CameraTranslateAndOrbit : MonoBehaviour
 		if (!hasHitRestrictedHitArea) {
 			if (_isTrackingMouse0) {
 				Vector3 mousePositionDifference = Mouse.current.position.ReadValue() - _mouse0StartPosition;
-				_dummyTransform.Rotate(Vector3.up, mousePositionDifference.x * orbitSpeed, Space.World);
-
-				_dummyTransform.Rotate(_dummyTransform.right.normalized, mousePositionDifference.y * -orbitSpeed,
-					Space.World);
+				_dummyTransform.Rotate(Vector3.up, mousePositionDifference.x * orbitSpeed / 75f, Space.World);
+				_dummyTransform.Rotate(_dummyTransform.right.normalized, mousePositionDifference.y * -orbitSpeed / 75f, Space.World);
 				_rot2 = _dummyTransform.rotation;
 				_mouse0StartPosition = Mouse.current.position.ReadValue();
 			}
-			_transformCache.rotation = Quaternion.Lerp(_transformCache.rotation, _rot2, Time.deltaTime / smoothing * 4);
+			_transformCache.rotation = Quaternion.Lerp(_transformCache.rotation, _rot2, Time.deltaTime / smoothing * 80);
 		}
 
 		if (Mouse.current.leftButton.wasReleasedThisFrame) {
@@ -207,8 +137,8 @@ public class CameraTranslateAndOrbit : MonoBehaviour
 				//Vector3 XZPlanerDirection = transformCache.forward.normalized;
 				//XZPlanerDirection.y = 0;
 
-				positionOffset += _transformCache.up.normalized * (mousePositionDifference.y * -(_radius * panSpeed / 100f));
-				positionOffset += _transformCache.right.normalized * (mousePositionDifference.x * -(_radius * panSpeed / 100f));
+				positionOffset += _transformCache.up.normalized * (mousePositionDifference.y * -(_radius * panSpeed / 60000f));
+				positionOffset += _transformCache.right.normalized * (mousePositionDifference.x * -(_radius * panSpeed / 60000f));
 
 				/*
 				if(positionOffset.y < 0){
@@ -225,7 +155,7 @@ public class CameraTranslateAndOrbit : MonoBehaviour
 
 		if (!hasHitRestrictedHitArea) {
 			if (!isAnimating) {
-				var delta = Mouse.current.scroll.y.ReadValue() / 10f * zoomSpeed * -_radius;
+				var delta = Mouse.current.scroll.y.ReadValue() / 300f * zoomSpeed * -_radius;
 				_radius += delta;
 				if (_radius < RadiusMin) {
 					var radDiff = RadiusMin - _radius;
@@ -235,8 +165,8 @@ public class CameraTranslateAndOrbit : MonoBehaviour
 			}
 		}
 
-		_positionOffsetCurrent = Vector3.Lerp(_positionOffsetCurrent, positionOffset, Time.deltaTime / smoothing * 4);
-		_radiusCurrent = Mathf.Lerp(_radiusCurrent, _radius, Time.deltaTime / smoothing * 4);
+		_positionOffsetCurrent = Vector3.Lerp(_positionOffsetCurrent, positionOffset, Time.deltaTime / smoothing * 80);
+		_radiusCurrent = Mathf.Lerp(_radiusCurrent, _radius, Time.deltaTime / smoothing * 80);
 
 		_focusPoint = _transformCache.forward * -1f * _radiusCurrent;
 		_transformCache.position = _focusPoint + _positionOffsetCurrent;
